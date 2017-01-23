6e6bb28211753dc830dcd8184bd9bf5b1108f34c
Vesa Luusua <vesa.luusua@gmail.com>
2017-01-23 19:55:55 +0200

    Create NamedLink component

---
+++ src/components/NamedLink/NamedLink.test.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { BrowserRouter } from 'react-router';
+import { RoutesProvider } from '../index';
+import Routes from '../../Routes';
+import NamedLink from './NamedLink';
+import renderer from 'react-test-renderer';
+
+describe('NamedLink', () => {
+  it('should contain correct link', () => {
+    const id = 12;
+    const routesConf = [
+      { pattern: '/somepage/:id', name: 'SomePage', component: () => <div>blaa</div> },
+    ];
+    const component = renderer.create(
+      (
+        <BrowserRouter>
+          <RoutesProvider routes={routesConf}>
+            <NamedLink name="SomePage" params={{ id }}>to SomePage</NamedLink>
+          </RoutesProvider>
+        </BrowserRouter>
+      ),
+    );
+    const json = component.toJSON();
+    expect(json.type).toEqual('a');
+    expect(json.props.href).toEqual(`/somepage/${id}`);
+    expect(json.children).toEqual([ 'to SomePage' ]);
+  });
+});
