c02ae5148ceea21f3a91e151aa5db87967458bf9
Vesa Luusua <vesa.luusua@gmail.com>
2017-01-24 18:12:37 +0200

    Initial React-Intl setup

--- src/containers/LandingPage/LandingPage.js
+++ src/containers/LandingPage/LandingPage.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import { FormattedMessage } from 'react-intl';
 import { Link } from 'react-router';
 import { PageLayout } from '../../components';
 
 export default () => (
   <PageLayout title="Landing page">
-    <Link to="/s?location=helsinki">Find studios</Link>
+    <Link to="/s?location=helsinki">
+      <FormattedMessage
+        id="landingpage.examplelink"
+        defaultMessage="Show nice studios! (default)"
+      />
+    </Link>
   </PageLayout>
 )
