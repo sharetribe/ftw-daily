2a60fcdc3b79ef7adbece6acc0a8b216419aa8de
Vesa Luusua <vesa.luusua@gmail.com>
2017-01-24 18:51:08 +0200

    Snapshot tests need IntlProvider - even when using default locales

--- src/containers/LandingPage/LandingPage.test.js
+++ src/containers/LandingPage/LandingPage.test.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router';
 import renderer from 'react-test-renderer';
+import { IntlProvider } from 'react-intl';
 import LandingPage from './LandingPage';
 
 describe('LandingPage', () => {
   it('matches snapshot', () => {
     const component = renderer.create(
       (
-        <BrowserRouter>
-          <LandingPage />
-        </BrowserRouter>
+        <IntlProvider locale="en">
+          <BrowserRouter>
+            <LandingPage />
+          </BrowserRouter>
+        </IntlProvider>
       ),
     );
     const tree = component.toJSON();
