e1295ba2b8843e49261db57b3f7cb958eadc0e74
Vesa Luusua <vesa.luusua@gmail.com>
2017-01-25 18:33:09 +0200

    NamedLink used with query

--- src/containers/LandingPage/LandingPage.js
+++ src/containers/LandingPage/LandingPage.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import { FormattedMessage } from 'react-intl';
-import { Link } from 'react-router';
-import { PageLayout } from '../../components';
+import { NamedLink, PageLayout } from '../../components';
 
 export default () => (
   <PageLayout title="Landing page">
-    <Link to="/s?location=helsinki">
+    <NamedLink name="SearchPage" query={{ location: 'helsinki' }}>
       <FormattedMessage
         id="landingpage.examplelink"
         defaultMessage="Show nice studios! (default)"
       />
-    </Link>
+    </NamedLink>
   </PageLayout>
 )
