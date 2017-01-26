39aa6608325eccae7af06ed79d4d1413a6c4d2a3
Vesa Luusua <vesa.luusua@gmail.com>
2017-01-26 16:22:42 +0200

    Add wireframes for SearchPage

--- src/components/index.js
+++ src/components/index.js
@@ -1,8 +1,27 @@
+import FilterPanel from './FilterPanel/FilterPanel';
 import HeroSection from './HeroSection/HeroSection';
+import ListingCard from './ListingCard/ListingCard';
+import ListingCardSmall from './ListingCardSmall/ListingCardSmall';
+import MapPanel from './MapPanel/MapPanel';
+import Menu from './Menu/Menu';
 import NamedLink from './NamedLink/NamedLink';
 import NamedRedirect from './NamedRedirect/NamedRedirect';
 import PageLayout from './PageLayout/PageLayout';
 import RouterProvider from './RouterProvider/RouterProvider';
 import RoutesProvider from './RoutesProvider/RoutesProvider';
+import SearchResultsPanel from './SearchResultsPanel/SearchResultsPanel';
 
-export { HeroSection, NamedLink, NamedRedirect, PageLayout, RouterProvider, RoutesProvider };
+export {
+  FilterPanel,
+  HeroSection,
+  ListingCard,
+  ListingCardSmall,
+  MapPanel,
+  Menu,
+  NamedLink,
+  NamedRedirect,
+  PageLayout,
+  RouterProvider,
+  RoutesProvider,
+  SearchResultsPanel,
+};
