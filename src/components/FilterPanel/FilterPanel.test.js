2165c793ae0e1f2e6927d626ab6c834b23d8256d
Vesa Luusua <vesa.luusua@gmail.com>
2017-01-26 15:30:10 +0200

    FilterPanel for SearchPage

---
+++ src/components/FilterPanel/FilterPanel.test.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { TestProvider } from '../../util/test-helpers';
+import { RoutesProvider } from '../../components';
+import routesConfiguration from '../../routesConfiguration';
+import FilterPanel from './FilterPanel';
+
+describe('FilterPanel', () => {
+  it('matches snapshot', () => {
+    const component = renderer.create(
+      (
+        <TestProvider>
+          <RoutesProvider routes={routesConfiguration}>
+            <FilterPanel />
+          </RoutesProvider>
+        </TestProvider>
+      ),
+    );
+    const tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
