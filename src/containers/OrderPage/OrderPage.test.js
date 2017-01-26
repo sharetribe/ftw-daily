e048be76ef59df75e44177ce31b1b9163a8ee753
Kimmo Puputti <git@kimmo.kpuputti.fi>
2017-01-26 13:34:03 +0200

    Provide routes to the OrderPage test

--- src/containers/OrderPage/OrderPage.test.js
+++ src/containers/OrderPage/OrderPage.test.js
@@ -2,13 +2,17 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import { TestProvider } from '../../util/test-helpers';
 import OrderPage from './OrderPage';
+import { RoutesProvider } from '../../components';
+import routesConfiguration from '../../routesConfiguration';
 
 describe('OrderPage', () => {
   it('matches snapshot', () => {
     const component = renderer.create(
       (
         <TestProvider>
-          <OrderPage params={{ id: 1234 }} />
+          <RoutesProvider routes={routesConfiguration}>
+            <OrderPage params={{ id: 1234 }} />
+          </RoutesProvider>
         </TestProvider>
       ),
     );
