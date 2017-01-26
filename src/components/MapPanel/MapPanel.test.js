acf85d9af6f471068f4be78c40f1fd2fdfa00557
Vesa Luusua <vesa.luusua@gmail.com>
2017-01-26 15:48:58 +0200

    MapPanel for SearchPage

---
+++ src/components/MapPanel/MapPanel.test.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { TestProvider } from '../../util/test-helpers';
+import { RoutesProvider } from '../../components';
+import routesConfiguration from '../../routesConfiguration';
+import MapPanel from './MapPanel';
+
+describe('MapPanel', () => {
+  it('matches snapshot', () => {
+    const component = renderer.create(
+      (
+        <TestProvider>
+          <RoutesProvider routes={routesConfiguration}>
+            <MapPanel />
+          </RoutesProvider>
+        </TestProvider>
+      ),
+    );
+    const tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
