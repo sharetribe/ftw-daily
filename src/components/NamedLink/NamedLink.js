40451463645a8e8490a7f0f7f7a1a0748b1fa2f5
Kimmo Puputti <git@kimmo.kpuputti.fi>
2017-02-16 17:15:27 +0200

    Add support for activeClassName for the NamedLink component

--- src/components/NamedLink/NamedLink.js
+++ src/components/NamedLink/NamedLink.js
@@ -1,30 +1,80 @@
 /**
  * This component wraps React-Router's Link by providing name-based routing.
- * (Helps to narrow down the scope of possible format changes to routes.)
+ *
+ * The `name` prop should match a route in the flattened
+ * routesConfiguration passed in context by the RoutesProvider
+ * component. The `params` props is the route params for the route
+ * path of the given route name.
+ *
+ * The `to` prop is an object with the same shape as Link requires,
+ * but without `pathname` that will be generated from the given route
+ * name.
+ *
+ * Some additional props can be passed for the <a> element like
+ * `className` and `style`.
+ *
+ * The component can also be given the `activeClassName` prop that
+ * will be added to the element className if the current URL matches
+ * the one in the generated pathname of the link.
  */
 import React, { PropTypes } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, withRouter } from 'react-router-dom';
+import classNames from 'classnames';
 import { pathByRouteName, withFlattenedRoutes } from '../../util/routes';
 import * as propTypes from '../../util/propTypes';
 
-const NamedLink = props => {
-  const { name, search, hash, state, params, flattenedRoutes, ...rest } = props;
+export const NamedLinkComponent = props => {
+  const { name, params, flattenedRoutes } = props;
+
+  // Link props
+  const { to, children } = props;
+
   const pathname = pathByRouteName(name, flattenedRoutes, params);
-  const locationDescriptor = { pathname, search, hash, state };
-  return <Link to={locationDescriptor} {...rest} />;
+  const { match } = props;
+  const active = match.url && match.url === pathname;
+
+  // <a> element props
+  const { className, style, activeClassName } = props;
+  const aElemProps = { className: classNames(className, { [activeClassName]: active }), style };
+
+  return <Link to={{ pathname, ...to }} {...aElemProps}>{children}</Link>;
 };
 
-const { arrayOf, object, string } = PropTypes;
+const { arrayOf, object, string, shape, any } = PropTypes;
 
-NamedLink.defaultProps = { search: '', hash: '', state: {}, params: {} };
+NamedLinkComponent.defaultProps = {
+  params: {},
+  to: {},
+  children: null,
+  className: '',
+  style: {},
+  activeClassName: 'NamedLink_active',
+  match: {},
+};
 
-NamedLink.propTypes = {
+// This ensures a nice display name in snapshots etc.
+NamedLinkComponent.displayName = 'NamedLink';
+
+NamedLinkComponent.propTypes = {
+  // name of the route in routesConfiguration
   name: string.isRequired,
-  search: string,
-  hash: string,
-  state: object,
+  // params object for the named route
   params: object,
+  // Link component props
+  to: shape({ search: string, hash: string, state: object }),
+  children: any,
+  // Note: The Link `replace` prop conflicts with the replace function
+  // from withRouter. If the replace boolean is required for the Link
+  // component, add it here with another name.
+
+  // generic props for the underlying <a> element
+  className: string,
+  style: object,
+  activeClassName: string,
+  // from withFlattenedRoutes
   flattenedRoutes: arrayOf(propTypes.route).isRequired,
+  // from withRouter
+  match: object,
 };
 
-export default withFlattenedRoutes(NamedLink);
+export default withFlattenedRoutes(withRouter(NamedLinkComponent));
