fdb1f96847c97499194e90bf37a374814da2303d
Kimmo Puputti <git@kimmo.kpuputti.fi>
2017-02-23 13:37:49 +0200

    Add test helpers for creating users and listings

--- src/util/test-helpers.js
+++ src/util/test-helpers.js
@@ -5,7 +5,10 @@ import toJson from 'enzyme-to-json';
 import { IntlProvider } from 'react-intl';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import { types } from 'sharetribe-sdk';
 import configureStore from '../store';
+
+const { UUID, LatLng } = types;
 
 // Provide all the context for components that connect to the Redux
 // store, i18n, router, etc.
@@ -46,3 +49,25 @@ export const renderDeep = component => {
   );
   return comp.toJSON();
 };
+
+// Create a user that conforms to the util/propTypes user schema
+export const createUser = id => ({
+  id: new UUID(id),
+  type: 'user',
+  attributes: {
+    email: '${id}@example.com',
+    profile: { firstName: `${id} first name`, lastName: `${id} last name`, slug: `${id}-slug` },
+  },
+});
+
+// Create a user that conforms to the util/propTypes listing schema
+export const createListing = id => ({
+  id: new UUID(id),
+  type: 'listing',
+  attributes: {
+    title: `${id} title`,
+    description: `${id} description`,
+    address: `${id} address`,
+    geolocation: new LatLng(40, 60),
+  },
+});
