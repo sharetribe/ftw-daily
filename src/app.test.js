47e1b36df20e090c44174ca3bc45aacd3cc70fcd
Vesa Luusua <vesa.luusua@gmail.com>
2017-03-03 20:05:29 +0200

    Update tests

--- src/app.test.js
+++ src/app.test.js
@@ -25,7 +25,7 @@ describe('Application', () => {
     const urlTitles = {
       '/': 'Landing page',
       '/s': 'Search page',
-      '/l/listing-title-slug/1234': 'Banyan Studios 55€ / day',
+      '/l/listing-title-slug/1234': 'Loading listing data',
       '/u/1234': 'Profile page with display name: 1234',
       '/checkout/1234': 'Book Banyan Studios (1234)',
       '/login': 'Authentication page: login tab',
