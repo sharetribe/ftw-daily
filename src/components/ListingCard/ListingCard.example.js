27dfb47ea23e30a5e9dfe8e27eec0d67aa82935d
Kimmo Puputti <git@kimmo.kpuputti.fi>
2017-03-24 10:40:51 +0200

    Add missing props to example

--- src/components/ListingCard/ListingCard.example.js
+++ src/components/ListingCard/ListingCard.example.js
@@ -1,17 +1,22 @@
 /* eslint-disable no-console, import/prefer-default-export */
 import React from 'react';
 import ListingCard from './ListingCard';
-import { createUser, createListing } from '../../util/test-data';
+import { createUser, createListing, currencyConfig, fakeIntl } from '../../util/test-data';
 
 const author = createUser('user1');
 const listing = { ...createListing('listing1'), author };
 
-const ListingCardWrapper = () => (
+const ListingCardWrapper = props => (
   <div style={{ width: '400px' }}>
-    <ListingCard listing={listing} />
+    <ListingCard {...props} />
   </div>
 );
 
 export const ListingCardWrapped = {
   component: ListingCardWrapper,
+  props: {
+    currencyConfig,
+    intl: fakeIntl,
+    listing,
+  },
 };
