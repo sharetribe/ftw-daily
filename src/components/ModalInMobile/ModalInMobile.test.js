4fd245cdd784f471c4e0745edbce5bdc7318dcd1
Vesa Luusua <vesa.luusua@gmail.com>
2017-05-02 20:31:15 +0300

    ModalInMobile uses Modal

--- src/components/ModalInMobile/ModalInMobile.test.js
+++ src/components/ModalInMobile/ModalInMobile.test.js
@@ -1,18 +1,16 @@
 import React from 'react';
-import { fakeIntl } from '../../util/test-data';
 import { renderDeep } from '../../util/test-helpers';
-import { ModalInMobileComponent } from './ModalInMobile';
+import ModalInMobile from './ModalInMobile';
 
 describe('ModalInMobile', () => {
   it('no extra classes when window is missing', () => {
     const props = {
       className: 'test-class-from-props',
-      intl: fakeIntl,
       isModalOpenOnMobile: false,
       togglePageClassNames: v => v,
     };
 
-    const tree = renderDeep(<ModalInMobileComponent {...props}>Content</ModalInMobileComponent>);
+    const tree = renderDeep(<ModalInMobile {...props}>Content</ModalInMobile>);
     expect(tree).toMatchSnapshot();
   });
 });
