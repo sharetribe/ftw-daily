012402f2f76310a7c645a970db743c7173f80eb8
Kimmo Puputti <git@kimmo.kpuputti.fi>
2017-05-03 16:16:08 +0300

    Signup form fields and styles

--- src/containers/SignUpForm/SignUpForm.js
+++ src/containers/SignUpForm/SignUpForm.js
@@ -1,42 +1,61 @@
-// **************************************************************************
-//
-// D E A D
-//
-//     C O D E
-//
-//         W A R N I N G !
-//
-// This component is currently not in use because:
-//
-// - We don't have signup ability in API yet
-// - Link to the page/component has been removed
-//
-// If we decide to take this component into use again, remove this warning.
-//
-// **************************************************************************
+import React, { PropTypes } from 'react';
+import { compose } from 'redux';
+import { FormattedMessage, injectIntl, intlShape } from 'react-intl';
+import { reduxForm, propTypes as formPropTypes } from 'redux-form';
+import { Button, LabeledField } from '../../components';
 
-import React from 'react';
-import { Field, reduxForm, propTypes as formPropTypes } from 'redux-form';
-import { Button, Input } from '../../components';
+import css from './SignupForm.css';
 
-const SignUpForm = props => {
-  const { handleSubmit, pristine, submitting } = props;
+const SignupFormComponent = props => {
+  const { handleSubmit, pristine, submitting, inProgress, intl } = props;
+  const emailLabel = intl.formatMessage({
+    id: 'SignupForm.emailLabel',
+  });
+  const passwordLabel = intl.formatMessage({
+    id: 'SignupForm.passwordLabel',
+  });
+  const firstNameLabel = intl.formatMessage({
+    id: 'SignupForm.firstNameLabel',
+  });
+  const lastNameLabel = intl.formatMessage({
+    id: 'SignupForm.lastNameLabel',
+  });
+  const submitDisabled = pristine || submitting || inProgress;
   return (
-    <form onSubmit={handleSubmit}>
-      <label htmlFor="email">Email</label>
-      <Field name="email" component={Input.fieldComponent} type="email" />
-      <label htmlFor="firstName">First name</label>
-      <Field name="firstName" component={Input.fieldComponent} />
-      <label htmlFor="lastName">Last name</label>
-      <Field name="lastName" component={Input.fieldComponent} />
-      <label htmlFor="password">Password</label>
-      <Field name="password" component={Input.fieldComponent} type="password" />
-      <p>By confirming I accept the booking terms and conditions.</p>
-      <Button type="submit" disabled={pristine || submitting}>Sign up</Button>
+    <form className={css.form} onSubmit={handleSubmit}>
+      <div>
+        <div className={css.row}>
+          <LabeledField name="email" type="email" label={emailLabel} />
+        </div>
+        <div className={css.row}>
+          <LabeledField name="firstName" label={firstNameLabel} />
+        </div>
+        <div className={css.row}>
+          <LabeledField name="lastName" label={lastNameLabel} />
+        </div>
+        <div className={css.row}>
+          <LabeledField name="password" type="password" label={passwordLabel} />
+        </div>
+      </div>
+      <Button className={css.button} type="submit" disabled={submitDisabled}>
+        <FormattedMessage id="SignupForm.signUp" />
+      </Button>
     </form>
   );
 };
 
-SignUpForm.propTypes = { ...formPropTypes };
+SignupFormComponent.defaultProps = { inProgress: false };
 
-export default reduxForm({ form: 'signup' })(SignUpForm);
+const { bool } = PropTypes;
+
+SignupFormComponent.propTypes = {
+  ...formPropTypes,
+  inProgress: bool,
+  intl: intlShape.isRequired,
+};
+
+const formName = 'SignupForm';
+
+const SignupForm = compose(reduxForm({ form: formName }), injectIntl)(SignupFormComponent);
+
+export default SignupForm;
