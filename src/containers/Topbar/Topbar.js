407def94edd039360c0095fde415865bb014a5e7
Vesa Luusua <vesa.luusua@gmail.com>
2017-05-04 18:45:51 +0300

    Topbar uses mobile search modal and SearchForm inside of it.

--- src/containers/Topbar/Topbar.js
+++ src/containers/Topbar/Topbar.js
@@ -1,12 +1,14 @@
 import React, { Component, PropTypes } from 'react';
 import { compose } from 'redux';
 import { connect } from 'react-redux';
+import { pickBy } from 'lodash';
 import { logout, authenticationInProgress } from '../../ducks/Auth.duck';
-import { Button, FlatButton, MobileMenu, Modal, NamedLink } from '../../components';
+import { FlatButton, MobileMenu, Modal, NamedLink } from '../../components';
+import { SearchForm } from '../../containers';
 import { withFlattenedRoutes } from '../../util/contextHelpers';
 import { parse, stringify } from '../../util/urlHelpers';
 import { ensureUser } from '../../util/data';
-import { pathByRouteName } from '../../util/routes';
+import { createResourceLocatorString, pathByRouteName } from '../../util/routes';
 import * as propTypes from '../../util/propTypes';
 
 import css from './Topbar.css';
@@ -14,32 +16,60 @@ import css from './Topbar.css';
 /* eslint-disable react/no-danger */
 const House = () => <span dangerouslySetInnerHTML={{ __html: '&#127968;' }} />;
 const Burger = () => <span dangerouslySetInnerHTML={{ __html: '&#9776;' }} />;
+const Search = () => <span dangerouslySetInnerHTML={{ __html: '&#128269;' }} />;
 /* eslint-enable react/no-danger */
+
+const redirectToURLWithModalState = (props, modalStateParam) => {
+  const { history, location } = props;
+  const { pathname, search, state } = location;
+  const searchString = `?${stringify({ [modalStateParam]: 'open', ...parse(search) })}`;
+  history.push(`${pathname}${searchString}`, state);
+};
+
+const redirectToURLWithoutModalState = (props, modalStateParam) => {
+  const { history, location } = props;
+  const { pathname, search, state } = location;
+  const queryParams = pickBy(parse(search), (v, k) => {
+    return k !== modalStateParam;
+  });
+  const stringified = stringify(queryParams);
+  const searchString = stringified ? `?${stringified}` : '';
+  history.push(`${pathname}${searchString}`, state);
+};
 
 class TopbarComponent extends Component {
   constructor(props) {
     super(props);
     this.handleMobileMenuOpen = this.handleMobileMenuOpen.bind(this);
     this.handleMobileMenuClose = this.handleMobileMenuClose.bind(this);
+    this.handleMobileSearchOpen = this.handleMobileSearchOpen.bind(this);
+    this.handleMobileSearchClose = this.handleMobileSearchClose.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
     this.handleLogout = this.handleLogout.bind(this);
   }
 
   handleMobileMenuOpen() {
-    const { history, location } = this.props;
-    const { pathname, search, state } = location;
-    const searchString = `?${stringify({ mobilemenu: 'open', ...parse(search) })}`;
-
-    history.push(`${pathname}${searchString}`, state);
+    redirectToURLWithModalState(this.props, 'mobilemenu');
   }
 
   handleMobileMenuClose() {
-    const { history, location } = this.props;
-    const { pathname, search, state } = location;
-    const { mobilemenu, ...queryParams } = parse(search); // eslint-disable-line no-unused-vars
-    const stringified = stringify(queryParams);
-    const searchString = stringified ? `?${stringified}` : '';
+    redirectToURLWithoutModalState(this.props, 'mobilemenu');
+  }
 
-    history.push(`${pathname}${searchString}`, state);
+  handleMobileSearchOpen() {
+    redirectToURLWithModalState(this.props, 'mobilesearch');
+  }
+
+  handleMobileSearchClose() {
+    redirectToURLWithoutModalState(this.props, 'mobilesearch');
+  }
+
+  handleSubmit(values) {
+    const selectedPlace = values && values.location ? values.location.selectedPlace : null;
+    const { flattenedRoutes, history } = this.props;
+    const { address, origin, bounds } = selectedPlace || {};
+    const searchParams = { address, origin, bounds };
+    history.push(createResourceLocatorString('SearchPage', flattenedRoutes, {}, searchParams));
   }
 
   handleLogout() {
@@ -63,12 +93,12 @@ class TopbarComponent extends Component {
     const me = ensureUser(currentUser);
     const profile = me.attributes.profile;
     const name = me.id ? `${profile.firstName} ${profile.lastName}` : '';
-    const { mobilemenu } = parse(location.search);
+    const { mobilemenu, mobilesearch } = parse(location.search);
     const isMobileMenuOpen = mobilemenu === 'open';
-
-    const authAction = isAuthenticated
-      ? <Button className={css.logoutButton} onClick={this.handleLogout}>Logout</Button>
-      : <NamedLink name="LogInPage">Login</NamedLink>;
+    const isMobileSearchOpen = mobilesearch === 'open';
+    const mobileMenu = (
+      <MobileMenu isAuthenticated={isAuthenticated} name={name} onLogout={this.handleLogout} />
+    );
 
     return (
       <div className={css.root}>
@@ -81,16 +111,23 @@ class TopbarComponent extends Component {
               <House />
             </NamedLink>
           </div>
-          <div className={css.user}>
-            {authInProgress ? null : authAction}
-          </div>
+          <FlatButton className={css.searchMenu} onClick={this.handleMobileSearchOpen}>
+            <Search />
+          </FlatButton>
         </div>
         <Modal
           isOpen={isMobileMenuOpen}
           onClose={this.handleMobileMenuClose}
           togglePageClassNames={togglePageClassNames}
         >
-          <MobileMenu isAuthenticated={isAuthenticated} name={name} onLogout={this.handleLogout} />
+          {authInProgress ? null : mobileMenu}
+        </Modal>
+        <Modal
+          isOpen={isMobileSearchOpen}
+          onClose={this.handleMobileSearchClose}
+          togglePageClassNames={togglePageClassNames}
+        >
+          <SearchForm className={css.searchForm} onSubmit={this.handleSubmit} />
         </Modal>
       </div>
     );
