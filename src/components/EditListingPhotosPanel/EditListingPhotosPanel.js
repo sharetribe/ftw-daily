36bc69a45b9e37cab3660fddbe574fbdd76c6980
Kimmo Puputti <git@kimmo.kpuputti.fi>
2017-05-15 10:43:13 +0300

    Remove old Stripe payout info fields

--- src/components/EditListingPhotosPanel/EditListingPhotosPanel.js
+++ src/components/EditListingPhotosPanel/EditListingPhotosPanel.js
@@ -1,41 +1,48 @@
-import React, { PropTypes } from 'react';
+import React, { Component, PropTypes } from 'react';
 import classNames from 'classnames';
 import { FormattedMessage } from 'react-intl';
 import { EditListingPhotosForm } from '../../containers';
 
 import css from './EditListingPhotosPanel.css';
 
-const EditListingPhotosPanel = props => {
-  const {
-    className,
-    rootClassName,
-    images,
-    onImageUpload,
-    onSubmit,
-    onUpdateImageOrder,
-    stripeConnected,
-  } = props;
+class EditListingPhotosPanel extends Component {
+  constructor(props) {
+    super(props);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  handleSubmit(values) {
+    console.log('EditListingPhotosPanel.handleSubmit():', values); // eslint-disable-line
+    const { onSubmit } = this.props;
+    onSubmit(values);
+  }
+  render() {
+    const {
+      className,
+      rootClassName,
+      images,
+      onImageUpload,
+      onUpdateImageOrder,
+      stripeConnected,
+    } = this.props;
 
-  const rootClass = rootClassName || css.root;
-  const classes = classNames(rootClass, className);
+    const rootClass = rootClassName || css.root;
+    const classes = classNames(rootClass, className);
 
-  // TODO This will be defined in the stripe popup form later.
-  const country = 'US';
-
-  return (
-    <div className={classes}>
-      <h1><FormattedMessage id="EditListingPhotosPanel.title" /></h1>
-      <EditListingPhotosForm
-        initialValues={{ country, images }}
-        images={images}
-        onImageUpload={onImageUpload}
-        onSubmit={onSubmit}
-        onUpdateImageOrder={onUpdateImageOrder}
-        stripeConnected={stripeConnected}
-      />
-    </div>
-  );
-};
+    return (
+      <div className={classes}>
+        <h1><FormattedMessage id="EditListingPhotosPanel.title" /></h1>
+        <EditListingPhotosForm
+          initialValues={{ images }}
+          images={images}
+          onImageUpload={onImageUpload}
+          onSubmit={this.handleSubmit}
+          onUpdateImageOrder={onUpdateImageOrder}
+          stripeConnected={stripeConnected}
+        />
+      </div>
+    );
+  }
+}
 
 const { array, bool, func, string } = PropTypes;
 
