8c0a228926a819c120b2cded44999111b87c5af2
Kimmo Puputti <git@kimmo.kpuputti.fi>
2017-05-29 10:11:53 +0300

    Add grouping support for Styleguide component examples

--- src/containers/StyleguidePage/StyleguidePage.js
+++ src/containers/StyleguidePage/StyleguidePage.js
@@ -1,17 +1,18 @@
 import React, { PropTypes } from 'react';
-import { map, size } from 'lodash';
 import { NamedLink } from '../../components';
 import * as allExamples from '../../examples';
 
 import css from './StyleguidePage.css';
 
 const ALL = '*';
+const DEFAULT_GROUP = 'misc';
 
 const Example = props => {
   const {
     componentName,
     exampleName,
     component: ExampleComponent,
+    group,
     description,
     props: exampleProps,
     useDefaultWrapperStyles,
@@ -22,14 +23,14 @@ const Example = props => {
   return (
     <li>
       <h2 className={css.withPadding}>
-        <NamedLink name="StyleguideComponent" params={{ component: componentName }}>
+        <NamedLink name="StyleguideComponent" params={{ group, component: componentName }}>
           {componentName}
         </NamedLink>
         /
 
         <NamedLink
           name="StyleguideComponentExample"
-          params={{ component: componentName, example: exampleName }}
+          params={{ group, component: componentName, example: exampleName }}
         >
           {exampleName}
         </NamedLink>
@@ -37,7 +38,7 @@ const Example = props => {
       <span className={css.withPadding}>
         <NamedLink
           name="StyleguideComponentExampleRaw"
-          params={{ component: componentName, example: exampleName, type: 'raw' }}
+          params={{ group, component: componentName, example: exampleName }}
         >
           raw
         </NamedLink>
@@ -50,74 +51,77 @@ const Example = props => {
   );
 };
 
-const { bool, func, node, object, objectOf, oneOfType, shape, string } = PropTypes;
+const { bool, func, node, object, oneOfType, shape, string } = PropTypes;
 
-Example.defaultProps = { description: null, props: {}, useDefaultWrapperStyles: true };
+Example.defaultProps = {
+  description: null,
+  group: DEFAULT_GROUP,
+  props: {},
+  useDefaultWrapperStyles: true,
+};
 
 Example.propTypes = {
   componentName: string.isRequired,
   exampleName: string.isRequired,
+  group: string,
   component: oneOfType([func, node]).isRequired,
   description: string,
   props: object,
   useDefaultWrapperStyles: bool,
 };
 
-const Examples = props => {
-  const { examples } = props;
-  const toExamples = (exmpls, name) => (
-    <li key={name}>
-      <ul>
-        {map(exmpls, (exmpl, exampleName) => (
-          <Example key={exampleName} componentName={name} exampleName={exampleName} {...exmpl} />
-        ))}
-      </ul>
-    </li>
-  );
-  return (
-    <ul>
-      {map(examples, toExamples)}
-    </ul>
+const flatExamples = examples => {
+  return Object.keys(examples).reduce(
+    (flattened, componentName) => {
+      const exs = Object.keys(examples[componentName]).reduce(
+        (result, exampleName) => {
+          const ex = examples[componentName][exampleName];
+          return result.concat([
+            {
+              componentName,
+              exampleName,
+              ...ex,
+            },
+          ]);
+        },
+        []
+      );
+      return flattened.concat(exs);
+    },
+    []
   );
 };
 
-Examples.propTypes = { examples: objectOf(objectOf(object)).isRequired };
-
-const examplesFor = (examples, componentName, exampleName) => {
-  // All components, all examples
-  if (componentName === ALL) {
-    return examples;
-  }
-
-  // Specific component, all examples
-  const component = examples[componentName];
-  if (exampleName === ALL) {
-    return component ? { [componentName]: component } : {};
-  }
-
-  // Specific component, specific example
-  const example = component ? component[exampleName] : null;
-  return example ? { [componentName]: { [exampleName]: example } } : {};
+const examplesFor = (examples, group, componentName, exampleName) => {
+  return examples.filter(ex => {
+    return (group === ALL || ex.group === group) &&
+      (componentName === ALL || ex.componentName === componentName) &&
+      (exampleName === ALL || ex.exampleName === exampleName);
+  });
 };
 
 const StyleguidePage = props => {
-  const { params } = props;
-  const component = params.component || ALL;
-  const example = params.example || ALL;
-  const raw = params.type === 'raw';
-  const examples = examplesFor(allExamples, component, example);
+  const { params, raw } = props;
+  const group = params.group || ALL;
+  const componentName = params.component || ALL;
+  const exampleName = params.example || ALL;
+
+  const flattened = flatExamples(allExamples);
+  const examples = examplesFor(flattened, group, componentName, exampleName);
 
   // Raw examples are rendered without any wrapper
-  if (raw && examples[component] && examples[component][example]) {
-    const { component: ExampleComponent, props: exampleProps } = examples[component][example];
+  if (raw && examples.length > 0) {
+    const { component: ExampleComponent, props: exampleProps } = examples[0];
     return <ExampleComponent {...exampleProps} />;
   } else if (raw) {
-    return <p>No example with filter {component}/{example}/raw</p>;
+    return <p>No example with filter {componentName}/{exampleName}/raw</p>;
   }
 
-  const html = size(examples) > 0
-    ? <Examples examples={examples} />
-    : <p>No examples with filter: {component}/{example}</p>;
+  const html = examples.length > 0
+    ? <ul>
+        {examples.map(ex => <Example key={`${ex.componentName}/${ex.exampleName}`} {...ex} />)}
+      </ul>
+    : <p>No examples with filter: {componentName}/{exampleName}</p>;
 
   return (
     <section className={css.root}>
@@ -129,6 +133,15 @@ const StyleguidePage = props => {
   );
 };
 
-StyleguidePage.propTypes = { params: shape({ component: string, example: string }).isRequired };
+StyleguidePage.defaultProps = { raw: false };
+
+StyleguidePage.propTypes = {
+  params: shape({
+    group: string,
+    component: string,
+    example: string,
+  }).isRequired,
+  raw: bool,
+};
 
 export default StyleguidePage;
