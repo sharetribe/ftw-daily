b9d52607954ca85c8d63df57b127223e6bc9e45c
Vesa Luusua <vesa.luusua@gmail.com>
2017-07-04 21:33:22 +0300

    EditListingPage: manage scrolling

--- src/containers/EditListingPage/EditListingPage.js
+++ src/containers/EditListingPage/EditListingPage.js
@@ -1,14 +1,14 @@
-import React, { Component, PropTypes } from 'react';
+import React, { PropTypes } from 'react';
 import { compose } from 'redux';
 import { withRouter } from 'react-router-dom';
 import { intlShape, injectIntl } from 'react-intl';
 import { connect } from 'react-redux';
-import { union, without } from 'lodash';
 import { types } from '../../util/sdkLoader';
 import { createSlug } from '../../util/urlHelpers';
 import * as propTypes from '../../util/propTypes';
 import { EditListingWizard, NamedRedirect, PageLayout } from '../../components';
 import { getListingsById } from '../../ducks/marketplaceData.duck';
+import { manageDisableScrolling, isScrollingDisabled } from '../../ducks/UI.duck';
 import { createStripeAccount } from '../../ducks/user.duck';
 import {
   createListingDraft,
@@ -42,99 +42,82 @@ const formatRequestData = values => {
 };
 
 // N.B. All the presentational content needs to be extracted to their own components
-export class EditListingPageComponent extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { pageClassNames: '' };
-    this.togglePageClassNames = this.togglePageClassNames.bind(this);
+export const EditListingPageComponent = props => {
+  const {
+    fetchInProgress,
+    flattenedRoutes,
+    history,
+    intl,
+    onCreateListing,
+    onImageUpload,
+    onPayoutDetailsSubmit,
+    onUpdateImageOrder,
+    page,
+    params,
+    tab,
+    type,
+    currentUser,
+    getListing,
+    onCreateListingDraft,
+    onManageDisableScrolling,
+    onUpdateListingDraft,
+    scrollingDisabled,
+  } = props;
+  const isNew = type === 'new';
+  const hasIdParam = params && params.id;
+  const id = page.submittedListingId || (hasIdParam ? new types.UUID(params.id) : null);
+  const currentListing = getListing(id);
+
+  const shouldRedirect = page.submittedListingId && currentListing;
+  const showForm = isNew || currentListing;
+
+  if (shouldRedirect) {
+    // If page has already listingId (after submit) and current listings exist
+    // redirect to listing page
+    const slug = currentListing ? createSlug(currentListing.attributes.title) : null;
+    return <NamedRedirect name="ListingPage" params={{ id: id.uuid, slug }} />;
+  } else if (showForm) {
+    // Show form if user is posting a new listing or editing existing one
+    const disableForm = page.redirectToListing && !page.showListingsError;
+
+    // Images are passed to EditListingForm so that it can generate thumbnails out of them
+    const images = page.imageOrder.map(i => page.images[i]);
+
+    const title = isNew
+      ? intl.formatMessage({ id: 'EditListingPage.titleCreateListing' })
+      : intl.formatMessage({ id: 'EditListingPage.titleEditListing' });
+
+    return (
+      <PageLayout title={title} scrollingDisabled={scrollingDisabled}>
+        <EditListingWizard
+          className={css.wizard}
+          disabled={disableForm}
+          fetchInProgress={fetchInProgress}
+          flattenedRoutes={flattenedRoutes}
+          history={history}
+          images={images}
+          listing={page.listingDraft}
+          onCreateListing={onCreateListing}
+          onCreateListingDraft={onCreateListingDraft}
+          onUpdateListingDraft={onUpdateListingDraft}
+          onPayoutDetailsSubmit={onPayoutDetailsSubmit}
+          onImageUpload={onImageUpload}
+          onUpdateImageOrder={onUpdateImageOrder}
+          selectedTab={tab}
+          currentUser={currentUser}
+          onManageDisableScrolling={onManageDisableScrolling}
+        />
+      </PageLayout>
+    );
+  } else {
+    // If user has come to this page through a direct linkto edit existing listing,
+    // we need to load it first.
+    const loadingPageMsg = {
+      id: 'ListingPage.loadingListingData',
+    };
+    return <PageLayout title={intl.formatMessage(loadingPageMsg)} />;
   }
-
-  togglePageClassNames(className, addClass = true) {
-    this.setState(prevState => {
-      const prevPageClassNames = prevState.pageClassNames.split(' ');
-      const pageClassNames = addClass
-        ? union(prevPageClassNames, [className]).join(' ')
-        : without(prevPageClassNames, className).join(' ');
-
-      return { pageClassNames };
-    });
-  }
-
-  render() {
-    const {
-      fetchInProgress,
-      flattenedRoutes,
-      history,
-      intl,
-      onCreateListing,
-      onImageUpload,
-      onPayoutDetailsSubmit,
-      onUpdateImageOrder,
-      page,
-      params,
-      tab,
-      type,
-      currentUser,
-      getListing,
-      onCreateListingDraft,
-      onUpdateListingDraft,
-    } = this.props;
-    const isNew = type === 'new';
-    const hasIdParam = params && params.id;
-    const id = page.submittedListingId || (hasIdParam ? new types.UUID(params.id) : null);
-    const currentListing = getListing(id);
-
-    const shouldRedirect = page.submittedListingId && currentListing;
-    const showForm = isNew || currentListing;
-
-    if (shouldRedirect) {
-      // If page has already listingId (after submit) and current listings exist
-      // redirect to listing page
-      const slug = currentListing ? createSlug(currentListing.attributes.title) : null;
-      return <NamedRedirect name="ListingPage" params={{ id: id.uuid, slug }} />;
-    } else if (showForm) {
-      // Show form if user is posting a new listing or editing existing one
-      const disableForm = page.redirectToListing && !page.showListingsError;
-
-      // Images are passed to EditListingForm so that it can generate thumbnails out of them
-      const images = page.imageOrder.map(i => page.images[i]);
-
-      const title = isNew
-        ? intl.formatMessage({ id: 'EditListingPage.titleCreateListing' })
-        : intl.formatMessage({ id: 'EditListingPage.titleEditListing' });
-
-      return (
-        <PageLayout title={title} className={this.state.pageClassNames}>
-          <EditListingWizard
-            className={css.wizard}
-            disabled={disableForm}
-            fetchInProgress={fetchInProgress}
-            flattenedRoutes={flattenedRoutes}
-            history={history}
-            images={images}
-            listing={page.listingDraft}
-            onCreateListing={onCreateListing}
-            onCreateListingDraft={onCreateListingDraft}
-            onUpdateListingDraft={onUpdateListingDraft}
-            onPayoutDetailsSubmit={onPayoutDetailsSubmit}
-            onImageUpload={onImageUpload}
-            onUpdateImageOrder={onUpdateImageOrder}
-            selectedTab={tab}
-            currentUser={currentUser}
-            togglePageClassNames={this.togglePageClassNames}
-          />
-        </PageLayout>
-      );
-    } else {
-      // If user has come to this page through a direct linkto edit existing listing,
-      // we need to load it first.
-      const loadingPageMsg = {
-        id: 'ListingPage.loadingListingData',
-      };
-      return <PageLayout title={intl.formatMessage(loadingPageMsg)} />;
-    }
-  }
-}
+};
 
 EditListingPageComponent.loadData = id => {
   requestShowListing({ id, include: ['author', 'images'] });
@@ -171,7 +154,9 @@ EditListingPageComponent.propTypes = {
   currentUser: propTypes.currentUser,
   getListing: func.isRequired,
   onCreateListingDraft: func.isRequired,
+  onManageDisableScrolling: func.isRequired,
   onUpdateListingDraft: func.isRequired,
+  scrollingDisabled: bool.isRequired,
 };
 
 const mapStateToProps = state => {
@@ -183,7 +168,13 @@ const mapStateToProps = state => {
     const listings = getListingsById(state, [id]);
     return listings.length === 1 ? listings[0] : null;
   };
-  return { page, currentUser, getListing, fetchInProgress };
+  return {
+    page,
+    currentUser,
+    getListing,
+    fetchInProgress,
+    scrollingDisabled: isScrollingDisabled(state),
+  };
 };
 
 const mapDispatchToProps = dispatch => {
@@ -194,6 +185,8 @@ const mapDispatchToProps = dispatch => {
     onUpdateImageOrder: imageOrder => dispatch(updateImageOrder(imageOrder)),
     onCreateListingDraft: values => dispatch(createListingDraft(values)),
     onUpdateListingDraft: values => dispatch(updateListingDraft(values)),
+    onManageDisableScrolling: (componentId, disableScrolling) =>
+      dispatch(manageDisableScrolling(componentId, disableScrolling)),
   };
 };
 
