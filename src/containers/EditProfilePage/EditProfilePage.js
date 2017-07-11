4db96385ee18ba7600b960e8b2abcfeea01819f7
Vesa Luusua <vesa.luusua@gmail.com>
2017-07-11 19:25:31 +0300

    EditProfilePage: update PageLayout and Topbar handling

--- src/containers/EditProfilePage/EditProfilePage.js
+++ src/containers/EditProfilePage/EditProfilePage.js
@@ -1,13 +1,102 @@
 import React, { PropTypes } from 'react';
-import { PageLayout } from '../../components';
+import { compose } from 'redux';
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
+import * as propTypes from '../../util/propTypes';
+import { logout, authenticationInProgress } from '../../ducks/Auth.duck';
+import { manageDisableScrolling, isScrollingDisabled } from '../../ducks/UI.duck';
+import { PageLayout, Topbar } from '../../components';
 
-const EditProfilePage = props => {
-  const { params } = props;
-  return <PageLayout title={`Edit profile page with display name: ${params.displayName}`} />;
+export const EditProfilePageComponent = props => {
+  const {
+    authInfoError,
+    authInProgress,
+    currentUser,
+    currentUserHasListings,
+    history,
+    isAuthenticated,
+    location,
+    logoutError,
+    notificationCount,
+    onLogout,
+    onManageDisableScrolling,
+    params,
+  } = props;
+
+  return (
+    <PageLayout authInfoError={authInfoError} logoutError={logoutError} title={`Edit profile page with display name: ${params.displayName}`}>
+      <Topbar
+        authInProgress={authInProgress}
+        currentUser={currentUser}
+        currentUserHasListings={currentUserHasListings}
+        history={history}
+        isAuthenticated={isAuthenticated}
+        location={location}
+        notificationCount={notificationCount}
+        onLogout={onLogout}
+        onManageDisableScrolling={onManageDisableScrolling}
+      />
+    </PageLayout>
+  );
 };
 
-const { shape, string } = PropTypes;
+EditProfilePageComponent.defaultProps = {
+  authInfoError: null,
+  currentUser: null,
+  logoutError: null,
+  notificationCount: 0,
+};
 
-EditProfilePage.propTypes = { params: shape({ displayName: string.isRequired }).isRequired };
+const { bool, func, instanceOf, number, object, shape, string } = PropTypes;
+
+EditProfilePageComponent.propTypes = {
+  authInfoError: instanceOf(Error),
+  authInProgress: bool.isRequired,
+  currentUser: propTypes.currentUser,
+  currentUserHasListings: bool.isRequired,
+  isAuthenticated: bool.isRequired,
+  logoutError: instanceOf(Error),
+  notificationCount: number,
+  onLogout: func.isRequired,
+  onManageDisableScrolling: func.isRequired,
+  params: shape({ displayName: string.isRequired }).isRequired,
+
+  // from withRouter
+  history: shape({
+    push: func.isRequired,
+  }).isRequired,
+  location: shape({ state: object }).isRequired,
+};
+
+const mapStateToProps = state => {
+  // PageLayout needs authInfoError and logoutError, Topbar needs isAuthenticated
+  const { authInfoError, isAuthenticated, logoutError } = state.Auth;
+  // Topbar needs user info.
+  const {
+    currentUser,
+    currentUserHasListings,
+    currentUserNotificationCount: notificationCount,
+  } = state.user;
+  return {
+    authInfoError,
+    authInProgress: authenticationInProgress(state),
+    currentUser,
+    currentUserHasListings,
+    currentUserNotificationCount: notificationCount,
+    isAuthenticated,
+    logoutError,
+    scrollingDisabled: isScrollingDisabled(state),
+  };
+};
+
+const mapDispatchToProps = dispatch => ({
+  onLogout: historyPush => dispatch(logout(historyPush)),
+  onManageDisableScrolling: (componentId, disableScrolling) =>
+    dispatch(manageDisableScrolling(componentId, disableScrolling)),
+});
+
+const EditProfilePage = compose(connect(mapStateToProps, mapDispatchToProps), withRouter)(
+  EditProfilePageComponent
+);
 
 export default EditProfilePage;
