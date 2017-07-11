6afdadb4aaf95573af2af2a643369f81cf3391b2
Vesa Luusua <vesa.luusua@gmail.com>
2017-07-11 19:25:49 +0300

    ManageListingsPage: update PageLayout and Topbar handling

--- src/containers/ManageListingsPage/ManageListingsPage.test.js
+++ src/containers/ManageListingsPage/ManageListingsPage.test.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import { renderShallow } from '../../util/test-helpers';
-import ManageListingsPage from './ManageListingsPage';
+import { ManageListingsPageComponent } from './ManageListingsPage';
 
-describe('ManageListingsPage', () => {
+const noop = () => null;
+
+describe('ContactDetailsPage', () => {
   it('matches snapshot', () => {
-    const tree = renderShallow(<ManageListingsPage />);
+    const tree = renderShallow(
+      <ManageListingsPageComponent
+        params={{ displayName: 'my-shop' }}
+        history={{ push: noop }}
+        location={{ search: '' }}
+        scrollingDisabled={false}
+        authInProgress={false}
+        currentUserHasListings={false}
+        isAuthenticated={false}
+        onLogout={noop}
+        onManageDisableScrolling={noop}
+      />
+    );
     expect(tree).toMatchSnapshot();
   });
 });
