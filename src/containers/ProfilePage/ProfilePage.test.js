519ab51661ef708b08c636e8b24fac59ee36a9e2
Vesa Luusua <vesa.luusua@gmail.com>
2017-07-11 19:27:08 +0300

    ProfilePage: update PageLayout and Topbar handling

--- src/containers/ProfilePage/ProfilePage.test.js
+++ src/containers/ProfilePage/ProfilePage.test.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import { renderShallow } from '../../util/test-helpers';
-import ProfilePage from './ProfilePage';
+import { ProfilePageComponent } from './ProfilePage';
+
+const noop = () => null;
 
 describe('ProfilePage', () => {
   it('matches snapshot', () => {
-    const tree = renderShallow(<ProfilePage params={{ displayName: 'most-awesome-shop' }} />);
+    const tree = renderShallow(
+      <ProfilePageComponent
+        params={{ displayName: 'most-awesome-shop' }}
+        history={{ push: noop }}
+        location={{ search: '' }}
+        scrollingDisabled={false}
+        authInProgress={false}
+        currentUserHasListings={false}
+        isAuthenticated={false}
+        onLogout={noop}
+        onManageDisableScrolling={noop}
+      />
+    );
     expect(tree).toMatchSnapshot();
   });
 });
