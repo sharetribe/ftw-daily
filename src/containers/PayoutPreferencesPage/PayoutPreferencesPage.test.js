84e43213d000b86c1d523ad3a06c71addb988edc
Vesa Luusua <vesa.luusua@gmail.com>
2017-07-11 19:26:50 +0300

    PayoutPreferencesPage: update PageLayout and Topbar handling

--- src/containers/PayoutPreferencesPage/PayoutPreferencesPage.test.js
+++ src/containers/PayoutPreferencesPage/PayoutPreferencesPage.test.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import { renderShallow } from '../../util/test-helpers';
-import PayoutPreferencesPage from './PayoutPreferencesPage';
+import { PayoutPreferencesPageComponent } from './PayoutPreferencesPage';
+
+const noop = () => null;
 
 describe('PayoutPreferencesPage', () => {
   it('matches snapshot', () => {
-    const tree = renderShallow(<PayoutPreferencesPage />);
+    const tree = renderShallow(
+      <PayoutPreferencesPageComponent
+        params={{ displayName: 'my-shop' }}
+        history={{ push: noop }}
+        location={{ search: '' }}
+        scrollingDisabled={false}
+        authInProgress={false}
+        currentUserHasListings={false}
+        isAuthenticated={false}
+        onLogout={noop}
+        onManageDisableScrolling={noop}
+      />
+    );
     expect(tree).toMatchSnapshot();
   });
 });
