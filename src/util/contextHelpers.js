6b92f4417101304a6d3ba3a954bffd45ae482d19
Kimmo Puputti <git@kimmo.kpuputti.fi>
2017-08-09 14:14:25 +0300

    Add helper HOC to listen to viewport dimension changes

--- src/util/contextHelpers.js
+++ src/util/contextHelpers.js
@@ -1,4 +1,5 @@
-import React, { PropTypes } from 'react';
+import React, { PropTypes, Component as ReactComponent } from 'react';
+import { throttle } from 'lodash';
 import * as propTypes from './propTypes';
 
 /**
@@ -42,4 +43,50 @@ export const withTogglePageClassNames = Component => {
   };
 
   return WithTogglePageClassNamesComponent;
+};
+
+/**
+ * A higher order component (HOC) that provides the current viewport
+ * dimensions to the wrapped component as a `viewport` prop that has
+ * the shape `{ width: 600, height: 400}`.
+ */
+export const withViewport = Component => {
+  // The resize event is flooded when the browser is resized. We'll
+  // use a small timeout to throttle changing the viewport since it
+  // will trigger rerendering.
+  const WAIT_MS = 100;
+
+  class WithViewportComponent extends ReactComponent {
+    constructor(props) {
+      super(props);
+      this.state = { width: 0, height: 0 };
+      this.handleWindowResize = this.handleWindowResize.bind(this);
+      this.setViewport = throttle(this.setViewport.bind(this), WAIT_MS);
+    }
+    componentDidMount() {
+      this.setViewport();
+      window.addEventListener('resize', this.handleWindowResize);
+    }
+    componentWillUnmount() {
+      window.removeEventListener('resize', this.handleWindowResize);
+    }
+    handleWindowResize() {
+      this.setViewport();
+    }
+    setViewport() {
+      this.setState({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    }
+    render() {
+      const viewport = this.state;
+      const props = { ...this.props, viewport };
+      return <Component {...props} />;
+    }
+  }
+
+  WithViewportComponent.displayName = `withViewport(${Component.displayName || Component.name})`;
+
+  return WithViewportComponent;
 };
