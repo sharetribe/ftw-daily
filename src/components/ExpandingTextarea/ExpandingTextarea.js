8708a9694f1f2cc6bcebc94c725f731134ee1827
Kimmo Puputti <git@kimmo.kpuputti.fi>
2017-08-18 08:16:57 +0300

    Clear initialisation timeout when unmounting

--- src/components/ExpandingTextarea/ExpandingTextarea.js
+++ src/components/ExpandingTextarea/ExpandingTextarea.js
@@ -4,12 +4,13 @@ import autosize from 'autosize';
 class ExpandingTextarea extends Component {
   constructor(props) {
     super(props);
+    this.timeoutId = null;
     this.textarea = null;
   }
   componentDidMount() {
     // Delay the autosize initialisation so that the autosize can
     // correctly calculate the height with the textarea value
-    window.setTimeout(
+    this.timeoutId = window.setTimeout(
       () => {
         autosize(this.textarea);
       },
@@ -21,6 +22,7 @@ class ExpandingTextarea extends Component {
   }
   componentWillUnmount() {
     autosize.destroy(this.textarea);
+    window.clearTimeout(this.timeoutId);
   }
   render() {
     return (
