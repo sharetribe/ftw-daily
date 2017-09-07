41b5c04d569c5a3c9705756bbff99713e4d9e181
Hannu Lyytikainen <hannu@sharetribe.com>
2017-09-07 14:08:11 +0300

    Add PasswordRecoveryForm

--- src/containers/PasswordRecoveryForm/PasswordRecoveryForm.js
+++ src/containers/PasswordRecoveryForm/PasswordRecoveryForm.js
@@ -1,21 +1,62 @@
 import React from 'react';
+import { compose } from 'redux';
+import { FormattedMessage, injectIntl, intlShape } from 'react-intl';
 import { Field, reduxForm, propTypes as formPropTypes } from 'redux-form';
-import { Button } from '../../components';
+import { PrimaryButton, TextInputField, NamedLink } from '../../components';
+import * as validators from '../../util/validators';
 
-const PasswordRecoveryForm = props => {
-  const { handleSubmit, pristine, submitting } = props;
+import css from './PasswordRecoveryForm.css';
+
+const PasswordRecoveryFormComponent = props => {
+  const { handleSubmit, pristine, submitting, form, intl } = props;
+
+  // email
+  const emailLabel = intl.formatMessage({
+    id: 'PasswordRecoveryForm.emailLabel',
+  });
+  const emailPlaceholder = intl.formatMessage({
+    id: 'PasswordRecoveryForm.emailPlaceholder',
+  });
+  const emailRequiredMessage = intl.formatMessage({
+    id: 'PasswordRecoveryForm.emailRequired',
+  });
+  const emailRequired = validators.required(emailRequiredMessage);
   return (
     <form onSubmit={handleSubmit}>
-      <label htmlFor="email">Email</label>
-      <Field name="email" component="input" type="email" />
-      <p>We will send you instructions to your email.</p>
-      <Button type="submit" disabled={pristine || submitting}>Send</Button>
+      <TextInputField
+        className={css.email}
+        type="email"
+        name="email"
+        id={`${form}.email`}
+        label={emailLabel}
+        placeholder={emailPlaceholder}
+        validate={emailRequired}
+      />
+      <p className={css.bottomWrapper}>
+        <NamedLink name="LoginPage" className={css.goToLoginLink}>
+          <span className={css.goToLoginLinkHelp}>
+            <FormattedMessage id="PasswordRecoveryForm.goToLoginHelp" />
+          </span>
+          {' '}
+          <FormattedMessage id="PasswordRecoveryForm.goToLogin" />
+        </NamedLink>
+      </p>
+      <PrimaryButton type="submit" disabled={pristine || submitting}>
+        <FormattedMessage id="PasswordRecoveryForm.sendInstructions" />
+      </PrimaryButton>
     </form>
   );
 };
 
-PasswordRecoveryForm.propTypes = { ...formPropTypes };
+PasswordRecoveryFormComponent.propTypes = {
+  ...formPropTypes,
+  intl: intlShape.isRequired,
+};
 
 const defaultFormName = 'PasswordRecoveryForm';
 
-export default reduxForm({ form: defaultFormName })(PasswordRecoveryForm);
+const PasswordRecoveryForm = compose(reduxForm({ form: defaultFormName }), injectIntl)(
+  PasswordRecoveryFormComponent
+);
+
+export default PasswordRecoveryForm;
