56244affb19e78f41117074beff2fb29eb78431c
Hannu Lyytikainen <hannu@sharetribe.com>
2017-09-07 11:34:52 +0300

    Add password forgotten link

--- src/containers/LoginForm/LoginForm.test.js
+++ src/containers/LoginForm/LoginForm.test.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import { renderDeep } from '../../util/test-helpers';
+import { RoutesProvider } from '../../components';
+import routesConfiguration from '../../routesConfiguration';
+import { flattenRoutes } from '../../util/routes';
 import LoginForm from './LoginForm';
 
 describe('LoginForm', () => {
   it('matches snapshot', () => {
-    const tree = renderDeep(<LoginForm />);
+    const flattenedRoutes = flattenRoutes(routesConfiguration);
+    const tree = renderDeep(
+      <RoutesProvider flattenedRoutes={flattenedRoutes}>
+        <LoginForm />
+      </RoutesProvider>
+    );
     expect(tree).toMatchSnapshot();
   });
 });
