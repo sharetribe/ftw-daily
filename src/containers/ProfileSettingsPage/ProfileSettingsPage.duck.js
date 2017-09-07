76cb0ac551ce177ffb4a62731d57c8081ab1475c
Vesa Luusua <vesa.luusua@gmail.com>
2017-09-08 01:23:51 +0300

    ProfileSettingsPage.duck: update profile call

--- src/containers/ProfileSettingsPage/ProfileSettingsPage.duck.js
+++ src/containers/ProfileSettingsPage/ProfileSettingsPage.duck.js
@@ -1,52 +1,79 @@
+import { updatedEntities, denormalisedEntities } from '../../util/data';
+import { currentUserShowSuccess } from '../../ducks/user.duck';
+
 const requestAction = actionType => params => ({ type: actionType, payload: { params } });
 const successAction = actionType => result => ({ type: actionType, payload: result.data });
 const errorAction = actionType => error => ({ type: actionType, payload: error, error: true });
 
 // ================ Action types ================ //
 
-export const UPLOAD_IMAGE_REQUEST = 'app/EditListingPage/UPLOAD_IMAGE_REQUEST';
-export const UPLOAD_IMAGE_SUCCESS = 'app/EditListingPage/UPLOAD_IMAGE_SUCCESS';
-export const UPLOAD_IMAGE_ERROR = 'app/EditListingPage/UPLOAD_IMAGE_ERROR';
+export const MARK_FORM_UPDATED = 'app/EditListingPage/MARK_FORM_UPDATED';
+export const CLEAR_UPDATED_FORM = 'app/EditListingPage/CLEAR_UPDATED_FORM';
 
-export const REMOVE_LISTING_IMAGE = 'app/EditListingPage/REMOVE_LISTING_IMAGE';
+export const UPLOAD_IMAGE_REQUEST = 'app/ProfileSettingsPage/UPLOAD_IMAGE_REQUEST';
+export const UPLOAD_IMAGE_SUCCESS = 'app/ProfileSettingsPage/UPLOAD_IMAGE_SUCCESS';
+export const UPLOAD_IMAGE_ERROR = 'app/ProfileSettingsPage/UPLOAD_IMAGE_ERROR';
+
+export const UPDATE_PROFILE_REQUEST = 'app/ProfileSettingsPage/UPDATE_PROFILE_REQUEST';
+export const UPDATE_PROFILE_SUCCESS = 'app/ProfileSettingsPage/UPDATE_PROFILE_SUCCESS';
+export const UPDATE_PROFILE_ERROR = 'app/ProfileSettingsPage/UPDATE_PROFILE_ERROR';
 
 // ================ Reducer ================ //
 
 const initialState = {
-  // Error instance placeholders for each endpoint
+  formUpdated: false,
+  image: null,
   uploadImageError: null,
-  image: {},
+  uploadInProgress: false,
   updateInProgress: false,
+  updateProfileError: null,
 };
 
 export default function reducer(state = initialState, action = {}) {
   const { type, payload } = action;
   switch (type) {
-
-    case UPLOAD_IMAGE_REQUEST: {
+    case UPLOAD_IMAGE_REQUEST:
       // payload.params: { id: 'tempId', file }
-      const image = {
-        [payload.params.id]: { ...payload.params },
-      };
-
       return {
         ...state,
-        image,
-        updateInProgress: true,
+        image: { ...payload.params },
+        uploadInProgress: true,
         uploadImageError: null,
       };
-    }
     case UPLOAD_IMAGE_SUCCESS: {
-      // payload.params: { id: 'tempId', imageId: 'some-real-id'}
+      // payload: { id: 'tempId', imageId: 'some-real-id'}
       const { id, imageId } = payload;
-      const file = state.image[id].file;
-      const image = { [id]: { id, imageId, file } };
-      return { ...state, image, updateInProgress: false };
+      const { file } = state.image || {};
+      const image = { id, imageId, file };
+      return { ...state, image, uploadInProgress: false };
     }
     case UPLOAD_IMAGE_ERROR: {
       // eslint-disable-next-line no-console
-      return { ...state, image: null,  updateInProgress: false , uploadImageError: payload.error };
+      return { ...state, image: null, uploadInProgress: false, uploadImageError: payload.error };
     }
+
+    case UPDATE_PROFILE_REQUEST:
+      return {
+        ...state,
+        updateInProgress: true,
+        updateProfileError: null,
+      };
+    case UPDATE_PROFILE_SUCCESS:
+      return {
+        ...state,
+        updateInProgress: false,
+      };
+    case UPDATE_PROFILE_ERROR:
+      return {
+        ...state,
+        updateInProgress: false,
+        updateProfileError: payload,
+      };
+
+    case MARK_FORM_UPDATED:
+      return { ...state, formUpdated: payload };
+    case CLEAR_UPDATED_FORM:
+      return { ...state, formUpdated: null, updateProfileError: null, uploadImageError: null };
 
     default:
       return state;
@@ -57,18 +84,32 @@ export default function reducer(state = initialState, action = {}) {
 
 // ================ Action creators ================ //
 
-// SDK method: listings.uploadImage
-export const uploadImage = requestAction(UPLOAD_IMAGE_REQUEST);
+export const markFormUpdated = tab => ({
+  type: MARK_FORM_UPDATED,
+  payload: tab,
+});
+
+export const clearUpdatedForm = () => ({
+  type: CLEAR_UPDATED_FORM,
+});
+
+// SDK method: images.uploadListingImage
+export const uploadImageRequest = requestAction(UPLOAD_IMAGE_REQUEST);
 export const uploadImageSuccess = successAction(UPLOAD_IMAGE_SUCCESS);
 export const uploadImageError = errorAction(UPLOAD_IMAGE_ERROR);
+
+// SDK method: sdk.currentUser.updateProfile
+export const updateProfileRequest = requestAction(UPDATE_PROFILE_REQUEST);
+export const updateProfileSuccess = successAction(UPDATE_PROFILE_SUCCESS);
+export const updateProfileError = errorAction(UPDATE_PROFILE_ERROR);
 
 // ================ Thunk ================ //
 
 // Images return imageId which we need to map with previously generated temporary id
-export function requestImageUpload(actionPayload) {
+export function uploadImage(actionPayload) {
   return (dispatch, getState, sdk) => {
     const id = actionPayload.id;
-    dispatch(uploadImage(actionPayload));
+    dispatch(uploadImageRequest(actionPayload));
 
     return sdk.images
       .uploadProfileImage({ image: actionPayload.file })
@@ -76,3 +117,26 @@ export function requestImageUpload(actionPayload) {
       .catch(e => dispatch(uploadImageError({ id, error: e })));
   };
 }
+
+export const updateProfile = actionPayload => {
+  return (dispatch, getState, sdk) => {
+    dispatch(updateProfileRequest());
+
+    return sdk.currentUser
+      .updateProfile(actionPayload, { expand: true, include: ['profileImage'] })
+      .then(response => {
+        dispatch(updateProfileSuccess(response));
+
+        // Temporary denormalization for profileImage
+        // Profile image will be included to users
+        const currentUserId = response.data.data.id;
+        const entities = updatedEntities({}, response.data);
+        const denormalised = denormalisedEntities(entities, 'current-user', [currentUserId]);
+        const currentUser = denormalised[0];
+
+        // Update current user in state.user.currentUser through user.duck.js
+        dispatch(currentUserShowSuccess(currentUser));
+      })
+      .catch(e => dispatch(updateProfileError(e)));
+  };
+};
