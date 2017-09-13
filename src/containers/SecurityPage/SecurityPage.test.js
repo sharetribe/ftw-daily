580ab39e176379da3b59fff8c24bd3500c3e79fe
Vesa Luusua <vesa.luusua@gmail.com>
2017-09-13 21:37:22 +0300

    Add currentUserHasOrders info to SecurityPage Verification props to SecurityPage

--- src/containers/SecurityPage/SecurityPage.test.js
+++ src/containers/SecurityPage/SecurityPage.test.js
@@ -17,6 +17,8 @@ describe('SecurityPage', () => {
         isAuthenticated={false}
         onLogout={noop}
         onManageDisableScrolling={noop}
+        sendVerificationEmailInProgress={false}
+        onResendVerificationEmail={noop}
       />
     );
     expect(tree).toMatchSnapshot();
