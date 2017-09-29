ddb02dd059f890b94ea9cced3940a67cd9c19c45
Kimmo Puputti <git@kimmo.kpuputti.fi>
2017-09-29 13:42:53 +0300

    Add test for rejected/autorejected tx transitions

--- src/components/OrderDetailsPanel/OrderDetailsPanel.test.js
+++ src/components/OrderDetailsPanel/OrderDetailsPanel.test.js
@@ -4,16 +4,54 @@ import { types } from '../../util/sdkLoader';
 import { createBooking, createListing, createUser, createTransaction } from '../../util/test-data';
 import { renderShallow } from '../../util/test-helpers';
 import { fakeIntl } from '../../util/test-data';
-import { TX_TRANSITION_PREAUTHORIZE } from '../../util/propTypes';
+import {
+  TX_TRANSITION_PREAUTHORIZE,
+  TX_TRANSITION_REJECT,
+  TX_TRANSITION_AUTO_REJECT,
+} from '../../util/propTypes';
 import { BookingBreakdown } from '../../components';
 import { OrderDetailsPanelComponent } from './OrderDetailsPanel.js';
 
 describe('OrderDetailsPanel', () => {
-  it('matches snapshot', () => {
+  it('preauthorized matches snapshot', () => {
     const { Money } = types;
     const tx = createTransaction({
       id: 'order-tx',
       lastTransition: TX_TRANSITION_PREAUTHORIZE,
+      total: new Money(16500, 'USD'),
+      booking: createBooking('booking1', {
+        start: new Date(Date.UTC(2017, 5, 10)),
+        end: new Date(Date.UTC(2017, 5, 13)),
+      }),
+      listing: createListing('listing1'),
+      provider: createUser('provider'),
+      customer: createUser('customer'),
+    });
+    const tree = renderShallow(<OrderDetailsPanelComponent transaction={tx} intl={fakeIntl} />);
+    expect(tree).toMatchSnapshot();
+  });
+  it('rejected matches snapshot', () => {
+    const { Money } = types;
+    const tx = createTransaction({
+      id: 'order-rejected',
+      lastTransition: TX_TRANSITION_REJECT,
+      total: new Money(16500, 'USD'),
+      booking: createBooking('booking1', {
+        start: new Date(Date.UTC(2017, 5, 10)),
+        end: new Date(Date.UTC(2017, 5, 13)),
+      }),
+      listing: createListing('listing1'),
+      provider: createUser('provider'),
+      customer: createUser('customer'),
+    });
+    const tree = renderShallow(<OrderDetailsPanelComponent transaction={tx} intl={fakeIntl} />);
+    expect(tree).toMatchSnapshot();
+  });
+  it('autorejected matches snapshot', () => {
+    const { Money } = types;
+    const tx = createTransaction({
+      id: 'order-autorejected',
+      lastTransition: TX_TRANSITION_AUTO_REJECT,
       total: new Money(16500, 'USD'),
       booking: createBooking('booking1', {
         start: new Date(Date.UTC(2017, 5, 10)),
--- src/components/SaleDetailsPanel/SaleDetailsPanel.test.js
+++ src/components/SaleDetailsPanel/SaleDetailsPanel.test.js
@@ -4,18 +4,74 @@ import { types } from '../../util/sdkLoader';
 import { createTransaction, createBooking, createListing, createUser } from '../../util/test-data';
 import { renderShallow } from '../../util/test-helpers';
 import { fakeIntl } from '../../util/test-data';
-import { TX_TRANSITION_PREAUTHORIZE } from '../../util/propTypes';
+import {
+  TX_TRANSITION_PREAUTHORIZE,
+  TX_TRANSITION_REJECT,
+  TX_TRANSITION_AUTO_REJECT,
+} from '../../util/propTypes';
 import { BookingBreakdown } from '../../components';
 import { SaleDetailsPanelComponent } from './SaleDetailsPanel.js';
 
 const noop = () => null;
 
 describe('SaleDetailsPanel', () => {
-  it('matches snapshot', () => {
+  it('preauthorized matches snapshot', () => {
     const { Money } = types;
     const transaction = createTransaction({
       id: 'sale-tx',
       lastTransition: TX_TRANSITION_PREAUTHORIZE,
+      total: new Money(16500, 'USD'),
+      commission: new Money(1000, 'USD'),
+      booking: createBooking('booking1', {
+        start: new Date(Date.UTC(2017, 5, 10)),
+        end: new Date(Date.UTC(2017, 5, 13)),
+      }),
+      listing: createListing('listing1'),
+      customer: createUser('customer1'),
+      lastTransitionedAt: new Date(Date.UTC(2017, 5, 10)),
+    });
+    const props = {
+      transaction,
+      onAcceptSale: noop,
+      onRejectSale: noop,
+      acceptInProgress: false,
+      rejectInProgress: false,
+      intl: fakeIntl,
+    };
+    const tree = renderShallow(<SaleDetailsPanelComponent {...props} />);
+    expect(tree).toMatchSnapshot();
+  });
+  it('rejected matches snapshot', () => {
+    const { Money } = types;
+    const transaction = createTransaction({
+      id: 'sale-tx',
+      lastTransition: TX_TRANSITION_REJECT,
+      total: new Money(16500, 'USD'),
+      commission: new Money(1000, 'USD'),
+      booking: createBooking('booking1', {
+        start: new Date(Date.UTC(2017, 5, 10)),
+        end: new Date(Date.UTC(2017, 5, 13)),
+      }),
+      listing: createListing('listing1'),
+      customer: createUser('customer1'),
+      lastTransitionedAt: new Date(Date.UTC(2017, 5, 10)),
+    });
+    const props = {
+      transaction,
+      onAcceptSale: noop,
+      onRejectSale: noop,
+      acceptInProgress: false,
+      rejectInProgress: false,
+      intl: fakeIntl,
+    };
+    const tree = renderShallow(<SaleDetailsPanelComponent {...props} />);
+    expect(tree).toMatchSnapshot();
+  });
+  it('autorejected matches snapshot', () => {
+    const { Money } = types;
+    const transaction = createTransaction({
+      id: 'sale-tx',
+      lastTransition: TX_TRANSITION_AUTO_REJECT,
       total: new Money(16500, 'USD'),
       commission: new Money(1000, 'USD'),
       booking: createBooking('booking1', {
