1bf7cc9963ea14037ebbe7f5f274bf8097b239cb
Kimmo Puputti <git@kimmo.kpuputti.fi>
2017-10-04 16:18:25 +0300

    Disable Buttons until component is mounted

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,30 +1,44 @@
-import React, { PropTypes } from 'react';
+import React, { PropTypes, Component } from 'react';
 import classNames from 'classnames';
 import { IconSpinner, IconCheckmark } from '../../components';
 
 import css from './Button.css';
 
-const Button = props => {
-  const { children, className, rootClassName, inProgress, ready, ...rest } = props;
-
-  const rootClass = rootClassName || css.root;
-  const classes = classNames(rootClass, className, {
-    [css.ready]: ready,
-    [css.inProgress]: inProgress,
-  });
-
-  let content;
-
-  if (inProgress) {
-    content = <IconSpinner rootClassName={css.spinner} />;
-  } else if (ready) {
-    content = <IconCheckmark rootClassName={css.checkmark} />;
-  } else {
-    content = children;
+class Button extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { mounted: false };
   }
+  componentDidMount() {
+    this.setState({ mounted: true }); // eslint-disable-line react/no-did-mount-set-state
+  }
+  render() {
+    const { children, className, rootClassName, inProgress, ready, disabled, ...rest } = this.props;
 
-  return <button className={classes} {...rest}>{content}</button>;
-};
+    const rootClass = rootClassName || css.root;
+    const classes = classNames(rootClass, className, {
+      [css.ready]: ready,
+      [css.inProgress]: inProgress,
+    });
+
+    let content;
+
+    if (inProgress) {
+      content = <IconSpinner rootClassName={css.spinner} />;
+    } else if (ready) {
+      content = <IconCheckmark rootClassName={css.checkmark} />;
+    } else {
+      content = children;
+    }
+
+    // All buttons are disabled until the component is mounted. This
+    // prevents e.g. being able to submit forms to the backend before
+    // the client side is handling the submit.
+    const buttonDisabled = this.state.mounted ? disabled : true;
+
+    return <button className={classes} {...rest} disabled={buttonDisabled}>{content}</button>;
+  }
+}
 
 const { node, string, bool } = PropTypes;
 
@@ -33,6 +47,7 @@ Button.defaultProps = {
   className: null,
   inProgress: false,
   ready: false,
+  disabled: false,
   children: null,
 };
 
@@ -42,6 +57,7 @@ Button.propTypes = {
 
   inProgress: bool,
   ready: bool,
+  disabled: bool,
 
   children: node,
 };
