a6c5437fd204d24cb3b1979f37663a0a69efb852
Vesa Luusua <vesa.luusua@gmail.com>
2017-10-10 20:26:44 +0300

    LoginForm.test: remove routeConfiguration and RoutesProvider

--- src/containers/LoginForm/LoginForm.test.js
+++ src/containers/LoginForm/LoginForm.test.js
@@ -1,18 +1,10 @@
 import React from 'react';
 import { renderDeep } from '../../util/test-helpers';
-import { RoutesProvider } from '../../components';
-import routeConfiguration from '../../routeConfiguration';
-import { flattenRoutes } from '../../util/routes';
 import LoginForm from './LoginForm';
 
 describe('LoginForm', () => {
   it('matches snapshot', () => {
-    const flattenedRoutes = flattenRoutes(routeConfiguration());
-    const tree = renderDeep(
-      <RoutesProvider flattenedRoutes={flattenedRoutes}>
-        <LoginForm />
-      </RoutesProvider>
-    );
+    const tree = renderDeep(<LoginForm />);
     expect(tree).toMatchSnapshot();
   });
 });
