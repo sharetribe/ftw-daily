f5f61288bebb186e97af3f689527c22bf12677a2
Vesa Luusua <vesa.luusua@gmail.com>
2017-10-11 20:14:58 +0300

    PayoutPreferencesPage uses TopbarContainer

--- src/containers/PayoutPreferencesPage/PayoutPreferencesPage.js
+++ src/containers/PayoutPreferencesPage/PayoutPreferencesPage.js
@@ -1,122 +1,48 @@
 import React, { PropTypes } from 'react';
 import { compose } from 'redux';
 import { connect } from 'react-redux';
-import { withRouter } from 'react-router-dom';
-import * as propTypes from '../../util/propTypes';
-import { sendVerificationEmail } from '../../ducks/user.duck';
-import { logout, authenticationInProgress } from '../../ducks/Auth.duck';
-import { manageDisableScrolling, isScrollingDisabled } from '../../ducks/UI.duck';
-import { Page, Topbar } from '../../components';
+import { isScrollingDisabled } from '../../ducks/UI.duck';
+import { Page } from '../../components';
+import { TopbarContainer } from '../../containers';
 
 export const PayoutPreferencesPageComponent = props => {
-  const {
-    authInfoError,
-    authInProgress,
-    currentUser,
-    currentUserHasListings,
-    currentUserHasOrders,
-    history,
-    isAuthenticated,
-    location,
-    logoutError,
-    notificationCount,
-    onLogout,
-    onManageDisableScrolling,
-    sendVerificationEmailInProgress,
-    sendVerificationEmailError,
-    onResendVerificationEmail,
-  } = props;
+  const { authInfoError, logoutError, scrollingDisabled } = props;
 
   return (
-    <Page authInfoError={authInfoError} logoutError={logoutError} title="Payout preferences">
-      <Topbar
-        authInProgress={authInProgress}
-        currentUser={currentUser}
-        currentUserHasListings={currentUserHasListings}
-        currentUserHasOrders={currentUserHasOrders}
-        history={history}
-        isAuthenticated={isAuthenticated}
-        location={location}
-        notificationCount={notificationCount}
-        onLogout={onLogout}
-        onManageDisableScrolling={onManageDisableScrolling}
-        onResendVerificationEmail={onResendVerificationEmail}
-        sendVerificationEmailInProgress={sendVerificationEmailInProgress}
-        sendVerificationEmailError={sendVerificationEmailError}
-      />
+    <Page
+      authInfoError={authInfoError}
+      logoutError={logoutError}
+      title="Payout preferences"
+      scrollingDisabled={scrollingDisabled}
+    >
+      <TopbarContainer />
     </Page>
   );
 };
 
 PayoutPreferencesPageComponent.defaultProps = {
   authInfoError: null,
-  currentUser: null,
-  currentUserHasOrders: null,
   logoutError: null,
-  notificationCount: 0,
-  sendVerificationEmailError: null,
 };
 
-const { bool, func, instanceOf, number, object, shape } = PropTypes;
+const { bool, instanceOf } = PropTypes;
 
 PayoutPreferencesPageComponent.propTypes = {
   authInfoError: instanceOf(Error),
-  authInProgress: bool.isRequired,
-  currentUser: propTypes.currentUser,
-  currentUserHasListings: bool.isRequired,
-  currentUserHasOrders: bool,
-  isAuthenticated: bool.isRequired,
   logoutError: instanceOf(Error),
-  notificationCount: number,
-  onLogout: func.isRequired,
-  onManageDisableScrolling: func.isRequired,
-  sendVerificationEmailInProgress: bool.isRequired,
-  sendVerificationEmailError: instanceOf(Error),
-  onResendVerificationEmail: func.isRequired,
-
-  // from withRouter
-  history: shape({
-    push: func.isRequired,
-  }).isRequired,
-  location: shape({ state: object }).isRequired,
+  scrollingDisabled: bool.isRequired,
 };
 
 const mapStateToProps = state => {
-  // Page needs authInfoError and logoutError, Topbar needs isAuthenticated
-  const { authInfoError, isAuthenticated, logoutError } = state.Auth;
-  // Topbar needs user info.
-  const {
-    currentUser,
-    currentUserHasListings,
-    currentUserHasOrders,
-    currentUserNotificationCount: notificationCount,
-    sendVerificationEmailInProgress,
-    sendVerificationEmailError,
-  } = state.user;
+  // Page needs authInfoError and logoutError
+  const { authInfoError, logoutError } = state.Auth;
   return {
     authInfoError,
-    authInProgress: authenticationInProgress(state),
-    currentUser,
-    currentUserHasListings,
-    currentUserHasOrders,
-    notificationCount,
-    isAuthenticated,
     logoutError,
     scrollingDisabled: isScrollingDisabled(state),
-    sendVerificationEmailInProgress,
-    sendVerificationEmailError,
   };
 };
 
-const mapDispatchToProps = dispatch => ({
-  onLogout: historyPush => dispatch(logout(historyPush)),
-  onManageDisableScrolling: (componentId, disableScrolling) =>
-    dispatch(manageDisableScrolling(componentId, disableScrolling)),
-  onResendVerificationEmail: () => dispatch(sendVerificationEmail()),
-});
-
-const PayoutPreferencesPage = compose(connect(mapStateToProps, mapDispatchToProps), withRouter)(
-  PayoutPreferencesPageComponent
-);
+const PayoutPreferencesPage = compose(connect(mapStateToProps))(PayoutPreferencesPageComponent);
 
 export default PayoutPreferencesPage;
