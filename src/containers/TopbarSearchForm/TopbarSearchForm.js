2beb0ea5703f86873adb2d25fc095a634fb1b9a1
Hannu Lyytikainen <hannu@sharetribe.com>
2017-10-12 14:20:12 +0300

    Hide software keyboard in topbar search

--- src/containers/TopbarSearchForm/TopbarSearchForm.js
+++ src/containers/TopbarSearchForm/TopbarSearchForm.js
@@ -1,4 +1,4 @@
-import React, { PropTypes } from 'react'; // eslint-disable-line react/no-deprecated
+import React, { Component, PropTypes } from 'react'; // eslint-disable-line react/no-deprecated
 import { compose } from 'redux';
 import { Field, reduxForm, propTypes as formPropTypes } from 'redux-form';
 import { intlShape, injectIntl } from 'react-intl';
@@ -7,43 +7,58 @@ import { Form, LocationAutocompleteInput } from '../../components';
 
 import css from './TopbarSearchForm.css';
 
-const TopbarSearchFormComponent = props => {
-  const { rootClassName, className, desktopInputRoot, intl, onSubmit, isMobile } = props;
+class TopbarSearchFormComponent extends Component {
+  constructor(props) {
+    super(props);
+    this.onChange = this.onChange.bind(this);
+    this.searchInput = null;
+  }
 
-  const onChange = location => {
+  onChange(location) {
     if (location.selectedPlace) {
       // Note that we use `onSubmit` instead of the conventional
       // `handleSubmit` prop for submitting. We want to autosubmit
       // when a place is selected, and don't require any extra
       // validations for the form.
-      onSubmit({ location });
+      this.props.onSubmit({ location });
+      // blur search input to hide software keyboard
+      if (this.searchInput) {
+        this.searchInput.blur();
+      }
     }
-  };
+  }
 
-  const classes = classNames(rootClassName, className);
-  const desktopInputRootClass = desktopInputRoot || css.desktopInputRoot;
+  render() {
+    const { rootClassName, className, desktopInputRoot, intl, isMobile } = this.props;
 
-  // Allow form submit only when the place has changed
-  const preventFormSubmit = e => e.preventDefault();
+    const classes = classNames(rootClassName, className);
+    const desktopInputRootClass = desktopInputRoot || css.desktopInputRoot;
 
-  return (
-    <Form className={classes} onSubmit={preventFormSubmit}>
-      <Field
-        name="location"
-        label="Location"
-        className={isMobile ? css.mobileInputRoot : desktopInputRootClass}
-        iconClassName={isMobile ? css.mobileIcon : css.desktopIcon}
-        inputClassName={isMobile ? css.mobileInput : css.desktopInput}
-        predictionsClassName={isMobile ? css.mobilePredictions : css.desktopPredictions}
-        placeholder={intl.formatMessage({ id: 'TopbarSearchForm.placeholder' })}
-        format={null}
-        component={LocationAutocompleteInput}
-        closeOnBlur={!isMobile}
-        onChange={onChange}
-      />
-    </Form>
-  );
-};
+    // Allow form submit only when the place has changed
+    const preventFormSubmit = e => e.preventDefault();
+
+    return (
+      <Form className={classes} onSubmit={preventFormSubmit}>
+        <Field
+          name="location"
+          label="Location"
+          className={isMobile ? css.mobileInputRoot : desktopInputRootClass}
+          iconClassName={isMobile ? css.mobileIcon : css.desktopIcon}
+          inputClassName={isMobile ? css.mobileInput : css.desktopInput}
+          predictionsClassName={isMobile ? css.mobilePredictions : css.desktopPredictions}
+          placeholder={intl.formatMessage({ id: 'TopbarSearchForm.placeholder' })}
+          format={null}
+          component={LocationAutocompleteInput}
+          closeOnBlur={!isMobile}
+          onChange={this.onChange}
+          inputRef={node => {
+            this.searchInput = node;
+          }}
+        />
+      </Form>
+    );
+  }
+}
 
 const { func, string, bool } = PropTypes;
 
