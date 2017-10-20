4eb449715d2a0a81fae140c0513dd93c616967e4
Vesa Luusua <vesa.luusua@gmail.com>
2017-10-20 15:15:15 +0300

    Footer content

--- src/config.js
+++ src/config.js
@@ -206,7 +206,7 @@ const siteTwitterHandle = '@sharetribe';
 const siteInstagramPage = null;
 
 // Facebook page is used in SEO schema (http://schema.org/Organization)
-const siteFacebookPage = null;
+const siteFacebookPage = 'https://www.facebook.com/Sharetribe/';
 
 // Facebook counts shares with app or page associated by this id
 // Currently it is unset, but you can read more about fb:app_id from
