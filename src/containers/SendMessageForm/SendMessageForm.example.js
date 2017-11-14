ff108ca489dd8d7c403e1ac32c1fca9f6d95207a
Kimmo Puputti <git@kimmo.kpuputti.fi>
2017-11-14 14:33:51 +0200

    Add in progress and error states

--- src/containers/SendMessageForm/SendMessageForm.example.js
+++ src/containers/SendMessageForm/SendMessageForm.example.js
@@ -3,6 +3,7 @@ import SendMessageForm from './SendMessageForm';
 export const Empty = {
   component: SendMessageForm,
   props: {
+    form: 'Styleguide_SendMessageForm_Empty',
     messagePlaceholder: 'Send message to Juho…',
     onChange: values => {
       console.log('values changed to:', values);
@@ -16,6 +17,26 @@ export const Empty = {
     onBlur: () => {
       console.log('blur on message form');
     },
+  },
+  group: 'forms',
+};
+
+export const InProgress = {
+  component: SendMessageForm,
+  props: {
+    form: 'Styleguide_SendMessageForm_InProgress',
+    messagePlaceholder: 'Send message to Juho…',
+    inProgress: true,
+  },
+  group: 'forms',
+};
+
+export const Error = {
+  component: SendMessageForm,
+  props: {
+    form: 'Styleguide_SendMessageForm_Error',
+    messagePlaceholder: 'Send message to Juho…',
+    sendMessageError: { type: 'error', name: 'ExampleError' },
   },
   group: 'forms',
 };
