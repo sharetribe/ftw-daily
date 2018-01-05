6188f3b82613d773eb5e132bab76d82a7d7c9298
Hannu Lyytikainen <hannu@sharetribe.com>
2018-01-05 09:53:00 +0200

    Change SelectSingleCustomAttribute into a class
    Also pass controlling functions to the menu.

--- src/components/SelectSingleCustomAttribute/SelectSingleCustomAttribute.js
+++ src/components/SelectSingleCustomAttribute/SelectSingleCustomAttribute.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { FormattedMessage, injectIntl, intlShape } from 'react-intl';
 import classNames from 'classnames';
@@ -9,62 +9,81 @@ import { stringify } from '../../util/urlHelpers';
 import { Menu, MenuContent, MenuItem, MenuLabel, NamedLink } from '../../components';
 import css from './SelectSingleCustomAttribute.css';
 
-const SelectSingleCustomAttributeComponent = props => {
-  const { rootClassName, className, customAttribute, urlQueryParams, intl } = props;
+class SelectSingleCustomAttributeComponent extends Component {
+  constructor(props) {
+    super(props);
 
-  const ca = customAttribute && config.customAttributes[customAttribute];
+    this.state = { isOpen: false };
+    this.onToggleActive = this.onToggleActive.bind(this);
+  }
 
-  // name of the corresponding query parameter
-  const caParam = `ca_${customAttribute}`;
-  // current value of this custom attribute filter
-  const currentValue = urlQueryParams[caParam];
-  // query params where this custom attribute is cleared
-  const clearUrlQueryParams = omit(urlQueryParams, caParam);
-  // query string for clearing this custom attribute
-  const clearQueryString = stringify(clearUrlQueryParams);
+  onToggleActive(isOpen) {
+    this.setState({ isOpen: isOpen });
+  }
 
-  // resolve menu label text and class
-  const menuLabel = currentValue
-    ? intl.formatMessage({ id: `SelectSingleCustomAttribute.category.option.${currentValue}` })
-    : intl.formatMessage({ id: `SelectSingleCustomAttribute.${customAttribute}.label` });
-  const menuLabelClass = currentValue ? css.menuLabelSelected : css.menuLabel;
+  render() {
+    const { rootClassName, className, customAttribute, urlQueryParams, intl } = this.props;
 
-  const classes = classNames(rootClassName || css.root, className);
+    const ca = customAttribute && config.customAttributes[customAttribute];
 
-  return (
-    <Menu className={classes} useArrow={false} contentPlacementOffset={-14}>
-      <MenuLabel className={menuLabelClass}>{menuLabel}</MenuLabel>
-      <MenuContent className={css.menuContent}>
-        {ca.values.map(v => {
-          // check if this option is selected
-          const selected = currentValue === v;
-          // search query string with this option added
-          const queryString = stringify({ ...urlQueryParams, [caParam]: v });
-          // menu item border class
-          const menuItemBorderClass = selected ? css.menuItemBorderSelected : css.menuItemBorder;
+    // name of the corresponding query parameter
+    const caParam = `ca_${customAttribute}`;
+    // current value of this custom attribute filter
+    const currentValue = urlQueryParams[caParam];
+    // query params where this custom attribute is cleared
+    const clearUrlQueryParams = omit(urlQueryParams, caParam);
+    // query string for clearing this custom attribute
+    const clearQueryString = stringify(clearUrlQueryParams);
 
-          return (
-            <MenuItem key={v}>
-              <NamedLink className={css.menuItem} name="SearchPage" to={{ search: queryString }}>
-                <span className={menuItemBorderClass} />
-                <FormattedMessage id={`SelectSingleCustomAttribute.category.option.${v}`} />
-              </NamedLink>
-            </MenuItem>
-          );
-        })}
-        <MenuItem key={'clearLink'}>
-          <NamedLink
-            className={css.clearMenuItem}
-            name="SearchPage"
-            to={{ search: clearQueryString }}
-          >
-            <FormattedMessage id={'SelectSingleCustomAttribute.clear'} />
-          </NamedLink>
-        </MenuItem>
-      </MenuContent>
-    </Menu>
-  );
-};
+    // resolve menu label text and class
+    const menuLabel = currentValue
+      ? intl.formatMessage({ id: `SelectSingleCustomAttribute.category.option.${currentValue}` })
+      : intl.formatMessage({ id: `SelectSingleCustomAttribute.${customAttribute}.label` });
+    const menuLabelClass = currentValue ? css.menuLabelSelected : css.menuLabel;
+
+    const classes = classNames(rootClassName || css.root, className);
+
+    return (
+      <Menu
+        className={classes}
+        useArrow={false}
+        contentPlacementOffset={-14}
+        onToggleActive={this.onToggleActive}
+        isOpen={this.state.isOpen}
+      >
+        <MenuLabel className={menuLabelClass}>{menuLabel}</MenuLabel>
+        <MenuContent className={css.menuContent}>
+          {ca.values.map(v => {
+            // check if this option is selected
+            const selected = currentValue === v;
+            // search query string with this option added
+            const queryString = stringify({ ...urlQueryParams, [caParam]: v });
+            // menu item border class
+            const menuItemBorderClass = selected ? css.menuItemBorderSelected : css.menuItemBorder;
+
+            return (
+              <MenuItem key={v}>
+                <NamedLink className={css.menuItem} name="SearchPage" to={{ search: queryString }}>
+                  <span className={menuItemBorderClass} />
+                  <FormattedMessage id={`SelectSingleCustomAttribute.category.option.${v}`} />
+                </NamedLink>
+              </MenuItem>
+            );
+          })}
+          <MenuItem key={'clearLink'}>
+            <NamedLink
+              className={css.clearMenuItem}
+              name="SearchPage"
+              to={{ search: clearQueryString }}
+            >
+              <FormattedMessage id={'SelectSingleCustomAttribute.clear'} />
+            </NamedLink>
+          </MenuItem>
+        </MenuContent>
+      </Menu>
+    );
+  }
+}
 
 const { object, string } = PropTypes;
 
