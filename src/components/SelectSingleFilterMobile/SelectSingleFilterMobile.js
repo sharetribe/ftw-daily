5cc6ecc9739c8c9d8a1c74ba9f70b22d671bc1f1
Hannu Lyytikainen <hannu@sharetribe.com>
2018-01-12 00:01:00 +0200

    Add filter options visibility toggling

--- src/components/SelectSingleFilterMobile/SelectSingleFilterMobile.js
+++ src/components/SelectSingleFilterMobile/SelectSingleFilterMobile.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import { FormattedMessage, injectIntl, intlShape } from 'react-intl';
@@ -6,49 +6,76 @@ import { FormattedMessage, injectIntl, intlShape } from 'react-intl';
 import config from '../../config';
 import css from './SelectSingleFilterMobile.css';
 
-const SelectSingleFilterMobileComponent = props => {
-  const { rootClassName, className, customAttribute, urlQueryParams, onSelect, intl } = props;
+class SelectSingleFilterMobileComponent extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { isOpen: true };
+  }
 
-  const selectOption = option => {
-    onSelect(customAttribute, option);
-  };
+  render() {
+    const {
+      rootClassName,
+      className,
+      customAttribute,
+      urlQueryParams,
+      onSelect,
+      intl,
+    } = this.props;
 
-  const filterLabel = intl.formatMessage({
-    id: `SelectSingleFilterMobile.${customAttribute}.label`,
-  });
-  // custom attribute content
-  const ca = customAttribute && config.customAttributes[customAttribute];
-  // name of the corresponding query parameter
-  const caParam = `ca_${customAttribute}`;
-  // current value of this custom attribute filter
-  const currentValue = urlQueryParams[caParam];
+    const selectOption = option => {
+      onSelect(customAttribute, option);
+    };
 
-  const labelClass = currentValue ? css.filterLabelSelected : css.filterLabel;
+    const toggleIsOpen = () => {
+      this.setState({ isOpen: !this.state.isOpen });
+    };
 
-  const classes = classNames(rootClassName || css.root, className);
-  return (
-    <div className={classes}>
-      <div className={labelClass}>
-        <span>{filterLabel}</span>
-        <button className={css.clearButton} onClick={() => selectOption(null)}>
-          <FormattedMessage id={'SelectSingleFilterMobile.clear'} />
-        </button>
-      </div>
-      {ca.values.map(v => {
-        // check if this option is selected
-        const selected = currentValue === v;
-        // menu item border class
-        const optionBorderClass = selected ? css.optionBorderSelected : css.optionBorder;
-        return (
-          <button key={v} className={css.option} onClick={() => selectOption(v)}>
-            <span className={optionBorderClass} />
-            <FormattedMessage id={`SelectSingleFilterMobile.category.option.${v}`} />
+    const filterLabel = intl.formatMessage({
+      id: `SelectSingleFilterMobile.${customAttribute}.label`,
+    });
+    // custom attribute content
+    const ca = customAttribute && config.customAttributes[customAttribute];
+    // name of the corresponding query parameter
+    const caParam = `ca_${customAttribute}`;
+    // current value of this custom attribute filter
+    const currentValue = urlQueryParams[caParam];
+
+    const labelClass = currentValue ? css.filterLabelSelected : css.filterLabel;
+
+    const optionsContainerClass = this.state.isOpen
+      ? css.optionsContainerOpen
+      : css.optionsContainerClosed;
+
+    const classes = classNames(rootClassName || css.root, className);
+
+    return (
+      <div className={classes}>
+        <div className={labelClass}>
+          <button className={css.labelButton} onClick={toggleIsOpen}>
+            <span className={labelClass}>{filterLabel}</span>
           </button>
-        );
-      })}
-    </div>
-  );
-};
+          <button className={css.clearButton} onClick={() => selectOption(null)}>
+            <FormattedMessage id={'SelectSingleFilterMobile.clear'} />
+          </button>
+        </div>
+        <div className={optionsContainerClass}>
+          {ca.values.map(v => {
+            // check if this option is selected
+            const selected = currentValue === v;
+            // menu item border class
+            const optionBorderClass = selected ? css.optionBorderSelected : css.optionBorder;
+            return (
+              <button key={v} className={css.option} onClick={() => selectOption(v)}>
+                <span className={optionBorderClass} />
+                <FormattedMessage id={`SelectSingleFilterMobile.category.option.${v}`} />
+              </button>
+            );
+          })}
+        </div>
+      </div>
+    );
+  }
+}
 
 const { object, string, func } = PropTypes;
 
