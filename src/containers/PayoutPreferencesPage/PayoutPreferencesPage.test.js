4c942dafd360a587ca18337bc59a6ae3be78d2d0
Kimmo Puputti <git@kimmo.kpuputti.fi>
2018-02-27 12:03:00 +0200

    Fix tests

--- src/containers/PayoutPreferencesPage/PayoutPreferencesPage.test.js
+++ src/containers/PayoutPreferencesPage/PayoutPreferencesPage.test.js
@@ -3,6 +3,8 @@ import { renderShallow } from '../../util/test-helpers';
 import { fakeIntl, createCurrentUser } from '../../util/test-data';
 import { PayoutPreferencesPageComponent } from './PayoutPreferencesPage';
 
+const noop = () => null;
+
 describe('PayoutPreferencesPage', () => {
   it('matches snapshot with Stripe not connected', () => {
     const currentUser = createCurrentUser('stripe-not-connected', {
@@ -13,6 +15,10 @@ describe('PayoutPreferencesPage', () => {
       <PayoutPreferencesPageComponent
         currentUser={currentUser}
         scrollingDisabled={false}
+        fetchInProgress={false}
+        payoutDetailsSaved={false}
+        onPayoutDetailsFormChange={noop}
+        onPayoutDetailsFormSubmit={noop}
         intl={fakeIntl}
       />
     );
@@ -25,6 +31,26 @@ describe('PayoutPreferencesPage', () => {
       <PayoutPreferencesPageComponent
         currentUser={currentUser}
         scrollingDisabled={false}
+        fetchInProgress={false}
+        payoutDetailsSaved={false}
+        onPayoutDetailsFormChange={noop}
+        onPayoutDetailsFormSubmit={noop}
+        intl={fakeIntl}
+      />
+    );
+    expect(tree).toMatchSnapshot();
+  });
+  it('matches snapshot with details submitted', () => {
+    const currentUser = createCurrentUser('stripe-connected');
+    expect(currentUser.attributes.stripeConnected).toEqual(true);
+    const tree = renderShallow(
+      <PayoutPreferencesPageComponent
+        currentUser={currentUser}
+        scrollingDisabled={false}
+        fetchInProgress={false}
+        payoutDetailsSaved={true}
+        onPayoutDetailsFormChange={noop}
+        onPayoutDetailsFormSubmit={noop}
         intl={fakeIntl}
       />
     );
