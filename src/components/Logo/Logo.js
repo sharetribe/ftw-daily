e9a45042e4c04e18afed823fa81b29f6cc57812e
Hannu Lyytikainen <hannu@sharetribe.com>
2018-03-27 13:05:18 +0300

    Define svg colors in css

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import config from '../../config';
+import classNames from 'classnames';
 
+import config from '../../config';
 import IconLogo from './IconLogo';
 import LogoImage from './saunatime-logo.png';
+import css from './Logo.css';
 
 const Logo = props => {
   const { className, format, ...rest } = props;
+  const mobileClasses = classNames(css.logoMobile, className);
 
   if (format === 'desktop') {
     return <img className={className} src={LogoImage} alt={config.siteTitle} {...rest} />;
   }
 
-  return <IconLogo className={className} {...rest} />;
+  return <IconLogo className={mobileClasses} {...rest} />;
 };
 
 const { oneOf, string } = PropTypes;
