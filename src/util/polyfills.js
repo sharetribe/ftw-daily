265aa7b6f7caabecdbd65356e4aaf35b54141c38
Vesa Luusua <vesa.luusua@gmail.com>
2018-04-03 16:45:59 +0300

    Add polyfills

--- src/util/polyfills.js
+++ src/util/polyfills.js
@@ -10,3 +10,11 @@ require('array-includes').shim();
 
 // [].find
 require('array.prototype.find').shim();
+
+// Object.entries
+require('object.entries').shim();
+
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number/parseFloat
+if (typeof Number.parseFloat === 'undefined' && typeof window !== 'undefined') {
+  Number.parseFloat = window.parseFloat;
+}
