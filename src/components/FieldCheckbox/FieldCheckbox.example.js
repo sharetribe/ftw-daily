16cb103ad5e121039fe8794350fb9f2c3dd36dbc
Hannu Lyytikainen <hannu@sharetribe.com>
2018-04-06 14:50:29 +0300

    Refactor FieldCheckbox
    Wrap the whole component in redux-form Field instead of just using Field
    for the actual input element.

--- src/components/FieldCheckbox/FieldCheckbox.example.js
+++ src/components/FieldCheckbox/FieldCheckbox.example.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { reduxForm, propTypes as formPropTypes } from 'redux-form';
 import { Button } from '../../components';
 import FieldCheckbox from './FieldCheckbox';
+import { required } from '../../util/validators';
 
 const formName = 'Styleguide.FieldCheckbox.Form';
 
@@ -12,10 +13,13 @@ const FormComponent = props => {
 
   return (
     <form onSubmit={handleSubmit}>
+      <FieldCheckbox id="checkbox-id" name="checkbox-name" label="Check here, optional" />
+
       <FieldCheckbox
-        id="field-checkbox-example-id"
-        name="field-checkbox-example-name"
-        label="Check here"
+        id="checkbox-required-id"
+        name="checkbox-required-name"
+        label="Check here, required"
+        validate={required('This field is required')}
       />
 
       <Button style={{ marginTop: 24 }} type="submit" disabled={submitDisabled}>
