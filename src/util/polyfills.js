63afe46b904887c1620192df9b3d776cfbf3a4d3
Vesa Luusua <vesa.luusua@gmail.com>
2018-05-17 14:20:51 +0300

    Add missing polyfills: Number.isNaN and Number.parseInt

--- src/util/polyfills.js
+++ src/util/polyfills.js
@@ -21,3 +21,15 @@ require('object.values').shim();
 if (typeof Number.parseFloat === 'undefined' && typeof window !== 'undefined') {
   Number.parseFloat = window.parseFloat;
 }
+
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number/parseInt
+if (typeof Number.parseInt === 'undefined' && typeof window !== 'undefined') {
+  Number.parseInt = window.parseInt;
+}
+
+// NaN is the only value in javascript which is not equal to itself.
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number/isNaN
+if (typeof Number.isNaN === 'undefined') {
+  // eslint-disable-next-line no-self-compare
+  Number.isNaN = value => value !== value;
+}
