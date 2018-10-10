462a496bc6e8f83a385cdecec4ca0a3875e107bc
Janne Koivistoinen <janne@sharetribe.com>
2018-10-10 16:44:18 +0300

    Adds Overlay component to ManageListingCard

---
+++ src/components/ManageListingCard/Overlay.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { node, string } from 'prop-types';
+import classNames from 'classnames';
+
+import css from './Overlay.css';
+
+const Overlay = props => {
+  const { className, rootClassName, message, errorMessage, children } = props;
+
+  const classes = classNames(rootClassName || css.root, className);
+
+  return (
+    <div
+      className={classes}
+      onClick={event => {
+        event.preventDefault();
+        event.stopPropagation();
+      }}
+    >
+      <div className={css.overlay} />
+      <div className={css.overlayContent}>
+        {errorMessage ? <div className={css.errorMessage}>{errorMessage}</div> : null}
+        {message ? <div className={css.message}>{message}</div> : null}
+        {children}
+      </div>
+    </div>
+  );
+};
+
+Overlay.defaultProps = {
+  className: null,
+  rootClassName: null,
+  message: null,
+  errorMessage: null,
+  children: null,
+};
+
+Overlay.propTypes = {
+  className: string,
+  rootClassName: string,
+  message: string,
+  errorMessage: string,
+  children: node,
+};
+
+export default Overlay;
