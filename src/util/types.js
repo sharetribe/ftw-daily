3b834e98d058d6fd9b257f937c79579bcf376d03
Vesa Luusua <vesa.luusua@gmail.com>
2018-11-06 11:42:58 +0200

    Add LISTING_STATE_DRAFT to types.js

--- src/util/types.js
+++ src/util/types.js
@@ -124,11 +124,13 @@ propTypes.user = shape({
   profileImage: propTypes.image,
 });
 
+export const LISTING_STATE_DRAFT = 'draft';
 export const LISTING_STATE_PENDING_APPROVAL = 'pendingApproval';
 export const LISTING_STATE_PUBLISHED = 'published';
 export const LISTING_STATE_CLOSED = 'closed';
 
 const LISTING_STATES = [
+  LISTING_STATE_DRAFT,
   LISTING_STATE_PENDING_APPROVAL,
   LISTING_STATE_PUBLISHED,
   LISTING_STATE_CLOSED,
