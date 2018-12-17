fd4d258d05fbc8c5ee850b726588372ae601c985
Jenni Nurmi <jenni@sharetribe.com>
2018-12-17 09:59:04 +0200

    Add example for not required radio button field

--- src/components/FieldRadioButton/FieldRadioButton.example.js
+++ src/components/FieldRadioButton/FieldRadioButton.example.js
@@ -9,11 +9,17 @@ const FormComponent = props => (
     {...props}
     form={formName}
     render={fieldRenderProps => {
-      const { handleSubmit, onChange, invalid, pristine, submitting } = fieldRenderProps;
+      const {
+        handleSubmit,
+        onChange,
+        invalid,
+        pristine,
+        submitting,
+        required,
+        id,
+      } = fieldRenderProps;
 
       const submitDisabled = invalid || pristine || submitting;
-
-      const required = true;
 
       const showAsRequired = pristine && required;
 
@@ -26,21 +32,21 @@ const FormComponent = props => (
         >
           <FormSpy onChange={onChange} />
           <FieldRadioButton
-            id="option-id1"
+            id={`${id}-option-id1`}
             name="option-group"
             label="option 1"
             value="option1"
             showAsRequired={showAsRequired}
           />
           <FieldRadioButton
-            id="option-id2"
+            id={`${id}-option-id2`}
             name="option-group"
             label="option 2"
             value="option2"
             showAsRequired={showAsRequired}
           />
           <FieldRadioButton
-            id="option-id3"
+            id={`${id}-option-id3`}
             name="option-group"
             label="option 3"
             value="option3"
@@ -67,6 +73,25 @@ export const RadioButtonRequired = {
     onSubmit: values => {
       console.log('Submit values of FieldRadioButton: ', values);
     },
+    required: true,
+    id: 'radiobutton',
+  },
+  group: 'inputs',
+};
+
+export const RadioButtonNotRequired = {
+  component: FormComponent,
+  props: {
+    onChange: formState => {
+      if (Object.keys(formState.values).length > 0) {
+        console.log('form values changed to:', formState.values);
+      }
+    },
+    onSubmit: values => {
+      console.log('Submit values of FieldRadioButton: ', values);
+    },
+    required: false,
+    id: 'radiobutton2',
   },
   group: 'inputs',
 };
