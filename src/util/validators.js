38fc769539327ff145d73866249cd0ce7dab73fe
Vesa Luusua <vesa.luusua@gmail.com>
2019-03-13 15:33:55 +0200

    Validator for businessURL

--- src/util/validators.js
+++ src/util/validators.js
@@ -136,6 +136,24 @@ export const ageAtLeast = (message, minYears) => value => {
   return message;
 };
 
+export const validBusinessURL = message => value => {
+  if (typeof value === 'undefined' || value === null) {
+    return message;
+  }
+
+  const disallowedChars = /[^-A-Za-z0-9+&@#/%?=~_|!:,.;()]/;
+  const protocolTokens = value.split(':');
+  const includesProtocol = protocolTokens.length > 1;
+  const usesHttpProtocol = includesProtocol && !!protocolTokens[0].match(/^(https?)/);
+
+  const invalidCharacters = !!value.match(disallowedChars);
+  const invalidProtocol = !(usesHttpProtocol || !includesProtocol);
+  // Stripe checks against example.com
+  const isExampleDotCom = !!value.match(/^(https?:\/\/example\.com|example\.com)/);
+  const isLocalhost = !!value.match(/^(https?:\/\/localhost($|:|\/)|localhost($|:|\/))/);
+  return invalidCharacters || invalidProtocol || isExampleDotCom || isLocalhost ? message : VALID;
+};
+
 export const validSsnLast4 = message => value => {
   return value.length === 4 ? VALID : message;
 };
