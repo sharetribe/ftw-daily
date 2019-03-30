2a2185b07898de7b110e190d7ae9ac761925504e
lastlink <lastlink@users.noreply.github.com>
2019-03-30 04:47:56 -0400

    new .env parameters
    bookingProcessAlias & bookingUnitType as .env parameters

--- src/config.js
+++ src/config.js
@@ -29,7 +29,7 @@ const sortSearchByDistance = false;
 //
 // In a way, 'processAlias' defines which transaction process (or processes)
 // this particular web application is able to handle.
-const bookingProcessAlias = 'preauth-with-nightly-booking/release-1';
+const bookingProcessAlias = process.env.REACT_PROCESS_ALIAS || "preauth-with-nightly-booking/release-1";
 
 // The transaction line item code for the main unit type in bookings.
 //
@@ -37,7 +37,7 @@ const bookingProcessAlias = 'preauth-with-nightly-booking/release-1';
 //
 // Note: translations will use different translation keys for night, day or unit
 // depending on the value chosen.
-const bookingUnitType = 'line-item/night';
+const bookingUnitType = process.env.REACT_APP_BOOKING_UNIT_TYPE || "line-item/night";
 
 // Should the application fetch available time slots (currently defined as
 // start and end dates) to be shown on listing page.
