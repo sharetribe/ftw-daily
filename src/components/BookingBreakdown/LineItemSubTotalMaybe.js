551dff00ce549b0b6f117bf66d3999817db78431
Hannu Lyytikainen <hannu@sharetribe.com>
2019-04-04 17:18:56 +0300

    Change booking breakdown layout
    Make the subtotal line stick out a bit more.

--- src/components/BookingBreakdown/LineItemSubTotalMaybe.js
+++ src/components/BookingBreakdown/LineItemSubTotalMaybe.js
@@ -81,7 +81,7 @@ const LineItemSubTotalMaybe = props => {
   const formattedSubTotal = subTotalLineItems.length > 0 ? formatMoney(intl, subTotal) : null;
 
   return formattedSubTotal && showSubTotal ? (
-    <div className={css.lineItem}>
+    <div className={css.subTotalLineItem}>
       <span className={css.itemLabel}>
         <FormattedMessage id="BookingBreakdown.subTotal" />
       </span>
