26e82bcc5054e2d50de1bf7d6f4880fe780afd91
Vesa Luusua <vesa.luusua@gmail.com>
2019-04-12 13:25:12 +0300

    SearchPage: use sparse fields when querying listing data

--- src/containers/SearchPage/SearchPage.js
+++ src/containers/SearchPage/SearchPage.js
@@ -366,6 +366,8 @@ SearchPage.loadData = (params, search) => {
     page,
     perPage: RESULT_PAGE_SIZE,
     include: ['author', 'images'],
+    'fields.listing': ['title', 'geolocation', 'price'],
+    'fields.user': ['profile.displayName', 'profile.abbreviatedName'],
     'fields.image': ['variants.landscape-crop', 'variants.landscape-crop2x'],
     'limit.images': 1,
   });
