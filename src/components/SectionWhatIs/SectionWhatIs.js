40ff535edc3ba85ef9facb7751fde61308049a8e
Paulo Henriques <paulo.henriques1993@gmail.com>
2019-05-18 13:12:26 +0100

    Adjust on Home Page

--- src/components/SectionWhatIs/SectionWhatIs.js
+++ src/components/SectionWhatIs/SectionWhatIs.js
@@ -14,10 +14,10 @@ const SectionWhatIs = props => {
   return (
     <div className={classes}>
       <div className={css.title}>
-        <FormattedMessage id="SectionHowItWorks.titleLineOne" />
+        <FormattedMessage id="SectionWhatIs.title" />
       </div>
       <p>
-        <FormattedMessage id="Plataforma online que te permite reservar o acesso a uma piscina durante uma manhã, tarde ou dia inteiro. Mergulha na nossa plataforma e encontra a piscina indicada para um dia bem passado, seja ela localizada numa casa particular ou num estabelecimento turístico. O conceito assemelha-se muito às plataformas de reserva de casas e hotéis. Neste caso, em vez da reserva de quartos/casas, poderás reservar o acesso a uma piscina." />
+        <FormattedMessage id="SectionWhatIs.text" />
       </p>
       <div className={css.createListingLink}>
         <NamedLink name="NewListingPage">
