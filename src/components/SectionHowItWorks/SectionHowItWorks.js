b88c049c35a38aef3ffe2c197333c8d8174e1e12
Paulo Henriques <paulo.henriques1993@gmail.com>
2019-05-18 12:40:17 +0100

    Add icons in How To

--- src/components/SectionHowItWorks/SectionHowItWorks.js
+++ src/components/SectionHowItWorks/SectionHowItWorks.js
@@ -1,14 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { FormattedMessage } from 'react-intl';
 import classNames from 'classnames';
+import { lazyLoadWithDimensions } from '../../util/contextHelpers';
 
 import { NamedLink } from '../../components';
 
 import css from './SectionHowItWorks.css';
 
+import searchImage from './images/Search.png';
+import bookImage from './images/Book.png';
+import enjoyImage from './images/Enjoy.png';
+
 const SectionHowItWorks = props => {
   const { rootClassName, className } = props;
+  
+
+  class LocationImage extends Component {
+    render() {
+      const { alt, ...rest } = this.props;
+      return <img alt={alt} {...rest} />;
+    }
+  }
+  const LazyImage = lazyLoadWithDimensions(LocationImage);
+  
 
   const classes = classNames(rootClassName || css.root, className);
   return (
@@ -19,6 +34,9 @@ const SectionHowItWorks = props => {
 
       <div className={css.steps}>
         <div className={css.step}>
+          <div className={css.iconContainer}>
+            <img src={searchImage} className={css.icon} />
+          </div>
           <h2 className={css.stepTitle}>
             <FormattedMessage id="SectionHowItWorks.part1Title" />
           </h2>
@@ -28,6 +46,9 @@ const SectionHowItWorks = props => {
         </div>
 
         <div className={css.step}>
+          <div className={css.iconContainer}>
+            <img src={bookImage} className={css.icon} />
+          </div>
           <h2 className={css.stepTitle}>
             <FormattedMessage id="SectionHowItWorks.part2Title" />
           </h2>
@@ -37,6 +58,9 @@ const SectionHowItWorks = props => {
         </div>
 
         <div className={css.step}>
+          <div className={css.iconContainer}>
+            <img src={enjoyImage} className={css.icon} />
+          </div>
           <h2 className={css.stepTitle}>
             <FormattedMessage id="SectionHowItWorks.part3Title" />
           </h2>
