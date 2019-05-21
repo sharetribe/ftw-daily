2173b53e362e72fd1b5f1223bc4a716963339ce5
Jenni Nurmi <jenni@sharetribe.com>
2019-05-21 14:51:32 +0300

    Use country codes

--- src/containers/CheckoutPage/CheckoutPage.js
+++ src/containers/CheckoutPage/CheckoutPage.js
@@ -243,8 +243,6 @@ export class CheckoutPageComponent extends Component {
       email: ensureCurrentUser(currentUser).attributes.email,
     };
 
-    debugger;
-
     const requestPaymentParams = {
       pageData: this.state.pageData,
       speculatedTransaction,
@@ -271,6 +269,7 @@ export class CheckoutPageComponent extends Component {
 
         const routes = routeConfiguration();
         const initialMessageFailedToTransaction = initialMessageSuccess ? null : orderId;
+
         const orderDetailsPath = pathByRouteName('OrderDetailsPage', routes, { id: orderId.uuid });
 
         initializeOrderPage({ initialMessageFailedToTransaction }, routes, dispatch);
