fc75b4d04d404575d28c4a91b0ada9ac45822cc4
Toshi <31828146+abmgmt@users.noreply.github.com>
2019-05-30 10:09:32 -0400

    Changes to marketplace currency, About page image and text, and other homepage text changes.

--- src/containers/AboutPage/AboutPage.js
+++ src/containers/AboutPage/AboutPage.js
@@ -12,7 +12,7 @@ import {
 } from '../../components';
 
 import css from './AboutPage.css';
-import image from './about-us-1056.jpg';
+import image from './Jam-Spaces.jpg';
 
 const AboutPage = () => {
   const { siteTwitterHandle, siteFacebookPage } = config;
