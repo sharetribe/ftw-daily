e51f078b6fda93b90695ff41a7a3b9cfca52f9fb
Toshi <31828146+abmgmt@users.noreply.github.com>
2019-06-01 12:23:23 -0400

    Default location changed to include only Greater Montreal

--- src/default-location-searches.js
+++ src/default-location-searches.js
@@ -9,38 +9,10 @@ const { LatLng, LatLngBounds } = sdkTypes;
 // `predictionPlace` (util.types.place) properties.
 export default [
   {
-    id: 'default-helsinki',
+    id: 'default-greater-montreal',
     predictionPlace: {
-      address: 'Helsinki, Finland',
-      bounds: new LatLngBounds(new LatLng(60.29783, 25.25448), new LatLng(59.92248, 24.78287)),
+      address: 'Greater Montreal',
+      bounds: new LatLngBounds(new LatLng(45.330504, -74.001668), new LatLng(45.799914, -73.353174)),
     },
-  },
-  {
-    id: 'default-turku',
-    predictionPlace: {
-      address: 'Turku, Finland',
-      bounds: new LatLngBounds(new LatLng(60.53045, 22.38197), new LatLng(60.33361, 22.06644)),
-    },
-  },
-  {
-    id: 'default-tampere',
-    predictionPlace: {
-      address: 'Tampere, Finland',
-      bounds: new LatLngBounds(new LatLng(61.83657, 24.11838), new LatLng(61.42728, 23.5422)),
-    },
-  },
-  {
-    id: 'default-oulu',
-    predictionPlace: {
-      address: 'Oulu, Finland',
-      bounds: new LatLngBounds(new LatLng(65.56434, 26.77069), new LatLng(64.8443, 24.11494)),
-    },
-  },
-  {
-    id: 'default-ruka',
-    predictionPlace: {
-      address: 'Ruka, Finland',
-      bounds: new LatLngBounds(new LatLng(66.16997, 29.16773), new LatLng(66.16095, 29.13572)),
-    },
-  },
+  },           
 ];
