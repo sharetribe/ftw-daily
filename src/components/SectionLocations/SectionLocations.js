ea5101afae3113dd73b95fd894ac1494298bc3e1
trrstnn <edeclarin@gmail.com>
2019-06-20 14:37:54 -0700

    Personalizes location conent

--- src/components/SectionLocations/SectionLocations.js
+++ src/components/SectionLocations/SectionLocations.js
@@ -47,21 +47,21 @@ const SectionLocations = props => {
   return (
     <div className={classes}>
       <div className={css.title}>
-        <FormattedMessage id="SectionLocations.title" />
+        Explore local talents in your area!
       </div>
       <div className={css.locations}>
         {locationLink(
-          'Helsinki',
+          'Bay Area',
           helsinkiImage,
           '?address=Helsinki%2C%20Finland&bounds=60.2978389%2C25.254484899999966%2C59.9224887%2C24.782875800000056&origin=60.16985569999999%2C24.93837910000002'
         )}
         {locationLink(
-          'Rovaniemi',
+          'Atlanta',
           rovaniemiImage,
           '?address=Rovaniemi%2C%20Finland&bounds=67.18452510000002%2C27.32667850000007%2C66.1553745%2C24.736871199999996&origin=66.50394779999999%2C25.729390599999988'
         )}
         {locationLink(
-          'Ruka',
+          'Los Angeles',
           rukaImage,
           '?address=Ruka%2C%20Finland&bounds=66.1704578%2C29.14246849999995%2C66.1614402%2C29.110453699999994&origin=66.16594940000002%2C29.12646110000003'
         )}
