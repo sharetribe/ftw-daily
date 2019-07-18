7ba8684842153f68ab0a63b566b38a4066f999f6
shylands <steve@shylands.com>
2019-07-18 09:37:16 +0200

    adding correct location imgs

--- src/components/SectionLocations/SectionLocations.js
+++ src/components/SectionLocations/SectionLocations.js
@@ -11,6 +11,11 @@ import css from './SectionLocations.css';
 import helsinkiImage from './images/location_helsinki.jpg';
 import rovaniemiImage from './images/location_rovaniemi.jpg';
 import rukaImage from './images/location_ruka.jpg';
+import skerriesImage from './images/location_skerries.jpg';
+import adaremanorImage from './images/location_adaremanor.jpg';
+import maynoothImage from './images/location_maynooth.jpg';
+import kinsaleImage from './images/location_kinsale.jpg';
+import fotaImage from './images/location_fota.jpg';
 
 class LocationImage extends Component {
   render() {
@@ -52,22 +57,27 @@ const SectionLocations = props => {
       <div className={css.locations}>
         {locationLink(
           'Skerries',
-          helsinkiImage,
+          skerriesImage,
           '?address=Helsinki%2C%20Finland&bounds=60.2978389%2C25.254484899999966%2C59.9224887%2C24.782875800000056&origin=60.16985569999999%2C24.93837910000002'
         )}
         {locationLink(
           'Maynooth',
-          rovaniemiImage,
+          maynoothImage,
           '?address=Rovaniemi%2C%20Finland&bounds=67.18452510000002%2C27.32667850000007%2C66.1553745%2C24.736871199999996&origin=66.50394779999999%2C25.729390599999988'
         )}
         {locationLink(
           'Adare Manor',
-          rukaImage,
+          adaremanorImage,
           '?address=Ruka%2C%20Finland&bounds=66.1704578%2C29.14246849999995%2C66.1614402%2C29.110453699999994&origin=66.16594940000002%2C29.12646110000003'
         )}
         {locationLink(
           'Fota Island',
-          rukaImage,
+          fotaImage,
+          '?address=Ruka%2C%20Finland&bounds=66.1704578%2C29.14246849999995%2C66.1614402%2C29.110453699999994&origin=66.16594940000002%2C29.12646110000003'
+        )}
+        {locationLink(
+          'Kinsale',
+          kinsaleImage,
           '?address=Ruka%2C%20Finland&bounds=66.1704578%2C29.14246849999995%2C66.1614402%2C29.110453699999994&origin=66.16594940000002%2C29.12646110000003'
         )}
       </div>
