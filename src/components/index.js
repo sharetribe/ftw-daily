3767ae582d46692c579d34563af2a773b6f0f47b
Jenni Nurmi <jenni@sharetribe.com>
2019-07-25 10:37:53 +0300

    Move StripePaymentAddress to components

--- src/components/index.js
+++ src/components/index.js
@@ -139,6 +139,7 @@ export { default as SearchMapPriceLabel } from './SearchMapPriceLabel/SearchMapP
 export { default as SearchResultsPanel } from './SearchResultsPanel/SearchResultsPanel';
 export { default as SelectMultipleFilter } from './SelectMultipleFilter/SelectMultipleFilter';
 export { default as SelectSingleFilter } from './SelectSingleFilter/SelectSingleFilter';
+export { default as StripePaymentAddress } from './StripePaymentAddress/StripePaymentAddress';
 export { default as UserCard } from './UserCard/UserCard';
 
 //////////////////////////////////////////////
