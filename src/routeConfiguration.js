905f99043bd558e187b3005e9b8f56ef7c442163
Jenni Nurmi <jenni@sharetribe.com>
2019-07-25 10:44:18 +0300

    Create PaymentMethodsPage

--- src/routeConfiguration.js
+++ src/routeConfiguration.js
@@ -15,6 +15,7 @@ import {
   PasswordRecoveryPage,
   PasswordResetPage,
   PayoutPreferencesPage,
+  PaymentMethodsPage,
   PrivacyPolicyPage,
   ProfilePage,
   ProfileSettingsPage,
@@ -33,6 +34,7 @@ export const ACCOUNT_SETTINGS_PAGES = [
   'ContactDetailsPage',
   'PasswordChangePage',
   'PayoutPreferencesPage',
+  'PaymentMethodsPage',
 ];
 
 // https://en.wikipedia.org/wiki/Universally_unique_identifier#Nil_UUID
@@ -248,6 +250,14 @@ const routeConfiguration = () => {
       authPage: 'LoginPage',
       component: props => <PayoutPreferencesPage {...props} />,
       loadData: PayoutPreferencesPage.loadData,
+    },
+    {
+      path: '/account/payment-methods',
+      name: 'PaymentMethodsPage',
+      auth: true,
+      authPage: 'LoginPage',
+      component: props => <PaymentMethodsPage {...props} />,
+      loadData: PaymentMethodsPage.loadData,
     },
     {
       path: '/terms-of-service',
