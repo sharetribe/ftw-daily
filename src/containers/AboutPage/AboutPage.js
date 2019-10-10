125e08ed099d1976a48e4f1fb4d5ba0e4956a1a4
shylands <steve@shylands.com>
2019-10-11 08:30:47 +0900

    adding about page content

--- src/containers/AboutPage/AboutPage.js
+++ src/containers/AboutPage/AboutPage.js
@@ -35,54 +35,110 @@ const AboutPage = () => {
         </LayoutWrapperTopbar>
 
         <LayoutWrapperMain className={css.staticPageWrapper}>
-          <h1 className={css.pageTitle}>Experience the unique Finnish home sauna.</h1>
+          <h1 className={css.pageTitle}>öogo</h1>
           <img className={css.coverImage} src={image} alt="My first ice cream." />
 
           <div className={css.contentWrapper}>
-            <div className={css.contentSide}>
-              <p>Did you know that Finland has 3.2 million saunas - almost one sauna per person!</p>
-            </div>
+             <div className={css.contentSide}>
+              <p className={css.contentQuote}>“our business is to enhance your guest’s experience”</p>
+            </div> 
 
             <div className={css.contentMain}>
               <h2>
-                Most of the Finnish saunas are located at the homes of individuals - indeed, most
-                people in Finland live in an apartment with sauna in it. In addition, lots of people
-                have lakeside summer cottages, which also typically come with a separate sauna
-                building near the waterfront.
+                What is öogo?
               </h2>
-
               <p>
-                To truly experience a Finnish sauna, you need to look beyond the public saunas, and
-                instead visit a real home or cottage sauna. Saunatime makes this possible for
-                everyone. All our saunas are owned by individuals willing to let tourists and other
-                curious visitors to enter their sacred spaces.
+                öogo is a trusted, intuitive, referral-driven platform for booking childcare.
               </p>
-
-              <h3 className={css.subtitle}>Are you a sauna owner?</h3>
-
+              <h2>
+                Why öogo?
+              </h2>
               <p>
-                Saunatime offers you a good way to earn some extra cash! If you're not using your
-                sauna every evening, why not rent it to other people while it's free. And even if
-                you are using your sauna every evening (we understand, it's so good), why not invite
-                other people to join you when the sauna is already warm! A shared sauna experience
-                is often a more fulfilling one.
+                öogo was founded by parents who worked across a range of industries from childcare to hospitality so trust is the cornerstone of everything we do and we understand the pressures involved behind the scenes to ensure exceptional guest experiences every time.
               </p>
-
-              <h3 id="contact" className={css.subtitle}>
-                Create your own marketplace like Saunatime
-              </h3>
+              <h2>
+                Why now?
+              </h2>
               <p>
-                Saunatime is brought to you by the good folks at{' '}
-                <ExternalLink href="http://sharetribe.com">Sharetribe</ExternalLink>. Would you like
-                to create your own marketplace platform a bit like Saunatime? Or perhaps a mobile
-                app? With Sharetribe it's really easy. If you have a marketplace idea in mind, do
-                get in touch!
+                Young families are increasingly bringing their children on work trips, on multiple holidays and to weddings and conferences. Accessing trusted, reliable childcare locally, which enhances the guest experience, is becoming more important for the hospitality sector.             
+              </p>              
+              <h2>
+                The benefits to hotels:
+              </h2>
+              <p>
+                <b>No more texting and calling to see who is free</b> - select the day and see who is free with one click
               </p>
               <p>
+              <b>Flexible Booking</b> - Pre-approve pre-selected minders and babysitters or direct guests to the application and let them chose themselves see who is free with one click
+              </p>
+              <p>
+              <b>No upfront fee</b> - a booking fee is taken once the right-fit babysitter is selected and the job completed to the satisfaction of the guests and the hote
+                see who is free with one click
+              </p>
+              <p>
+              <b>Fully insured</b> - öogo’s platform and jobs booked through it are fully insured giving parents, minders and hotels piece of mind
+              </p>
+              <p>
+                <b>24 hour customer care</b> - if something does go wrong, there is a trained network of experienced minders on hand to pick up the phone
+              </p>
+              <p>
+                <b>Onsite Training</b> - Two week trial available with on-site guidance to ensure a smooth transition to the öogo platform
+              </p>
+              <p>
+                <b>Make öogo work for you</b> - tell öogo what you want - you know your guests best so give öogo the right profile of minders for your hotel and öogo will find and vet them for you
+              </p>
+              <p>
+                <b>Make babysitting a strength</b> - Suggest to guests and event coordinators at the time of booking that there is exceptional babysitting available to enhance the guest experience and use triggers to notify parents of this service if a family room is booked or a cot requested            
+              </p>              
+              <h2>How does öogo work?</h2>
+              <p>Select the the day you need a baby-sitter for by location, browse available babysitters and book the right-fit person for the job</p>
+              <ol className={css.contentListNumbered}>
+                <li><p>
+                  <b>Browse:</b> vetted minders - see only who is available and use the smart-filters for targeted searching</p>
+                </li>
+                <li><p><b>Book:</b> the right-fit minder with instant book or send them a message if you have a question</p></li>
+                <li><p><b>Pay:</b> online - no having to remember cash or tips</p></li>
+                <li><p><b>Review:</b> leave a review for the next guest</p></li>
+              </ol>
+              <h2>Events</h2>
+              <ul className={css.contentList}>
+                <li><p>See what minders are available for a wedding or event with just one click</p></li>
+                <li><p>Send event coordinators or wedding parties a link to available minders</p></li>
+                <li><p>No more emailing lists of mobile numbers or calling around to see who is free</p></li>
+                <li><p>Choose from minders that do overnights and look after large groups of children with activities</p></li>
+              </ul>
+              <h2>Trust is everything</h2>
+              <p>
+              <ul className={css.contentList}>
+                <li><p>öogo minders are öogo vetted</p></li>
+                <li><p>öogo minders are rated and reviewed after each job</p></li>
+                <li><p>öogo minders represent a wide range of personalities from students to grandmothers and maternity nurses to montessori teachers</p></li>
+                <li><p>öogo is minder focused. If the minders are well looked after, the children are well looked after which means happy parents and, ultimately, happy hotels.</p></li>
+              </ul>
+              </p>
+              <blockquote className={css.contentQuote}><p>
+                “our business is to enhance your guest’s experience”
+                it’s not enough that the baby-sitting job goes well - öogo sets out to match the right-fit minders for each job ensuring the babysitting service becomes a positive talking point after the stay.
+                </p>
+              </blockquote>
+              <p>The öogo platform is fully GDPR compliant and is hosted through Finish company, ShareTribe’s, platform with data securely stored on AWS.</p>
+              <p id="contact">
+                <ExternalLink href="https://öogo.me">öogo.me</ExternalLink> / <ExternalLink href="mailto:hello@öogo.me">hello@öogo.me</ExternalLink> / <ExternalLink href="help@öogo.me">help@öogo.me</ExternalLink> / <ExternalLink href="tel:18000000000">1800 xxx xxx</ExternalLink>
+              </p>
+
+
+
+
+
+
+              {/* <h3 className={css.subtitle}>Are you a sauna owner?</h3> */}
+
+{/*
+
                 You can also checkout our{' '}
                 <ExternalLink href={siteFacebookPage}>Facebook</ExternalLink> and{' '}
-                <ExternalLink href={siteTwitterPage}>Twitter</ExternalLink>.
-              </p>
+                <ExternalLink href={siteTwitterPage}>Twitter</ExternalLink>. */}
+
             </div>
           </div>
         </LayoutWrapperMain>
