ad2e2d5057dd046944027f84f387c1bcf802db9d
Måns Hellgren <mans.hellgren@metamatrix.se>
2019-10-13 22:36:48 +0200

    rename file

--- src/app.js
+++ src/app.js
@@ -41,7 +41,7 @@ import defaultMessages from './translations/en.json';
 
 // Step 3:
 // If you are using a non-english locale, point `messagesInLocale` to correct .json file
-import messagesInLocale from './translations/sv.json';
+import messagesInLocale from './translations/se.json';
 
 // If translation key is missing from `messagesInLocale` (e.g. fr.json),
 // corresponding key will be added to messages from `defaultMessages` (en.json)
