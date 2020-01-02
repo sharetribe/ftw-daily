b0ed3c7f5ce33e23ee03cc81476820e7f66c1164
Martin McKeaveney <martin@shogunsystems.co.uk>
2020-01-02 17:46:06 +0000

    fixed responsive design

--- src/containers/PartnersPage/PartnersPageHero/PartnersPageHero.js
+++ src/containers/PartnersPage/PartnersPageHero/PartnersPageHero.js
@@ -2,9 +2,9 @@ import React, { PureComponent } from 'react';
 import { string } from 'prop-types';
 import { FormattedMessage } from 'react-intl';
 import classNames from 'classnames';
+import { NamedLink } from '../../../components';
 import css from './PartnersPageHero.css';
 import * as animationData from '../../../assets/oogo-animation';
-import { NamedLink } from '../../../components';
 import Lottie from 'react-lottie';
 
 class LottieWrapper extends PureComponent {
@@ -38,7 +38,9 @@ const SectionHero = props => {
           <FormattedMessage id="PartnersPageHero.subTitle" />
         </h2>
         <section>
-          <h1><FormattedMessage id="PartnersPageHero.becomeAPartner" /></h1>
+          <h1>
+            <FormattedMessage id="PartnersPageHero.becomeAPartner" />
+          </h1>
           <ul className={css.heroLinksList}>
             <li>
               <a className={css.heroButton}>
