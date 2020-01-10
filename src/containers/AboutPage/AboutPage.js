7532f4419a2b795fbb751a37951b5e3f6c0d6ce7
Sespazmian <c.sebastian.berggren@gmail.com>
2020-01-10 15:08:00 +0000

    Revert "Added changes to "About page" as a category page"
    This reverts commit 7cf21efff98fe74cd2de808c559d41cb6294b132.

--- src/containers/AboutPage/AboutPage.js
+++ src/containers/AboutPage/AboutPage.js
@@ -13,31 +13,20 @@ import {
 
 import css from './AboutPage.css';
 import image from './about-us-1056.jpg';
-import json from './AboutPage.json';
 
 const AboutPage = () => {
   const { siteTwitterHandle, siteFacebookPage } = config;
   const siteTwitterPage = twitterPageURL(siteTwitterHandle);
 
-  const title = json.title;
-  const description = json.description;
-  const videoEmbed = json.videoEmbed;
-  const ingress = json.ingress;
-  const content = json.content;
-  const sidecontent = json.sidecontent;
-  const altText = json.altText;
-
-
-
   // prettier-ignore
   return (
     <StaticPage
-      title={title}
+      title="About Us"
       schema={{
         '@context': 'http://schema.org',
         '@type': 'AboutPage',
-        description: {description},
-        name: {title},
+        description: 'About Saunatime',
+        name: 'About page',
       }}
     >
       <LayoutSingleColumn>
@@ -46,23 +35,55 @@ const AboutPage = () => {
         </LayoutWrapperTopbar>
 
         <LayoutWrapperMain className={css.staticPageWrapper}>
-          <h1 className={css.pageTitle}>{title}</h1>
-          <img className={css.coverImage} src={image} alt="{altText}" />
-
+          <h1 className={css.pageTitle}>Experience the unique Finnish home sauna.</h1>
+          <img className={css.coverImage} src={image} alt="My first ice cream." />
 
           <div className={css.contentWrapper}>
             <div className={css.contentSide}>
-              <p>{sidecontent}</p>
+              <p>Did you know that Finland has 3.2 million saunas - almost one sauna per person!</p>
             </div>
 
             <div className={css.contentMain}>
               <h2>
-                {ingress}
+                Most of the Finnish saunas are located at the homes of individuals - indeed, most
+                people in Finland live in an apartment with sauna in it. In addition, lots of people
+                have lakeside summer cottages, which also typically come with a separate sauna
+                building near the waterfront.
               </h2>
-              
-              {content.map(s => (<p>{s}</p>))}
-            </div>
 
+              <p>
+                To truly experience a Finnish sauna, you need to look beyond the public saunas, and
+                instead visit a real home or cottage sauna. Saunatime makes this possible for
+                everyone. All our saunas are owned by individuals willing to let tourists and other
+                curious visitors to enter their sacred spaces.
+              </p>
+
+              <h3 className={css.subtitle}>Are you a sauna owner?</h3>
+
+              <p>
+                Saunatime offers you a good way to earn some extra cash! If you're not using your
+                sauna every evening, why not rent it to other people while it's free. And even if
+                you are using your sauna every evening (we understand, it's so good), why not invite
+                other people to join you when the sauna is already warm! A shared sauna experience
+                is often a more fulfilling one.
+              </p>
+
+              <h3 id="contact" className={css.subtitle}>
+                Create your own marketplace like Saunatime
+              </h3>
+              <p>
+                Saunatime is brought to you by the good folks at{' '}
+                <ExternalLink href="http://sharetribe.com">Sharetribe</ExternalLink>. Would you like
+                to create your own marketplace platform a bit like Saunatime? Or perhaps a mobile
+                app? With Sharetribe it's really easy. If you have a marketplace idea in mind, do
+                get in touch!
+              </p>
+              <p>
+                You can also checkout our{' '}
+                <ExternalLink href={siteFacebookPage}>Facebook</ExternalLink> and{' '}
+                <ExternalLink href={siteTwitterPage}>Twitter</ExternalLink>.
+              </p>
+            </div>
           </div>
         </LayoutWrapperMain>
 
