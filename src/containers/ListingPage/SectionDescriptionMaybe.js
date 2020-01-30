ef50e8a876e254700da83ddb24c5bf3c1e08e2b3
dkramskoj <59252953+dkramskoj@users.noreply.github.com>
2020-01-30 09:57:45 +0200

    Toggle text component created (#99)
    1. ToggleText component created; 2. withArrowIcon css rule added to marketplace.css

--- src/containers/ListingPage/SectionDescriptionMaybe.js
+++ src/containers/ListingPage/SectionDescriptionMaybe.js
@@ -1,24 +1,25 @@
 import React from 'react';
 import { FormattedMessage } from '../../util/reactIntl';
 import { richText } from '../../util/richText';
-
+import ToggleText from '../../components/ToggleText/ToggleText';
 import css from './ListingPage.css';
 
 const MIN_LENGTH_FOR_LONG_WORDS_IN_DESCRIPTION = 20;
 
 const SectionDescriptionMaybe = props => {
   const { description } = props;
+
   return description ? (
     <div className={css.listingSectionContainer}>
       <h2 className={css.listingSectionTitle}>
         <FormattedMessage id="ListingPage.descriptionTitle" />
       </h2>
-      <p className={css.listingPrimaryText}>
-        {richText(description, {
-          longWordMinLength: MIN_LENGTH_FOR_LONG_WORDS_IN_DESCRIPTION,
-          longWordClass: css.longWord,
-        })}
-      </p>
+          <ToggleText CustomTag="p" className={css.listingPrimaryText} maxLength={250} customChildren>
+                  {richText(description, {
+                      longWordMinLength: MIN_LENGTH_FOR_LONG_WORDS_IN_DESCRIPTION,
+                      longWordClass: css.longWord,
+                  })}
+           </ToggleText>
     </div>
   ) : null;
 };
