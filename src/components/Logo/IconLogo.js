5b483c4b8d5bfae27764e3c76f728ba92869a31e
Kalle <kalle.a.moen@gmail.com>
2020-02-16 22:33:33 +0000

    Uploaded final logo

--- src/components/Logo/IconLogo.js
+++ src/components/Logo/IconLogo.js
@@ -6,17 +6,55 @@ const IconLogo = props => {
 
   return (
     <svg
-      className={className}
-      {...rest}
-      width="21"
-      height="25"
-      viewBox="0 0 21 25"
       xmlns="http://www.w3.org/2000/svg"
+      width="35"
+      height="35"
+      viewBox="0 0 35 35"
     >
-      <path
-        d="M10.063 23.646c-2.286 0-4.025-1.852-4.025-4.138 0-3.342 2.405-6.49 5.69-7.246-1.875 1.382-1.77 4.06-1.032 5.176.506-.416.956-.87 1.033-1.55 1.727.68 2.358 2.147 2.358 3.733 0 2.288-1.74 4.026-4.025 4.026m9.64-12.442c-.108-.352-.536-.467-.807-.24-.7.592-1.47 1.063-2.3 1.406-.21-6.144-4.754-11.247-10.8-12.214-.42-.066-.73.385-.514.753 1.53 2.598 1.656 5.74.443 8.395-.59-.816-1.033-1.717-1.32-2.686-.1-.342-.522-.482-.805-.244C1.313 8.29 0 11.1 0 14.086c0 5.62 4.553 10.063 10.063 10.063 5.548 0 10.062-4.514 10.062-10.063 0-.982-.143-1.952-.423-2.883"
-        fillRule="evenodd"
-      />
+      <g fill="none" fillRule="evenodd" stroke="none" strokeWidth="1">
+        <g fillRule="nonzero">
+          <circle
+            cx="8.5"
+            cy="25.51"
+            r="8.5"
+            fill="#8EFF8C"
+            opacity="0.8"
+          ></circle>
+          <circle
+            cx="25.51"
+            cy="25.51"
+            r="8.5"
+            fill="#FFABD7"
+            opacity="0.8"
+          ></circle>
+          <path
+            fill="#FFF5A6"
+            d="M8.5 17.01H25.51V34.02H8.5z"
+            opacity="0.8"
+          ></path>
+          <circle
+            cx="25.51"
+            cy="8.5"
+            r="8.5"
+            fill="#5CBFCC"
+            opacity="0.75"
+          ></circle>
+          <path
+            fill="#8EFF8C"
+            d="M17.01 8.5H34.02V25.51H17.01z"
+            opacity="0.8"
+          ></path>
+          <circle
+            cx="8.5"
+            cy="8.5"
+            r="8.5"
+            fill="#FFF5A6"
+            opacity="0.8"
+          ></circle>
+          <path fill="#5CBFCC" d="M0 8.5H17.01V25.51H0z" opacity="0.75"></path>
+          <path fill="#FFABD7" d="M8.5 0H25.51V17.01H8.5z" opacity="0.8"></path>
+        </g>
+      </g>
     </svg>
   );
 };
--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -4,7 +4,7 @@ import classNames from 'classnames';
 
 import config from '../../config';
 import IconLogo from './IconLogo';
-import LogoImage from './hotpatch-logo-temp.png';
+import LogoImage from './hotpatch-logo.png';
 import css from './Logo.css';
 
 const Logo = props => {
