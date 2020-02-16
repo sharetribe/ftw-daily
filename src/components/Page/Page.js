4922506fcbe54fc1e439f651d80d84ebf05f49e8
Pier <pier.stein93@gmail.com>
2020-02-16 14:24:08 +0800

    Change Thumbnail
    Does this work?

--- src/components/Page/Page.js
+++ src/components/Page/Page.js
@@ -10,8 +10,8 @@ import { metaTagProps } from '../../util/seo';
 import { canonicalRoutePath } from '../../util/routes';
 import { CookieConsent } from '../../components';
 
-import facebookImage from '../../assets/saunatimeFacebook-1200x630.jpg';
-import twitterImage from '../../assets/saunatimeTwitter-600x314.jpg';
+import facebookImage from '../../assets/Cws img when sharing.png';
+import twitterImage from '../../assets/Cws img when sharing.png';
 import css from './Page.css';
 
 const preventDefault = e => {
