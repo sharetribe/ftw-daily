5f9051b5fc574a1a6bb75b7501356eebe05409dc
Sespazmian <c.sebastian.berggren@gmail.com>
2020-02-18 18:21:49 +0000

    Logo cleanup

--- src/components/Logo/IconLogo.js
+++ src/components/Logo/IconLogo.js
@@ -1,9 +1,6 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 
 const IconLogo = props => {
-  const { className, ...rest } = props;
-
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
@@ -59,14 +56,5 @@ const IconLogo = props => {
   );
 };
 
-const { string } = PropTypes;
-
-IconLogo.defaultProps = {
-  className: null,
-};
-
-IconLogo.propTypes = {
-  className: string,
-};
 
 export default IconLogo;
