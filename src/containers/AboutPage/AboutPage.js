acb32eee5b26435981d71fa212caa6ca34f47ae6
Benjamin Kröni <46282622+benjaminkroeni@users.noreply.github.com>
2020-03-04 17:57:45 +0100

    Update AboutPage.js

--- src/containers/AboutPage/AboutPage.js
+++ src/containers/AboutPage/AboutPage.js
@@ -42,8 +42,7 @@ const AboutPage = ({ title }) => {
         </LayoutWrapperTopbar>
 
         <LayoutWrapperMain className={css.staticPageWrapper}>
-          <h1 className={css.pageTitle}>Hallo! Wir sind HorseDeal24</h1>
-          <h1 className={css.pageTitle}>Von der Idee zur Pferde-Sharing Plattform</h1>
+          <h1 className={css.pageTitle}>Hallo! Wir sind HorseDeal24{"\n"}Das ist ein Test</h1>
           <img className={css.coverImage} src={image} alt="My first ice cream." />
 
           <div className={css.contentWrapper}>
