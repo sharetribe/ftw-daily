2727ce260dae8f0965d5f485e0dca0187df15ecc
Kalle <kalle.a.moen@gmail.com>
2020-03-04 09:02:44 -0300

    Typos

--- src/default-location-searches.js
+++ src/default-location-searches.js
@@ -9,38 +9,10 @@ const { LatLng, LatLngBounds } = sdkTypes;
 // `predictionPlace` (util.types.place) properties.
 export default [
   {
-    id: 'default-helsinki',
+    id: 'default-london',
     predictionPlace: {
-      address: 'Helsinki, Finland',
-      bounds: new LatLngBounds(new LatLng(60.29783, 25.25448), new LatLng(59.92248, 24.78287)),
-    },
-  },
-  {
-    id: 'default-turku',
-    predictionPlace: {
-      address: 'Turku, Finland',
-      bounds: new LatLngBounds(new LatLng(60.53045, 22.38197), new LatLng(60.33361, 22.06644)),
-    },
-  },
-  {
-    id: 'default-tampere',
-    predictionPlace: {
-      address: 'Tampere, Finland',
-      bounds: new LatLngBounds(new LatLng(61.83657, 24.11838), new LatLng(61.42728, 23.5422)),
-    },
-  },
-  {
-    id: 'default-oulu',
-    predictionPlace: {
-      address: 'Oulu, Finland',
-      bounds: new LatLngBounds(new LatLng(65.56434, 26.77069), new LatLng(64.8443, 24.11494)),
-    },
-  },
-  {
-    id: 'default-ruka',
-    predictionPlace: {
-      address: 'Ruka, Finland',
-      bounds: new LatLngBounds(new LatLng(66.16997, 29.16773), new LatLng(66.16095, 29.13572)),
+      address: 'London, United Kingdom',
+      bounds: new LatLngBounds(new LatLng(51.67789511, 0.29732496), new LatLng(51.3925498, -0.57645)),
     },
   },
 ];
