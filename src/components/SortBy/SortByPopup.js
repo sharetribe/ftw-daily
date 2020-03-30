f578cb8656e1693af154d16013afa9c17da3b453
Kimmo Puputti <git@kimmo.kpuputti.fi>
2020-03-30 13:29:37 +0300

    Change sorting icon

--- src/components/SortBy/SortByPopup.js
+++ src/components/SortBy/SortByPopup.js
@@ -12,10 +12,18 @@ const optionLabel = (options, key) => {
 
 const SortByIcon = () => {
   return (
-    <span className={css.icon}>
-      <span className={css.iconUp}>▲</span>
-      <span className={css.iconDown}>▼</span>
-    </span>
+    <svg className={css.icon} width="10" height="16" xmlns="http://www.w3.org/2000/svg">
+      <g
+        stroke="#4a4a4a"
+        strokeWidth="1.5"
+        fill="none"
+        fillRule="evenodd"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      >
+        <path d="M3.25 7.125v7.438M5 12.813l-1.75 1.75-1.75-1.75M6.75 8.875V1.438M5 3.188l1.75-1.75 1.75 1.75" />
+      </g>
+    </svg>
   );
 };
 
