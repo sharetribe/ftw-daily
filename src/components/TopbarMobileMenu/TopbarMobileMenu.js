12064a8e5f86eeffd534c6d41ca920e233ee1674
dkramskoj <59252953+dkramskoj@users.noreply.github.com>
2020-03-31 15:13:59 +0300

    Mobile topbar force rerender (#135)

--- src/components/TopbarMobileMenu/TopbarMobileMenu.js
+++ src/components/TopbarMobileMenu/TopbarMobileMenu.js
@@ -13,56 +13,71 @@ import { AvatarLarge, InlineTextButton, NamedLink, NotificationBadge } from '../
 
 import css from './TopbarMobileMenu.css';
 
-const TopbarMobileMenu = props => {
-  const {
-    isAuthenticated,
-    currentPage,
-    currentUserHasListings,
-    currentUser,
-    notificationCount,
-    onLogout,
-  } = props;
-
-  const user = ensureCurrentUser(currentUser);
-
-  if (!isAuthenticated) {
-    const signup = (
-      <NamedLink name="SignupPage" className={css.signupLink}>
-        <FormattedMessage id="TopbarMobileMenu.signupLink" />
-      </NamedLink>
-    );
-
-    const login = (
-      <NamedLink name="LoginPage" className={css.loginLink}>
-        <FormattedMessage id="TopbarMobileMenu.loginLink" />
-      </NamedLink>
-    );
-
-    const signupOrLogin = (
-      <span className={css.authenticationLinks}>
-        <FormattedMessage id="TopbarMobileMenu.signupOrLogin" values={{ signup, login }} />
-      </span>
-    );
-    return (
-      <div className={css.root}>
-        <div className={css.content}>
-          <div className={css.authenticationGreeting}>
-            <FormattedMessage
-              id="TopbarMobileMenu.unauthorizedGreeting"
-              values={{ lineBreak: <br />, signupOrLogin }}
-            />
-          </div>
-        </div>
-        <div className={css.footer}>
-          <NamedLink className={css.createNewListingLink} name="NewListingPage">
-            <FormattedMessage id="TopbarMobileMenu.newListingLink" />
-          </NamedLink>
-        </div>
-      </div>
-    );
+class TopbarMobileMenu extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { appIsMounted: false };
   }
 
-  const notificationCountBadge =
+  componentDidMount() {
+    // Re-render for isomorphic purposes
+    requestAnimationFrame(() => {
+      this.setState({ appIsMounted : true });
+    });
+  }
+
+  render() {
+    const {
+      isAuthenticated,
+      currentPage,
+      currentUserHasListings,
+      currentUser,
+      notificationCount,
+      onLogout,
+    } = this.props;
+
+    const { appIsMounted } = this.state;
+
+    const user = ensureCurrentUser(currentUser);
+
+    if (!isAuthenticated) {
+      const signup = (
+        <NamedLink name="SignupPage" className={css.signupLink}>
+          <FormattedMessage id="TopbarMobileMenu.signupLink" />
+        </NamedLink>
+      );
+  
+      const login = (
+        <NamedLink name="LoginPage" className={css.loginLink}>
+          <FormattedMessage id="TopbarMobileMenu.loginLink" />
+        </NamedLink>
+      );
+  
+      const signupOrLogin = (
+        <span className={css.authenticationLinks}>
+          <FormattedMessage id="TopbarMobileMenu.signupOrLogin" values={{ signup, login }} />
+        </span>
+      );
+      return (
+        <div className={css.root}>
+          <div className={css.content}>
+            <div className={css.authenticationGreeting}>
+              <FormattedMessage
+                id="TopbarMobileMenu.unauthorizedGreeting"
+                values={{ lineBreak: <br />, signupOrLogin }}
+              />
+            </div>
+          </div>
+          <div className={css.footer}>
+            <NamedLink className={css.createNewListingLink} name="NewListingPage">
+              <FormattedMessage id="TopbarMobileMenu.newListingLink" />
+            </NamedLink>
+          </div>
+        </div>
+      );
+    }
+
+    const notificationCountBadge =
     notificationCount > 0 ? (
       <NotificationBadge className={css.notificationBadge} count={notificationCount} />
     ) : null;
@@ -75,58 +90,65 @@ const TopbarMobileMenu = props => {
     return currentPage === page || isAccountSettingsPage ? css.currentPage : null;
   };
 
-  return (
-    <div className={css.root}>
-      <AvatarLarge className={css.avatar} user={currentUser} />
-       <div className={css.content}>
-
-        <span className={css.greeting}>
-          <FormattedMessage id="TopbarMobileMenu.greeting" values={{ displayName }} />
-        </span>
-
-        <InlineTextButton rootClassName={css.logoutButton} onClick={onLogout}>
-          <FormattedMessage id="TopbarMobileMenu.logoutLink" />
-        </InlineTextButton>
-
-        {/*<NamedLink
-          className={classNames(css.inbox, currentPageClass('InboxPage'))}
-          name="InboxPage"
-          params={{ tab: currentUserHasListings ? 'sales' : 'orders' }}
-        >
-          <FormattedMessage id="TopbarMobileMenu.inboxLink" />
-          notificationCountBadge 
-        </NamedLink>*/}
-
-        <NamedLink
-          className={classNames(css.navigationLink, currentPageClass('ManageListingsPage'))}
-          name="ManageListingsPage"
-        >
-          <FormattedMessage id="TopbarMobileMenu.yourListingsLink" />
-        </NamedLink>
-
-        <NamedLink
-          className={classNames(css.navigationLink, currentPageClass('ProfileSettingsPage'))}
-          name="ProfileSettingsPage"
-        >
-          <FormattedMessage id="TopbarMobileMenu.profileSettingsLink" />
-        </NamedLink>
-
-        <NamedLink
-          className={classNames(css.navigationLink, currentPageClass('AccountSettingsPage'))}
-          name="AccountSettingsPage"
-        >
-          <FormattedMessage id="TopbarMobileMenu.accountSettingsLink" />
-        </NamedLink>
+  if(appIsMounted) {
+    return (
+      <div className={css.root}>
+        <AvatarLarge className={css.avatar} user={currentUser} />
+         <div className={css.content}>
+  
+          <span className={css.greeting}>
+            <FormattedMessage id="TopbarMobileMenu.greeting" values={{ displayName }} />
+          </span>
+  
+          <InlineTextButton rootClassName={css.logoutButton} onClick={onLogout}>
+            <FormattedMessage id="TopbarMobileMenu.logoutLink" />
+          </InlineTextButton>
+  
+          <NamedLink
+            className={classNames(css.inbox, currentPageClass('InboxPage'))}
+            name="InboxPage"
+            params={{ tab: currentUserHasListings ? 'sales' : 'orders' }}
+          >
+            <FormattedMessage id="TopbarMobileMenu.inboxLink" />
+            {notificationCountBadge}
+          </NamedLink>
+  
+          <NamedLink
+            className={classNames(css.navigationLink, currentPageClass('ManageListingsPage'))}
+            name="ManageListingsPage"
+          >
+            <FormattedMessage id="TopbarMobileMenu.yourListingsLink" />
+          </NamedLink>
+  
+          <NamedLink
+            className={classNames(css.navigationLink, currentPageClass('ProfileSettingsPage'))}
+            name="ProfileSettingsPage"
+          >
+            <FormattedMessage id="TopbarMobileMenu.profileSettingsLink" />
+          </NamedLink>
+  
+          <NamedLink
+            className={classNames(css.navigationLink, currentPageClass('AccountSettingsPage'))}
+            name="AccountSettingsPage"
+          >
+            <FormattedMessage id="TopbarMobileMenu.accountSettingsLink" />
+          </NamedLink>
+        </div>
+  
+        <div className={css.footer}>
+          <NamedLink className={css.createNewListingLink} name="NewListingPage">
+            <FormattedMessage id="TopbarMobileMenu.newListingLink" />
+          </NamedLink>
+        </div>
       </div>
+    );
+  }
+  else {
+    return null
+  }
+  }
+}
 
-      <div className={css.footer}>
-        <NamedLink className={css.createNewListingLink} name="NewListingPage">
-          <FormattedMessage id="TopbarMobileMenu.newListingLink" />
-        </NamedLink>
-      </div>
-    </div>
-  );
-};
 
 TopbarMobileMenu.defaultProps = { currentUser: null, notificationCount: 0, currentPage: null };
 
