f055e71e112a967608ef2dcec21e241c861594b2
dkramskoj <kramskoj.d@brights.cloud>
2020-06-23 14:48:18 +0300

    loadable components added

--- src/components/CalendarWrapper/CalendarWrapper.js
+++ src/components/CalendarWrapper/CalendarWrapper.js
@@ -1,16 +1,24 @@
-import React, { Component } from 'react';
+import React from 'react';
+import loadable from '@loadable/component';
 
-import FullCalendar from '@fullcalendar/react'
+// const Interactions = loadable(() => import('@fullcalendar/interaction'));
+// const ListPlugin = loadable(() => import('@fullcalendar/list'));
+//const DayGrid = loadable(() => import('@fullcalendar/daygrid'));
+//const TimeGrid = loadable(() => import('@fullcalendar/timegrid'));
+
+// import FullCalendar from '@fullcalendar/react'
 import dayGridPlugin from '@fullcalendar/daygrid'
+import interactionPlugin from '@fullcalendar/interaction';
 import timeGridPlugin from '@fullcalendar/timegrid';
 import listPlugin from '@fullcalendar/list';
-import interactionPlugin from '@fullcalendar/interaction';
+
+const FullCalendar = loadable(() => import('@fullcalendar/react'), { ssr: false });
 
 const CalendarWrapper = ({header, defaultView, selectable, editable, events, eventClick, select, eventDrop, eventResize}) => {
-    return (
+    return typeof window !== 'undefined' && (
     <FullCalendar 
-        plugins={[dayGridPlugin, timeGridPlugin, listPlugin, interactionPlugin]}
-
+        // plugins={[DayGrid, TimeGrid, ListPlugin, Interactions]}
+        plugins={[dayGridPlugin, interactionPlugin, timeGridPlugin, listPlugin]}
         defaultView={defaultView} 
         header={header}
         selectable={selectable}
