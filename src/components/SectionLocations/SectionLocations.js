7018f621fc0428258b5bea4d212716e3a3b7e9c4
jaeyoungchang5 <58242673+jaeyoungchang5@users.noreply.github.com>
2020-07-14 11:30:29 -0400

    update: landing page's feature products link to product page

--- src/components/SectionLocations/SectionLocations.js
+++ src/components/SectionLocations/SectionLocations.js
@@ -8,9 +8,8 @@ import { NamedLink } from '../../components';
 
 import css from './SectionLocations.css';
 
-import helsinkiImage from './images/location_helsinki.jpg';
-import rovaniemiImage from './images/location_rovaniemi.jpg';
-import rukaImage from './images/location_ruka.jpg';
+import pivotpowerImage from '../../containers/ProductPage/img/PivotPower_Image.png';
+import alignImage from '../../containers/ProductPage/img/Align_Image.png';
 
 class LocationImage extends Component {
   render() {
@@ -23,7 +22,7 @@ const LazyImage = lazyLoadWithDimensions(LocationImage);
 const locationLink = (name, image, searchQuery) => {
   const nameText = <span className={css.locationName}>{name}</span>;
   return (
-    <NamedLink name="SearchPage" to={{ search: searchQuery }} className={css.location}>
+    <NamedLink name="ProductPage" className={css.location}>
       <div className={css.imageWrapper}>
         <div className={css.aspectWrapper}>
           <LazyImage src={image} alt={name} className={css.locationImage} />
@@ -36,6 +35,19 @@ const locationLink = (name, image, searchQuery) => {
         />
       </div>
     </NamedLink>
+    // <NamedLink name="SearchPage" to={{ search: searchQuery }} className={css.location}>
+      // <div className={css.imageWrapper}>
+      //   <div className={css.aspectWrapper}>
+      //     <LazyImage src={image} alt={name} className={css.locationImage} />
+      //   </div>
+      // </div>
+      // <div className={css.linkText}>
+      //   <FormattedMessage
+      //     id="SectionLocations.listingsInLocation"
+      //     values={{ location: nameText }}
+      //   />
+      // </div>
+    // </NamedLink>
   );
 };
 
@@ -51,19 +63,14 @@ const SectionLocations = props => {
       </div>
       <div className={css.locations}>
         {locationLink(
-          'Helsinki',
-          helsinkiImage,
+          'Pivot Power',
+          pivotpowerImage,
           '?address=Helsinki%2C%20Finland&bounds=60.2978389%2C25.254484899999966%2C59.9224887%2C24.782875800000056&origin=60.16985569999999%2C24.93837910000002'
         )}
         {locationLink(
-          'Rovaniemi',
-          rovaniemiImage,
+          'Align',
+          alignImage,
           '?address=Rovaniemi%2C%20Finland&bounds=67.18452510000002%2C27.32667850000007%2C66.1553745%2C24.736871199999996&origin=66.50394779999999%2C25.729390599999988'
-        )}
-        {locationLink(
-          'Ruka',
-          rukaImage,
-          '?address=Ruka%2C%20Finland&bounds=66.1704578%2C29.14246849999995%2C66.1614402%2C29.110453699999994&origin=66.16594940000002%2C29.12646110000003'
         )}
       </div>
     </div>
