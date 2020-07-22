d9dcd493d6cbb25dfe39edc4ac254d893d8cd10c
Sonia Yang <sonia.yang@berkeley.edu>
2020-07-22 07:06:55 -0700

    Integrate with Shopify

--- server/index.js
+++ server/index.js
@@ -147,6 +147,121 @@ const noCacheHeaders = {
 const httpAgent = new http.Agent({ keepAlive: true });
 const httpsAgent = new https.Agent({ keepAlive: true });
 
+
+
+
+const { SHOPIFY_API_SECRET_KEY, SHOPIFY_API_KEY } = process.env;
+
+
+// shopify 
+const dotenv = require('dotenv').config();
+// const express = require('express');
+// const app = express();
+const crypto = require('crypto');
+const cookie = require('cookie');
+const nonce = require('nonce')();
+const querystring = require('querystring');
+const request = require('request-promise');
+
+const apiKey = SHOPIFY_API_KEY;
+const apiSecret = SHOPIFY_API_SECRET_KEY;
+
+// TODO (SY): change scopes
+const scopes = 'read_orders';
+const forwardingAddress = "https://b63a1cac1054.ngrok.io"; // Replace this with your HTTPS Forwarding address
+
+app.get('/shopify', (req, res) => {
+  const shop = req.query.shop;
+  if (shop) {
+    const state = nonce();
+    const redirectUri = forwardingAddress + '/shopify/callback';
+    const installUrl = 'https://' + shop +
+      '/admin/oauth/authorize?client_id=' + apiKey +
+      '&scope=' + scopes +
+      '&state=' + state +
+      '&redirect_uri=' + redirectUri;
+
+    res.cookie('state', state);
+    res.redirect(installUrl);
+  } else {
+    return res.status(400).send('Missing shop parameter. Please add ?shop=your-development-shop.myshopify.com to your request');
+  }
+});
+
+app.get('/shopify/callback', (req, res) => {
+  const { shop, hmac, code, state } = req.query;
+  const stateCookie = cookie.parse(req.headers.cookie).state;
+
+  if (state !== stateCookie) {
+    return res.status(403).send('Request origin cannot be verified');
+  }
+
+  if (shop && hmac && code) {
+    const map = Object.assign({}, req.query);
+    delete map['signature'];
+    delete map['hmac'];
+    const message = querystring.stringify(map);
+    const providedHmac = Buffer.from(hmac, 'utf-8');
+    const generatedHash = Buffer.from(
+      crypto
+        .createHmac('sha256', apiSecret)
+        .update(message)
+        .digest('hex'),
+      'utf-8'
+    );
+    let hashEquals = false;
+    // timingSafeEqual will prevent any timing attacks. Arguments must be buffers
+    try {
+      hashEquals = crypto.timingSafeEqual(generatedHash, providedHmac)
+      // timingSafeEqual will return an error if the input buffers are not the same length.
+    } catch (e) {
+      hashEquals = false;
+    };
+
+    if (!hashEquals) {
+      return res.status(400).send('HMAC validation failed');
+    }
+
+    const accessTokenRequestUrl = 'https://' + shop + '/admin/oauth/access_token';
+    const accessTokenPayload = {
+      client_id: apiKey,
+      client_secret: apiSecret,
+      code,
+    };
+
+    request.post(accessTokenRequestUrl, { json: accessTokenPayload })
+      .then((accessTokenResponse) => {
+        const accessToken = accessTokenResponse.access_token;
+
+        const shopRequestUrl = 'https://' + shop + '/admin/api/2020-07/shop.json';
+        const shopRequestHeaders = {
+          'X-Shopify-Access-Token': accessToken,
+        };
+
+        request.get(shopRequestUrl, { headers: shopRequestHeaders })
+          .then((shopResponse) => {
+            res.end(shopResponse);
+          })
+          .catch((error) => {
+            res.status(error.statusCode).send(error.error.error_description);
+          });
+        // TODO
+        // Use access token to make API call to 'shop' endpoint
+      })
+      .catch((error) => {
+        res.status(error.statusCode).send(error.error.error_description);
+      });
+
+    // TODO
+    // Validate request is from Shopify
+    // Exchange temporary code for a permanent access token
+    // Use access token to make API call to 'shop' endpoint
+  } else {
+    res.status(400).send('Required parameters missing');
+  }
+});
+
+
 app.get('*', (req, res) => {
   if (req.url.startsWith('/static/')) {
     // The express.static middleware only handles static resources
@@ -264,3 +379,4 @@ app.listen(PORT, () => {
     console.log(`Open http://localhost:${PORT}/ and start hacking!`);
   }
 });
+
