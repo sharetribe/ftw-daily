d3e47662b90753c819e4490c3e09e3a84a4df5a8
Aleksandr Chernov <Naytilys685@gmail.com>
2020-08-03 16:05:59 +0300

    modal block information fix

--- src/containers/LandingPage/LandingPage.js
+++ src/containers/LandingPage/LandingPage.js
@@ -22,14 +22,15 @@ import {
   LayoutWrapperMain,
   LayoutWrapperFooter,
   ListingCard,
-  Footer, ModalStripeInformation,
+  Footer,
+  ModalStripeInformation,
 } from '../../components';
 import { TopbarContainer } from '../../containers';
 
 import facebookImage from '../../assets/saunatimeFacebook-1200x630.jpg';
 import twitterImage from '../../assets/saunatimeTwitter-600x314.jpg';
 import css from './LandingPage.css';
-import SkippingBlock from '../../components/SkippingBlock/SkippingBlock';
+
 
 export const LandingPageComponent = props => {
   const { onManageDisableScrolling, history, intl, listings, location, scrollingDisabled } = props;
@@ -66,7 +67,6 @@ export const LandingPageComponent = props => {
           <TopbarContainer />
         </LayoutWrapperTopbar>
         <LayoutWrapperMain>
-          <SkippingBlock/>
           <div className={css.heroContainer}>
             <SectionHero className={css.hero} history={history} location={location} />
           </div>
