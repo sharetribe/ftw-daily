1d72a5f451aa169248f81cf9cfd28579d5b3eab3
Phil Ondrejack <pondrejack@chain.io>
2020-08-19 17:25:52 -0400

    make map dynamic on scroll [SEO]

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -1,4 +1,5 @@
-import React, { Component } from 'react'
+import { useScrollPosition } from '@n8tb1t/use-scroll-position'
+import React, { Component, useState } from 'react'
 import {
   bool, number, object, string
 } from 'prop-types'
@@ -9,54 +10,64 @@ import { StaticMap, DynamicMap, isMapsLibLoaded } from './MapboxMap'
 
 import css from './Map.css'
 
-export class Map extends Component {
-  render() {
-    const {
-      className,
-      rootClassName,
-      mapRootClassName,
-      address,
-      center,
-      obfuscatedCenter,
-      zoom,
-      mapsConfig,
-      useStaticMap,
-      metadata,
-      createSurf
-    } = this.props
-    const classes = classNames(rootClassName || css.root, className)
-    const mapClasses = mapRootClassName || css.mapRoot
+const Map = (props) => {
+  const {
+    className,
+    rootClassName,
+    mapRootClassName,
+    address,
+    center,
+    obfuscatedCenter,
+    zoom,
+    mapsConfig,
+    useStaticMap,
+    metadata,
+    createSurf
+  } = props
+  const classes = classNames(rootClassName || css.root, className)
+  const mapClasses = mapRootClassName || css.mapRoot
 
-    if (mapsConfig.fuzzy.enabled && !obfuscatedCenter) {
-      throw new Error(
-        'Map: obfuscatedCenter prop is required when config.maps.fuzzy.enabled === true'
-      )
-    }
-    if (!mapsConfig.fuzzy.enabled && !center) {
-      throw new Error('Map: center prop is required when config.maps.fuzzy.enabled === false')
-    }
+  const [staticMap, setStaticMap] = useState(useStaticMap)
 
-    const location = mapsConfig.fuzzy.enabled ? obfuscatedCenter : center
+  useScrollPosition(
+    ({ prevPos, currPos }) => {
+      const isVisible = currPos.y < -1000
+      if (isVisible && staticMap) {
+        setStaticMap(false)
+      }
+    },
+    [staticMap]
+  )
 
-    return !isMapsLibLoaded() ? (
-      <div className={classes} />
-    ) : useStaticMap ? (
-      <StaticMap center={location} zoom={zoom} address={address} mapsConfig={mapsConfig} />
-    ) : (
-      <DynamicMap
-        containerElement={<div className={classes} />}
-        mapElement={<div className={mapClasses} />}
-        containerClassName={classes}
-        mapClassName={mapClasses}
-        center={location}
-        zoom={zoom}
-        address={address}
-        mapsConfig={mapsConfig}
-        metadata={metadata}
-        createSurf={createSurf}
-      />
+  if (mapsConfig.fuzzy.enabled && !obfuscatedCenter) {
+    throw new Error(
+      'Map: obfuscatedCenter prop is required when config.maps.fuzzy.enabled === true'
     )
   }
+  if (!mapsConfig.fuzzy.enabled && !center) {
+    throw new Error('Map: center prop is required when config.maps.fuzzy.enabled === false')
+  }
+
+  const location = mapsConfig.fuzzy.enabled ? obfuscatedCenter : center
+  console.log(staticMap)
+  return !isMapsLibLoaded() ? (
+    <div className={classes} />
+  ) : staticMap ? (
+    <StaticMap center={location} zoom={zoom} address={address} mapsConfig={mapsConfig} />
+  ) : (
+    <DynamicMap
+      containerElement={<div className={classes} />}
+      mapElement={<div className={mapClasses} />}
+      containerClassName={classes}
+      mapClassName={mapClasses}
+      center={location}
+      zoom={zoom}
+      address={address}
+      mapsConfig={mapsConfig}
+      metadata={metadata}
+      createSurf={createSurf}
+    />
+  )
 }
 
 Map.defaultProps = {
