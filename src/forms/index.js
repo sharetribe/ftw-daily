0876e0d7b3bde90b00d12d14b9df7be6037e7714
Jenni Laakso <jenni@sharetribe.com>
2020-09-22 11:13:11 +0300

    Create ConfirmSignupForm

--- src/forms/index.js
+++ src/forms/index.js
@@ -1,5 +1,6 @@
 export { default as BookingDatesForm } from './BookingDatesForm/BookingDatesForm';
 export { default as ContactDetailsForm } from './ContactDetailsForm/ContactDetailsForm';
+export { default as ConfirmSignupForm } from './ConfirmSignupForm/ConfirmSignupForm';
 export { default as EditListingAvailabilityForm } from './EditListingAvailabilityForm/EditListingAvailabilityForm';
 export { default as EditListingDescriptionForm } from './EditListingDescriptionForm/EditListingDescriptionForm';
 export { default as EditListingFeaturesForm } from './EditListingFeaturesForm/EditListingFeaturesForm';
