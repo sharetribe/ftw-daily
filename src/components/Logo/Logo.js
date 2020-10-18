af781e1093aacb7ad2631c95bb60990b165d4e17
Aaron Su <aaronsu@cs.washington.edu>
2020-10-17 20:26:51 -0700

    Change default to be for printer

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,24 +1,27 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+import {oneOf, string} from 'prop-types';
 import classNames from 'classnames';
 
 import config from '../../config';
-import IconLogo from './IconLogo';
-import LogoImage from './saunatime-logo.png';
+import MobileLogoImage from './temp2-logo.jpg';
+import DesktopLogoImage from './temp2-logo.jpg';
 import css from './Logo.css';
 
 const Logo = props => {
   const { className, format, ...rest } = props;
-  const mobileClasses = classNames(css.logoMobile, className);
+  const isMobile = format !== 'desktop';
+  const classes = classNames(className, { [css.logoMobile]: isMobile });
+  const logoImage = isMobile ? MobileLogoImage : DesktopLogoImage;
 
-  if (format === 'desktop') {
-    return <img className={className} src={LogoImage} alt={config.siteTitle} {...rest} />;
-  }
-
-  return <IconLogo className={mobileClasses} {...rest} />;
+  return (
+    <img
+      className={classes}
+      src={logoImage}
+      alt={config.siteTitle}
+      {...rest}
+    />
+  );
 };
-
-const { oneOf, string } = PropTypes;
 
 Logo.defaultProps = {
   className: null,
--- src/default-location-searches.js
+++ src/default-location-searches.js
@@ -9,38 +9,17 @@ const { LatLng, LatLngBounds } = sdkTypes;
 // `predictionPlace` (util.types.place) properties.
 export default [
   {
-    id: 'default-helsinki',
+    id: 'default-berkeley',
     predictionPlace: {
-      address: 'Helsinki, Finland',
-      bounds: new LatLngBounds(new LatLng(60.29783, 25.25448), new LatLng(59.92248, 24.78287)),
+      address: 'Berkeley, United States',
+      bounds: new LatLngBounds(new LatLng(37.992, -122.015), new LatLng(37.582, -122.484)),
     },
   },
   {
-    id: 'default-turku',
+    id: 'default-losangeles',
     predictionPlace: {
-      address: 'Turku, Finland',
-      bounds: new LatLngBounds(new LatLng(60.53045, 22.38197), new LatLng(60.33361, 22.06644)),
+      address: 'Los Angeles, United States',
+      bounds: new LatLngBounds(new LatLng(34.669, -117.266), new LatLng(33.213, -118.855)),
     },
-  },
-  {
-    id: 'default-tampere',
-    predictionPlace: {
-      address: 'Tampere, Finland',
-      bounds: new LatLngBounds(new LatLng(61.83657, 24.11838), new LatLng(61.42728, 23.5422)),
-    },
-  },
-  {
-    id: 'default-oulu',
-    predictionPlace: {
-      address: 'Oulu, Finland',
-      bounds: new LatLngBounds(new LatLng(65.56434, 26.77069), new LatLng(64.8443, 24.11494)),
-    },
-  },
-  {
-    id: 'default-ruka',
-    predictionPlace: {
-      address: 'Ruka, Finland',
-      bounds: new LatLngBounds(new LatLng(66.16997, 29.16773), new LatLng(66.16095, 29.13572)),
-    },
-  },
+  }
 ];
