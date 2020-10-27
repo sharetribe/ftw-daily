ec1428c1da06cc54cf8aa928d8550abd7f8864fd
Vesa Luusua <vesa.luusua@gmail.com>
2020-10-28 00:51:07 +0200

    Fix warnings about removed eslint rules

--- src/forms/ContactDetailsForm/ContactDetailsForm.js
+++ src/forms/ContactDetailsForm/ContactDetailsForm.js
@@ -127,7 +127,6 @@ class ContactDetailsFormComponent extends Component {
               </span>
             );
           } else {
-            /* eslint-disable jsx-a11y/no-static-element-interactions */
             resendEmailMessage = (
               <span
                 className={css.helperLink}
@@ -137,7 +136,6 @@ class ContactDetailsFormComponent extends Component {
                 <FormattedMessage id="ContactDetailsForm.resendEmailVerificationText" />
               </span>
             );
-            /* eslint-enable jsx-a11y/no-static-element-interactions */
           }
 
           // Email status info: unverified, verified and pending email (aka changed unverified email)
