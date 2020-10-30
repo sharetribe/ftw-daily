351ebd5c7d2ac884b967c75fa85125e98a8efa3d
zgrnk <jakelyons87@gmail.com>
2020-10-30 15:09:43 +0000

    hero section tweaks, new copy

--- src/components/SectionHero/SectionHero.js
+++ src/components/SectionHero/SectionHero.js
@@ -20,18 +20,21 @@ const SectionHero = props => {
         <h2 className={css.heroSubTitle}>
           <FormattedMessage id="SectionHero.subTitle" />
         </h2>
-        <NamedLink
-          name="SearchPage"
-          to={{
-            search:
-              // 'address=&bounds=61.91109664%2C133.04500644%2C-61.91109664%2C-157.8640886',  // world map
-              'address=&bounds=59.49417013%2C4.15978193%2C49.54972301%2C-10.51994741', // slightly zoomed in UK map
-              // 'address=&bounds=60.9093537990869%2C1.86276379960989%2C49.802416901086%2C-8.74974065661991', // entire UK map
-            }}
-          className={css.heroButton}
-        >
-          <FormattedMessage id="SectionHero.browseButton" />
-        </NamedLink>
+        <div className={css.heroButtonsContatiner}>
+          <NamedLink
+            name="SearchPage"
+            to={{
+              search:
+                'address=&bounds=59.49417013%2C4.15978193%2C49.54972301%2C-10.51994741', // slightly zoomed in UK map
+              }}
+            className={css.heroButton}
+          >
+            <FormattedMessage id="SectionHero.browseButton" />
+          </NamedLink>
+          <NamedLink name="NewListingPage" className={css.heroButton} >
+            <FormattedMessage id="SectionHero.listButton" />
+          </NamedLink>
+        </div>
       </div>
     </div>
   );
