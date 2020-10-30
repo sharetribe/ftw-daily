d241a453f93ff513e5ee600de4a424ce7181afa3
zgrnk <jakelyons87@gmail.com>
2020-10-30 09:19:58 +0000

    how it works section, code clean up

--- src/components/SectionHowItWorks/SectionHowItWorks.js
+++ src/components/SectionHowItWorks/SectionHowItWorks.js
@@ -7,23 +7,9 @@ import {ReactComponent as CalendarIcon} from './svgs/calendar.svg';
 import {ReactComponent as HandShakeIcon} from './svgs/hand_shake.svg';
 import {ReactComponent as SearchIcon} from './svgs/search.svg';
 import {ReactComponent as MoneyIcon} from './svgs/money.svg';
-import {ReactComponent as CreateIcon} from './svgs/create.svg';
+import {ReactComponent as UploadIcon} from './svgs/upload.svg';
 
 import css from './SectionHowItWorks.css';
-
-const userGreyTab =
-  <div
-    className={css.rightSideGreyToggle}
-    onClick={() => onGreyTxtClick()}>
-      <p>I have space</p>
-  </div>;
-
-const hostGreyTab =
-  <div
-    className={css.leftSideGreyToggle}
-    onClick={() => onGreyTxtClick()}>
-      <p>I need space</p>
-  </div>;
 
 const onGreyTxtClick = () => {
   const toggleDiv = document.getElementById("toggle");
@@ -35,14 +21,31 @@ const onGreyTxtClick = () => {
 const SectionHowItWorks = props => {
   const { rootClassName, className } = props;
   const [isUser, setToggle] = useState(true);
-
   const classes = classNames(rootClassName || css.root, className);
-  const typeOfUserTxt = isUser ? 'For Professionals' : 'For Hosts';
-  const toggleCss = isUser ? css.toggleHeaderBlue : css.toggleHeaderGreen;
-  const typeOfUser = <span className={toggleCss}>{typeOfUserTxt}</span>;
-  const svgCss = isUser ? css.svgBlue : css.svgPink;
-  const Step1Icon = isUser ? SearchIcon : CreateIcon;
+
+  const touTxt = isUser ? "SectionHowItWorks.titleToggleUser" : "SectionHowItWorks.titleToggleHost";
+  const toggleCss = isUser ? css.toggleHeaderBlue : css.toggleHeaderPink;
+  const typeOfUser = <span className={toggleCss}><FormattedMessage id={touTxt}/></span>;
+
+  // NOTE to change tglGreyedOutText, must not only change the en.json,
+  // it also must be changed inside of the css file...
+  // SectionHowItWorks.css -> line 101 & line 72 inside of
+  // '.tglButton .checkbox:checked + .knobs:before' AND '.tglButton .knobs:before'
+  const tglGreyedOutText = isUser ? "SectionHowItWorks.toggleGreyTextUser" : "SectionHowItWorks.toggleGreyTextHost";
+  const tglGreyCss = isUser ? css.rightSideGreyToggle : css.leftSideGreyToggle;
+
+  const svgCss = isUser ? classNames(css.svg, css.blue) : classNames(css.svg, css.pink);
+
+  const Step1Icon = isUser ? SearchIcon : UploadIcon;
+  const step1Title = isUser ? "SectionHowItWorks.part1UserTitle" : "SectionHowItWorks.part1HostTitle";
+  const step1Text = isUser ? "SectionHowItWorks.part1UserText" : "SectionHowItWorks.part1HostText";
+
+  const step2Title = isUser ? "SectionHowItWorks.part2UserTitle" : "SectionHowItWorks.part2HostTitle";
+  const step2Text = isUser ? "SectionHowItWorks.part2UserText" : "SectionHowItWorks.part2HostText";
+
   const Step3Icon = isUser ? HandShakeIcon : MoneyIcon;
+  const step3Title = isUser ? "SectionHowItWorks.part3UserTitle" : "SectionHowItWorks.part3HostTitle";
+  const step3Text = isUser ? "SectionHowItWorks.part3UserText" : "SectionHowItWorks.part3HostText";
 
   return (
     <div className={classes}>
@@ -62,57 +65,35 @@ const SectionHowItWorks = props => {
           <input type="checkbox" className={css.checkbox} />
           <div className={css.knobs} />
           <div className={css.layer} />
-          {isUser ? userGreyTab : hostGreyTab}
+          <div className={tglGreyCss} onClick={() => onGreyTxtClick()}>
+              <p><FormattedMessage id={tglGreyedOutText}/></p>
+          </div>
         </div>
       </div>
 
       <div className={css.steps}>
         <div className={css.step}>
           <div className={css.svgDiv}>
-            <Step1Icon className={svgCss} style={{width: 75, height: 85}} />
+            <Step1Icon className={svgCss}/>
           </div>
-          <h2 className={css.stepTitle}>
-            <FormattedMessage
-              id={ isUser ? "SectionHowItWorks.part1UserTitle" : "SectionHowItWorks.part1HostTitle"}
-            />
-          </h2>
-          <p>
-            <FormattedMessage
-              id={ isUser ? "SectionHowItWorks.part1UserText" : "SectionHowItWorks.part1HostText"}
-            />
-          </p>
+          <h2 className={css.stepTitle}><FormattedMessage id={step1Title}/></h2>
+          <p><FormattedMessage id={step1Text}/></p>
         </div>
 
         <div className={css.step}>
           <div className={css.svgDiv}>
-            <CalendarIcon className={svgCss} style={{width: 75, height: 85}} />
+            <CalendarIcon className={svgCss}/>
           </div>
-          <h2 className={css.stepTitle}>
-            <FormattedMessage
-              id={ isUser ? "SectionHowItWorks.part2UserTitle" : "SectionHowItWorks.part2HostTitle"}
-            />
-          </h2>
-          <p>
-            <FormattedMessage
-              id={ isUser ? "SectionHowItWorks.part2UserText" : "SectionHowItWorks.part2HostText"}
-            />
-          </p>
+          <h2 className={css.stepTitle}><FormattedMessage id={step2Title}/></h2>
+          <p><FormattedMessage id={step2Text}/></p>
         </div>
 
         <div className={css.step}>
           <div className={css.svgDiv}>
-            <Step3Icon className={svgCss} style={{width: 75, height: 85}} />
+            <Step3Icon className={svgCss}/>
           </div>
-          <h2 className={css.stepTitle}>
-            <FormattedMessage
-              id={ isUser ? "SectionHowItWorks.part3UserTitle" : "SectionHowItWorks.part3HostTitle"}
-            />
-          </h2>
-          <p>
-            <FormattedMessage
-              id={ isUser ? "SectionHowItWorks.part3UserText" : "SectionHowItWorks.part3HostText"}
-            />
-          </p>
+          <h2 className={css.stepTitle}><FormattedMessage id={step3Title}/></h2>
+          <p><FormattedMessage id={step3Text}/></p>
         </div>
       </div>
     </div>
