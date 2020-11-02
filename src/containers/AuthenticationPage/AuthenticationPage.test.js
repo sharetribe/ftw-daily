4b7b7d7ee0a80843e87f82572ec58fd6b63762a5
Jenni Laakso <jenni@sharetribe.com>
2020-11-02 15:50:50 +0200

    AuhtenticationPage.test.js: add tests for showing Google or Facebook or both

--- src/containers/AuthenticationPage/AuthenticationPage.test.js
+++ src/containers/AuthenticationPage/AuthenticationPage.test.js
@@ -6,10 +6,101 @@ import { AuthenticationPageComponent } from './AuthenticationPage';
 const noop = () => null;
 
 describe('AuthenticationPageComponent', () => {
-  // We need to overwrite REACT_APP_FACEBOOK_APP_ID before running the test
+  // We need to overwrite social login client ids before running the test
   // to make sure it's same in local environment and in CI
   beforeEach(() => {
     process.env = Object.assign(process.env, { REACT_APP_FACEBOOK_APP_ID: '' });
+    process.env = Object.assign(process.env, { REACT_APP_GOOGLE_CLIENT_ID: '' });
+  });
+
+  it('matches snapshot', () => {
+    const props = {
+      history: { push: noop },
+      location: { state: { from: '/protected' } },
+      tab: 'login',
+      isAuthenticated: false,
+      authInProgress: false,
+      scrollingDisabled: false,
+      currentUserHasListings: false,
+      onLogout: noop,
+      onManageDisableScrolling: noop,
+      submitLogin: noop,
+      submitSignup: noop,
+      intl: fakeIntl,
+      sendVerificationEmailInProgress: false,
+      onResendVerificationEmail: noop,
+    };
+    const tree = renderShallow(<AuthenticationPageComponent {...props} />);
+    expect(tree).toMatchSnapshot();
+  });
+});
+
+describe('AuthenticationPageComponent with Facebook login', () => {
+  // We need to overwrite social login client ids before running the test
+  // to make sure it's same in local environment and in CI
+  beforeEach(() => {
+    process.env = Object.assign(process.env, { REACT_APP_FACEBOOK_APP_ID: 'test' });
+    process.env = Object.assign(process.env, { REACT_APP_GOOGLE_CLIENT_ID: '' });
+  });
+
+  it('matches snapshot', () => {
+    const props = {
+      history: { push: noop },
+      location: { state: { from: '/protected' } },
+      tab: 'login',
+      isAuthenticated: false,
+      authInProgress: false,
+      scrollingDisabled: false,
+      currentUserHasListings: false,
+      onLogout: noop,
+      onManageDisableScrolling: noop,
+      submitLogin: noop,
+      submitSignup: noop,
+      intl: fakeIntl,
+      sendVerificationEmailInProgress: false,
+      onResendVerificationEmail: noop,
+    };
+    const tree = renderShallow(<AuthenticationPageComponent {...props} />);
+    expect(tree).toMatchSnapshot();
+  });
+});
+
+describe('AuthenticationPageComponent with Google login', () => {
+  // We need to overwrite social login client ids before running the test
+  // to make sure it's same in local environment and in CI
+  beforeEach(() => {
+    process.env = Object.assign(process.env, { REACT_APP_FACEBOOK_APP_ID: '' });
+    process.env = Object.assign(process.env, { REACT_APP_GOOGLE_CLIENT_ID: 'test' });
+  });
+
+  it('matches snapshot', () => {
+    const props = {
+      history: { push: noop },
+      location: { state: { from: '/protected' } },
+      tab: 'login',
+      isAuthenticated: false,
+      authInProgress: false,
+      scrollingDisabled: false,
+      currentUserHasListings: false,
+      onLogout: noop,
+      onManageDisableScrolling: noop,
+      submitLogin: noop,
+      submitSignup: noop,
+      intl: fakeIntl,
+      sendVerificationEmailInProgress: false,
+      onResendVerificationEmail: noop,
+    };
+    const tree = renderShallow(<AuthenticationPageComponent {...props} />);
+    expect(tree).toMatchSnapshot();
+  });
+});
+
+describe('AuthenticationPageComponent with Google and Facebook login', () => {
+  // We need to overwrite social login client ids before running the test
+  // to make sure it's same in local environment and in CI
+  beforeEach(() => {
+    process.env = Object.assign(process.env, { REACT_APP_FACEBOOK_APP_ID: 'test' });
+    process.env = Object.assign(process.env, { REACT_APP_GOOGLE_CLIENT_ID: 'test' });
   });
 
   it('matches snapshot', () => {
