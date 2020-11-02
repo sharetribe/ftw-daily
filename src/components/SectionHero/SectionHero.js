e0619b9863c22ae71cc04f7d46e4a51dccee5d02
zgrnk <jakelyons87@gmail.com>
2020-11-02 14:53:43 +0000

    updated hero, mobile css tweaks

--- src/components/SectionHero/SectionHero.js
+++ src/components/SectionHero/SectionHero.js
@@ -6,10 +6,13 @@ import { NamedLink } from '../../components';
 
 import css from './SectionHero.css';
 
+// import mobileImage from './images/heroMobile.png';
+
 const SectionHero = props => {
   const { rootClassName, className } = props;
 
   const classes = classNames(rootClassName || css.root, className);
+  const makeSpaceWork = <span className={css.bold}><FormattedMessage id="SectionHero.subTitleBold" /></span>;
 
   return (
     <div className={classes}>
@@ -18,7 +21,10 @@ const SectionHero = props => {
           <FormattedMessage id="SectionHero.title" />
         </h1>
         <h2 className={css.heroSubTitle}>
-          <FormattedMessage id="SectionHero.subTitle" />
+          <FormattedMessage
+            id="SectionHero.subTitle"
+            values={{ subText: makeSpaceWork }}
+          />
         </h2>
         <div className={css.heroButtonsContatiner}>
           <NamedLink
@@ -27,7 +33,7 @@ const SectionHero = props => {
               search:
                 'address=&bounds=59.49417013%2C4.15978193%2C49.54972301%2C-10.51994741', // slightly zoomed in UK map
               }}
-            className={css.heroButton}
+            className={css.heroButtonPink}
           >
             <FormattedMessage id="SectionHero.browseButton" />
           </NamedLink>
