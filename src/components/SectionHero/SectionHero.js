a8b911f938019a75e7338244ea53bcf6f50672d2
zgrnk <jakelyons87@gmail.com>
2020-11-09 17:46:43 +0000

    fix new hero for mobile

--- src/components/SectionHero/SectionHero.js
+++ src/components/SectionHero/SectionHero.js
@@ -6,7 +6,7 @@ import { NamedLink } from '../../components';
 
 import css from './SectionHero.css';
 
-// import mobileImage from './images/heroMobile.png';
+import mobileImage from './images/heroMobile.png';
 
 const SectionHero = props => {
   const { rootClassName, className } = props;
@@ -16,32 +16,41 @@ const SectionHero = props => {
 
   return (
     <div className={classes}>
-      <div className={css.heroContent}>
-        <h1 className={css.heroMainTitle}>
-          <FormattedMessage id="SectionHero.title" />
-        </h1>
-        <h2 className={css.heroSubTitle}>
-          <FormattedMessage
-            id="SectionHero.subTitle"
-            values={{ subText: makeSpaceWork }}
-          />
-        </h2>
-        <div className={css.heroButtonsContatiner}>
-          <NamedLink
-            name="SearchPage"
-            to={{
-              search:
-                'address=&bounds=59.49417013%2C4.15978193%2C49.54972301%2C-10.51994741', // slightly zoomed in UK map
-              }}
-            className={css.heroButtonPink}
-          >
-            <FormattedMessage id="SectionHero.browseButton" />
-          </NamedLink>
-          <NamedLink name="NewListingPage" className={css.heroButton} >
-            <FormattedMessage id="SectionHero.listButton" />
-          </NamedLink>
+      <div className={css.flexContainer}>
+        <div className={css.heroTextContent}>
+          <h1 className={css.heroMainTitle}>
+            <FormattedMessage id="SectionHero.title" />
+          </h1>
+          <h2 className={css.heroSubTitle}>
+            <FormattedMessage
+              id="SectionHero.subTitle"
+              values={{ subText: makeSpaceWork }}
+            />
+          </h2>
+          <div className={css.heroButtonsContatiner}>
+            <NamedLink
+              name="SearchPage"
+              to={{
+                search:
+                  'address=&bounds=59.49417013%2C4.15978193%2C49.54972301%2C-10.51994741', // slightly zoomed in UK map
+                }}
+              className={css.heroButtonPink}
+            >
+              <FormattedMessage id="SectionHero.browseButton" />
+            </NamedLink>
+            <NamedLink name="NewListingPage" className={css.heroButton} >
+              <FormattedMessage id="SectionHero.listButton" />
+            </NamedLink>
+          </div>
+        </div>
+        <div className={css.heroImgContent}>
+          {/* <div className={css.emptyMobileDiv} /> */}
+          <div className={css.mobileImgContainer}>
+            <img src={mobileImage} alt='hotpatch make space work' className={css.mobileImage}/>
+          </div>
         </div>
       </div>
+
     </div>
   );
 };
