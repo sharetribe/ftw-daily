51b51c9d24a3380a5e6ec92c8be3d9616bfb956f
trungle <trung@mission.plus>
2021-01-29 01:57:11 -0800

    add slider to home page

--- src/components/SectionHero/SectionHero.js
+++ src/components/SectionHero/SectionHero.js
@@ -1,37 +1,109 @@
-import React from 'react';
+import React, { Fragment, useState } from 'react';
 import { string } from 'prop-types';
-import { FormattedMessage } from '../../util/reactIntl';
 import classNames from 'classnames';
 import { NamedLink } from '../../components';
+import Carousel, { Dots } from '@brainhubeu/react-carousel';
+import '@brainhubeu/react-carousel/lib/style.css';
 
 import css from './SectionHero.module.css';
 
+import bass from './images/bass.jpg';
+import dizay from './images/dizay.jpg';
+import dreadbox from './images/dreadbox.jpg';
+import guitaracoutic from './images/guitaracoutic.jpg';
+import guitarelectric from './images/guitarelectric.jpg';
+import piano from './images/piano.jpg';
+
 const SectionHero = props => {
   const { rootClassName, className } = props;
+  const [value, setValue] = useState(0);
+  const onChange = (item) => {
+    setValue(item)
+  }
 
   const classes = classNames(rootClassName || css.root, className);
-
   return (
-    <div className={classes}>
-      <div className={css.heroContent}>
-        <h1 className={css.heroMainTitle}>
-          <FormattedMessage id="SectionHero.title" />
-        </h1>
-        <h2 className={css.heroSubTitle}>
-          <FormattedMessage id="SectionHero.subTitle" />
-        </h2>
+    <Fragment>
+      <Carousel
+        onChange={onChange}
+        value={value}
+        slidesPerPage={3}
+        slidesPerScroll={1}
+        animationSpeed={1500}
+        autoPlay={5000}
+        stopAutoPlayOnHover
+        infinite
+        clickToChange
+        centered
+        itemWidth={920}
+        offset={20}
+        breakpoints={{
+          1000: {
+            slidesPerPage: 3,
+            clickToChange: true,
+            centered: true,
+            infinite: true,
+          },
+          500: {
+            slidesPerPage: 1,
+            slidesPerScroll: 1,
+            clickToChange: true,
+            centered: true,
+            infinite: true,
+          },
+        }}
+      >
         <NamedLink
           name="SearchPage"
           to={{
-            search:
-              '',
+            search: '',
           }}
-          className={css.heroButton}
         >
-          <FormattedMessage id="SectionHero.browseButton" />
+          <img src={bass} />
         </NamedLink>
-      </div>
-    </div>
+        <NamedLink
+          name="SearchPage"
+          to={{
+            search: '',
+          }}
+        >
+          <img src={dizay} />
+        </NamedLink>
+        <NamedLink
+          name="SearchPage"
+          to={{
+            search: '',
+          }}
+        >
+          <img src={dreadbox} />
+        </NamedLink>
+        <NamedLink
+          name="SearchPage"
+          to={{
+            search: '',
+          }}
+        >
+          <img src={guitaracoutic} />
+        </NamedLink>
+        <NamedLink
+          name="SearchPage"
+          to={{
+            search: '',
+          }}
+        >
+          <img src={guitarelectric} />
+        </NamedLink>
+        <NamedLink
+          name="SearchPage"
+          to={{
+            search: '',
+          }}
+        >
+          <img src={piano} />
+        </NamedLink>
+      </Carousel>
+      <Dots value={value} onChange={onChange} number={6} />
+    </Fragment>
   );
 };
 
--- src/containers/LandingPage/LandingPage.js
+++ src/containers/LandingPage/LandingPage.js
@@ -60,7 +60,8 @@ export const LandingPageComponent = props => {
         </LayoutWrapperTopbar>
         <LayoutWrapperMain>
           <div className={css.heroContainer}>
-            <SectionHero className={css.hero} history={history} location={location} />
+            {/* <SectionHero className={css.hero} history={history} location={location} /> */}
+            <SectionHero />
           </div>
           <ul className={css.sections}>
             <li className={css.section}>
