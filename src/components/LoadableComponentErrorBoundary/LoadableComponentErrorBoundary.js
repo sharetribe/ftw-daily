4ab4d052cff19b2b2cecf3b2669e6725994f84cc
Vesa Luusua <vesa.luusua@gmail.com>
2021-04-09 15:04:58 +0300

    Fix: LoadableComponentErrorBoundary should be used in prod, not in dev

--- src/components/LoadableComponentErrorBoundary/LoadableComponentErrorBoundary.js
+++ src/components/LoadableComponentErrorBoundary/LoadableComponentErrorBoundary.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+
+import config from '../../config';
+
 import { LoadableComponentErrorBoundaryPage } from './LoadableComponentErrorBoundaryPage';
 
 // Use ErrorBoyndary to catch ChunkLoadError
@@ -23,4 +26,16 @@ class LoadableComponentErrorBoundary extends Component {
   }
 }
 
-export default LoadableComponentErrorBoundary;
+// LoadableComponentErrorBoundary helps in situations
+// where production build changes in the server and
+// long-living client app tries to fetch code chunks that don't exist anymore.
+// Note: in development mode with Hot Module Reloading (HMR) in use, this causes error loops.
+const UseLoadableErrorBoundaryOnlyInProdutionMode = props => {
+  const { children } = props;
+  return config.dev ? (
+    children
+  ) : (
+    <LoadableComponentErrorBoundary>{children}</LoadableComponentErrorBoundary>
+  );
+};
+export default UseLoadableErrorBoundaryOnlyInProdutionMode;
