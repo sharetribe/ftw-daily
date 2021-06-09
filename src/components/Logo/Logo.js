753b77a3b2f44f3cbe4daefeea16a66dadbc6aab
Doris Sun <dorissun@Doriss-MacBook-Pro.local>
2021-06-08 20:17:03 -0400

    branding customization

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -3,19 +3,19 @@ import PropTypes from 'prop-types';
 import classNames from 'classnames';
 
 import config from '../../config';
-import IconLogo from './IconLogo';
-import LogoImage from './saunatime-logo.png';
+//import IconLogo from './IconLogo';
+import LogoImage from './kichi-logo.jpg';
 import css from './Logo.module.css';
 
 const Logo = props => {
   const { className, format, ...rest } = props;
   const mobileClasses = classNames(css.logoMobile, className);
 
-  if (format === 'desktop') {
+  //if (format === 'desktop') {
     return <img className={className} src={LogoImage} alt={config.siteTitle} {...rest} />;
-  }
+  //}
 
-  return <IconLogo className={mobileClasses} {...rest} />;
+  //return <IconLogo className={mobileClasses} {...rest} />;
 };
 
 const { oneOf, string } = PropTypes;
