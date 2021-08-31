6b73b411983d4547f060224bcd457b2c62dbc94a
Vesa Luusua <vesa.luusua@gmail.com>
2021-08-31 12:47:11 +0300

    Update react-intl v5.20.10 (from v3.1.13)

--- src/util/reactIntl.js
+++ src/util/reactIntl.js
@@ -1,11 +1,5 @@
 import { func, shape } from 'prop-types';
-import {
-  IntlProvider,
-  FormattedMessage,
-  FormattedDate,
-  FormattedHTMLMessage,
-  injectIntl,
-} from 'react-intl/dist/react-intl';
+import { IntlProvider, FormattedMessage, FormattedDate, injectIntl } from 'react-intl';
 // Note: we import React Intl directly from dist directory, because
 // by default the library assumes the usage of ES modules
 // and that don't work with server-side rendering.
@@ -13,18 +7,10 @@ import {
 
 const intlShape = shape({
   formatDate: func.isRequired,
-  formatHTMLMessage: func.isRequired,
   formatMessage: func.isRequired,
   formatNumber: func.isRequired,
   formatPlural: func.isRequired,
   formatRelativeTime: func.isRequired,
   formatTime: func.isRequired,
 });
-export {
-  IntlProvider,
-  FormattedMessage,
-  FormattedDate,
-  FormattedHTMLMessage,
-  injectIntl,
-  intlShape,
-};
+export { IntlProvider, FormattedMessage, FormattedDate, injectIntl, intlShape };
