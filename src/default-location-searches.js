1fab9f4c81d369726c70c3e98b4ba2099fcd3ebf
dolinakaryna <karyna.dolina@roobykon.com>
2021-10-19 20:28:08 +0300

    53025 fix adress

--- src/default-location-searches.js
+++ src/default-location-searches.js
@@ -11,42 +11,42 @@ const defaultLocations = [
   {
     id: 'default-london',
     predictionPlace: {
-      address: 'London',
+      address: 'London, United Kingdom',
       bounds: new LatLngBounds(new LatLng(51.67789511, 0.29732496), new LatLng(51.3925498, -0.57645)),
     },
   },
   {
     id: 'default-birmingham',
     predictionPlace: {
-      address: 'Birmingham',
+      address: 'Birmingham, United Kingdom',
       bounds: new LatLngBounds(new LatLng(52.57779203, -1.72540735), new LatLng(52.32126288, -2.03104471)),
     },
   },
   {
     id: 'default-manchester',
     predictionPlace: {
-      address: 'Manchester',
+      address: 'Manchester, United Kingdom',
       bounds: new LatLngBounds(new LatLng(53.59821106, -2.08320402), new LatLng(53.3476832, -2.38884138)),
     },
   },
   {
     id: 'default-liverpool',
     predictionPlace: {
-      address: 'Liverpool',
+      address: 'Liverpool, United Kingdom',
       bounds: new LatLngBounds(new LatLng(53.5729014, -2.77243704), new LatLng(53.31894244, -3.08142752)),
     },
   },
   {
     id: 'default-edinburgh',
     predictionPlace: {
-      address: 'Edinburgh',
+      address: 'Edinburgh, United Kingdom',
       bounds: new LatLngBounds(new LatLng(56.21396061, -2.98871222), new LatLng(55.74296152, -3.58918372)),
     },
   },
   {
     id: 'default-sheffield',
     predictionPlace: {
-      address: 'Sheffield',
+      address: 'Sheffield, United Kingdom',
       bounds: new LatLngBounds(new LatLng(53.40297225, -1.43730778), new LatLng(53.34019056, -1.51371712)),
     },
   },
