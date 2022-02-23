5ff4dd465289e57d5271d0cf86371896fcc35cb2
Duc-Lam-jh <duclam227@gmail.com>
2022-02-23 13:46:05 +0700

    change default availability to unavailable until user select

--- src/components/EditListingAvailabilityPanel/EditListingAvailabilityPanel.js
+++ src/components/EditListingAvailabilityPanel/EditListingAvailabilityPanel.js
@@ -31,16 +31,17 @@ const EditListingAvailabilityPanel = props => {
   const defaultAvailabilityPlan = {
     type: 'availability-plan/day',
     entries: [
-      { dayOfWeek: 'mon', seats: 1 },
-      { dayOfWeek: 'tue', seats: 1 },
-      { dayOfWeek: 'wed', seats: 1 },
-      { dayOfWeek: 'thu', seats: 1 },
-      { dayOfWeek: 'fri', seats: 1 },
-      { dayOfWeek: 'sat', seats: 1 },
-      { dayOfWeek: 'sun', seats: 1 },
+      { dayOfWeek: 'mon', seats: 0 },
+      { dayOfWeek: 'tue', seats: 0 },
+      { dayOfWeek: 'wed', seats: 0 },
+      { dayOfWeek: 'thu', seats: 0 },
+      { dayOfWeek: 'fri', seats: 0 },
+      { dayOfWeek: 'sat', seats: 0 },
+      { dayOfWeek: 'sun', seats: 0 },
     ],
   };
   const availabilityPlan = currentListing.attributes.availabilityPlan || defaultAvailabilityPlan;
+  console.log(currentListing.attributes.availabilityPlan);
 
   return (
     <div className={classes}>
