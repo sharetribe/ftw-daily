bd24d45ca52e0402377ceaefca032f719e98be40
sentral09 <jude@hotpatch.com>
2022-05-20 12:56:15 +0100

    changing schemaImage

--- src/containers/LandingPage/LandingPage.js
+++ src/containers/LandingPage/LandingPage.js
@@ -25,8 +25,8 @@ import {
 } from '../../components';
 import { TopbarContainer } from '../../containers';
 
-import facebookImage from '../../assets/HotPatchLogoNew.png';
-import twitterImage from '../../assets/HotPatchLogoNew.png';
+import facebookImage from '../../assets/HotPatchLogoNew-458x84.png';
+import twitterImage from '../../assets/HotPatchLogoNew-458x84.png';
 import css from './LandingPage.module.css';
 
 export const LandingPageComponent = props => {
