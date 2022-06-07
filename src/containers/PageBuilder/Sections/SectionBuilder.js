e5caa036f02c5af055745b67fcbe634e9d210447
Vesa Luusua <vesa.luusua@gmail.com>
2022-06-07 16:43:34 +0300

    Update SectionBuilder.js to use separated section components

--- src/containers/PageBuilder/Sections/SectionBuilder.js
+++ src/containers/PageBuilder/Sections/SectionBuilder.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import classNames from 'classnames';
 
-import Field, { pickValidProps } from '../Fields/Field.js';
-import BlockBuilder, { BlockTag, BlockDefault } from '../Blocks/BlockBuilder.js';
+import { BlockTag, BlockDefault } from '../Blocks/BlockBuilder.js';
+
+// Section components
+import SectionColumns from './SectionColumns.js';
+import SectionArticle from './SectionArticle.js';
+
 import css from './SectionBuilder.module.css';
 
 /////////////////////
@@ -38,118 +42,6 @@ const SectionTag = props => {
   );
 };
 
-/////////////////////
-// Custom sections //
-/////////////////////
-
-// TODO This is only an example. There's no example for this yet
-const SectionArticle = props => {
-  const { sectionId, title, background, backgroundImage, blocks, theme, options } = props;
-
-  // If external mapping has been included for fields
-  // E.g. { h1: { component: MyAwesomeHeader } }
-  const fieldComponents = options?.fieldComponents;
-  const fieldOptions = { fieldComponents };
-
-  // TODO do we support this in Console?
-  // Find background color if it is included
-  const fixedBackgroundData = { ...background, type: 'hexColor' }; // TODO remove if type is included
-  const colorProp = pickValidProps(fixedBackgroundData, fieldOptions);
-  const backgroundColorMaybe = colorProp.color ? { backgroundColor: colorProp.color } : {};
-
-  // TODO do we support this in Console?
-  // Create Image field for background image
-  // This will be passed to SectionTag as responsive "background" image
-  const backgroundImageMaybe = backgroundImage
-    ? {
-        backgroundImage: (
-          <Field
-            data={{ ...backgroundImage, type: 'backgroundImage', bgColor: '#00AAFF' }}
-            className={css.backgroundImage}
-            options={fieldOptions}
-          />
-        ),
-      }
-    : {};
-
-  return (
-    <SectionTag id={sectionId} style={backgroundColorMaybe} {...backgroundImageMaybe} theme={theme}>
-      <header className={css.sectionDetails}>
-        <Field className={css.title} data={title} options={fieldOptions} />
-      </header>
-      {blocks ? (
-        <div className={css.articleMain}>
-          <BlockBuilder blocks={blocks} options={options} />
-        </div>
-      ) : null}
-    </SectionTag>
-  );
-};
-
-const SectionColumns = props => {
-  const {
-    sectionId,
-    numColumns,
-    title,
-    ingress,
-    background,
-    backgroundImage,
-    callToAction,
-    theme = 'light',
-    blocks,
-    options,
-  } = props;
-
-  // If external mapping has been included for fields
-  // E.g. { h1: { component: MyAwesomeHeader } }
-  const fieldComponents = options?.fieldComponents;
-  const fieldOptions = { fieldComponents };
-
-  // Find background color if it is included
-  const fixedBackgroundData = { ...background, type: 'hexColor' }; // TODO remove if type is included
-  const colorProp = pickValidProps(fixedBackgroundData, fieldOptions);
-  const backgroundColorMaybe = colorProp.color ? { backgroundColor: colorProp.color } : {};
-
-  // Create Image field for background image
-  // This will be passed to SectionTag as responsive "background" image
-  const backgroundImageMaybe = backgroundImage
-    ? {
-        backgroundImage: (
-          <Field
-            data={{ ...backgroundImage, type: 'backgroundImage', bgColor: '#aae3ff' }}
-            className={css.backgroundImage}
-            options={fieldOptions}
-          />
-        ),
-      }
-    : {};
-
-  // Select class for column container
-  const columnsClasses =
-    numColumns === 4
-      ? css.columns4
-      : numColumns === 3
-      ? css.columns3
-      : numColumns === 2
-      ? css.columns2
-      : css.columns1;
-
-  return (
-    <SectionTag id={sectionId} style={backgroundColorMaybe} {...backgroundImageMaybe} theme={theme}>
-      <header className={css.sectionDetails}>
-        <Field data={title} className={css.title} options={fieldOptions} />
-        <Field data={ingress} className={css.ingress} options={fieldOptions} />
-        <Field data={callToAction} className={css.ctaButton} options={fieldOptions} />
-      </header>
-      {blocks ? (
-        <div className={columnsClasses}>
-          <BlockBuilder blocks={blocks} ctaButtonClass={css.ctaButton} options={options} />
-        </div>
-      ) : null}
-    </SectionTag>
-  );
-};
-
 //////////////////////
 // Section builder //
 //////////////////////
@@ -175,12 +67,32 @@ const SectionBuilder = props => {
     return config?.component;
   };
 
+  // These are shared classes.
+  // Use these to have consistent styles between different section components
+  // E.g. share the same title styles
+  const defaultClasses = {
+    backgroundImage: css.backgroundImage,
+    sectionDetails: css.sectionDetails,
+    title: css.title,
+    ingress: css.ingress,
+    ctaButton: css.ctaButton,
+    singleColumn: css.singleColumn,
+  };
+
   return (
     <>
       {sections.map(section => {
         const Section = getComponent(section.sectionType);
         if (Section) {
-          return <Section key={section.sectionId} options={otherOption} {...section} />;
+          return (
+            <Section
+              key={section.sectionId}
+              tag={SectionTag}
+              defaultClasses={defaultClasses}
+              options={otherOption}
+              {...section}
+            />
+          );
         } else {
           // If the section type is unknown, the app can't know what to render
           console.warn(`Unknown section type (${section.sectionType}) detected.`);
