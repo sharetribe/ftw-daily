8bdd9b42e0596eee2cdf5d81c8e9ddd9cbb796fa
Vesa Luusua <vesa.luusua@gmail.com>
2022-06-10 21:30:28 +0300

    Sanitize URL (essentially, Braintree's sanitizer)

--- src/util/sanitize.js
+++ src/util/sanitize.js
@@ -24,6 +24,55 @@ const sanitizeText = str =>
     ? str.replace(ESCAPE_TEXT_REGEXP, ch => ESCAPE_TEXT_REPLACEMENTS[ch])
     : '';
 
+// URL sanitizer. This code is adapted from
+// https://github.com/braintree/sanitize-url/
+// <sanitizeUrl>
+const INVALID_PROTOCOL_REGEXP = /^([^\w]*)(javascript|data|vbscript)/im;
+const HTML_ENTITIES_REGEXP = /&#(\w+)(^\w|;)?/g;
+const CTRL_CHARACTERS_REGEXP = /[\u0000-\u001F\u007F-\u009F\u2000-\u200D\uFEFF]/gim;
+const URL_SCHEME_REGEXP = /^([^:]+):/gm;
+const RELATIVE_FIRST_CHARACTERS = ['.', '/'];
+
+function isRelativeUrlWithoutProtocol(url) {
+  return RELATIVE_FIRST_CHARACTERS.indexOf(url[0]) > -1;
+}
+
+// adapted from https://stackoverflow.com/a/29824550/2601552
+function decodeHtmlCharacters(str) {
+  return str.replace(HTML_ENTITIES_REGEXP, (match, dec) => {
+    return String.fromCharCode(dec);
+  });
+}
+
+export function sanitizeUrl(url) {
+  const sanitizedUrl = decodeHtmlCharacters(url || '')
+    .replace(CTRL_CHARACTERS_REGEXP, '')
+    .trim();
+
+  if (!sanitizedUrl) {
+    return 'about:blank';
+  }
+
+  if (isRelativeUrlWithoutProtocol(sanitizedUrl)) {
+    return sanitizedUrl;
+  }
+
+  const urlSchemeParseResults = sanitizedUrl.match(URL_SCHEME_REGEXP);
+
+  if (!urlSchemeParseResults) {
+    return sanitizedUrl;
+  }
+
+  const urlScheme = urlSchemeParseResults[0];
+
+  if (INVALID_PROTOCOL_REGEXP.test(urlScheme)) {
+    return 'about:blank';
+  }
+
+  return sanitizedUrl;
+}
+// </sanitizeUrl>
+
 /**
  * Sanitize user entity.
  * If you add public data, you should probably sanitize it here.
