fa2e21d590f040ac0f9f74b6534f7624463dcf91
Vesa Luusua <vesa.luusua@gmail.com>
2022-07-15 01:18:41 +0300

    Update LandingPage container to start using PageBuilder Include initial version of fallback page/asset app.test: update to work with new LandingPage setup

---
+++ src/containers/LandingPage/FallbackPage.js
@@ -0,0 +1,91 @@
+import React from 'react';
+
+import PageBuilder from '../PageBuilder/PageBuilder';
+
+// Create fallback content (array of sections) in page asset format:
+export const fallbackSections = {
+  sections: [
+    {
+      sectionType: 'features',
+      sectionId: 'hero',
+      background: { type: 'hexColor', color: '#ffff00' },
+      // backgroundImage: {
+      //   type: 'image',
+      //   alt: 'Background image',
+      //   image: {
+      //     id: 'image',
+      //     type: 'imageAsset',
+      //     attributes: {
+      //       variants: {
+      //         square1x: {
+      //           url: `https://picsum.photos/400/400`,
+      //           width: 400,
+      //           height: 400,
+      //         },
+      //         square2x: {
+      //           url: `https://picsum.photos/800/800`,
+      //           width: 800,
+      //           height: 800,
+      //         },
+      //       },
+      //     },
+      //   },
+      // },
+      blocks: [
+        {
+          blockType: 'default-block',
+          blockId: 'hero-content',
+          media: {
+            type: 'image',
+            alt: 'First image',
+            image: {
+              id: 'image',
+              type: 'imageAsset',
+              attributes: {
+                variants: {
+                  square1x: {
+                    url: `https://picsum.photos/400/400`,
+                    width: 400,
+                    height: 400,
+                  },
+                  square2x: {
+                    url: `https://picsum.photos/800/800`,
+                    width: 800,
+                    height: 800,
+                  },
+                },
+              },
+            },
+          },
+          title: { type: 'heading1', content: 'My marketplace' },
+          text: {
+            type: 'markdown',
+            content:
+              '### My unique marketplace for booking listings\n### You can also list your services here!',
+          },
+          callToAction: {
+            type: 'internalButtonLink',
+            href: '/s',
+            label: 'Browse marketplace',
+          },
+        },
+      ],
+    },
+  ],
+};
+
+// This is the fallback page, in case there's no Privacy Policy asset defined in Console.
+const FallbackPage = props => {
+  const { title, description, schema, contentType } = props;
+  return (
+    <PageBuilder
+      pageAssetsData={fallbackSections}
+      title={title}
+      description={description}
+      schema={schema}
+      contentType={contentType}
+    />
+  );
+};
+
+export default FallbackPage;
