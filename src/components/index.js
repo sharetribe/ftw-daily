35ae61635f0bf7edaa8d6e9ba421321bee2830c6
Vesa Luusua <vesa.luusua@gmail.com>
2022-08-30 14:19:29 +0300

    TermsOfServicePage is built with PageBuilder and Asset Delivery API. It defaults to fallback page

--- src/components/index.js
+++ src/components/index.js
@@ -158,7 +158,6 @@ export {default as LoadableComponentErrorBoundary } from './LoadableComponentErr
 export { default as ModalMissingInformation } from './ModalMissingInformation/ModalMissingInformation';
 export { default as ReviewModal } from './ReviewModal/ReviewModal';
 export { default as PrivacyPolicy } from './PrivacyPolicy/PrivacyPolicy';
-export { default as TermsOfService } from './TermsOfService/TermsOfService';
 export { default as EditListingAvailabilityPanel } from './EditListingAvailabilityPanel/EditListingAvailabilityPanel';
 export { default as EditListingDescriptionPanel } from './EditListingDescriptionPanel/EditListingDescriptionPanel';
 export { default as EditListingFeaturesPanel } from './EditListingFeaturesPanel/EditListingFeaturesPanel';
