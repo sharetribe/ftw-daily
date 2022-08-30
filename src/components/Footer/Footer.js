8d0be5c358f4ce71b717839cd22269009cdd8a1c
Vesa Luusua <vesa.luusua@gmail.com>
2022-08-30 17:10:43 +0300

    Remove hard-coded AboutPage in favor of CMSPage and about page asset

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -87,7 +87,7 @@ const Footer = props => {
                   </NamedLink>
                 </li>
                 <li className={css.listItem}>
-                  <NamedLink name="AboutPage" className={css.link}>
+                  <NamedLink name="CMSPage" params={{ pageId: 'about' }} className={css.link}>
                     <FormattedMessage id="Footer.toAboutPage" />
                   </NamedLink>
                 </li>
@@ -102,7 +102,12 @@ const Footer = props => {
                   </NamedLink>
                 </li>
                 <li className={css.listItem}>
-                  <NamedLink name="AboutPage" to={{ hash: '#contact' }} className={css.link}>
+                  <NamedLink
+                    name="CMSPage"
+                    params={{ pageId: 'about' }}
+                    to={{ hash: '#contact' }}
+                    className={css.link}
+                  >
                     <FormattedMessage id="Footer.toContactPage" />
                   </NamedLink>
                 </li>
