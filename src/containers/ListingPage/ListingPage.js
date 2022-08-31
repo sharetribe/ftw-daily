d36f9eb7f16438afd00debb83a97c702720be5b4
victor vorobyov <victor.vorobyov@roobykon.com>
2022-08-31 22:44:30 +0800

    Fix display amount and hide error

--- src/containers/ListingPage/ListingPage.js
+++ src/containers/ListingPage/ListingPage.js
@@ -73,7 +73,7 @@ const MIN_LENGTH_FOR_LONG_WORDS_IN_TITLE = 16;
 const { UUID, Money } = sdkTypes;
 
 const priceData = (price, intl) => {
-  if (price && price.currency === config.currency) {
+  if (price && price.currency === config.currency || price.currency === config.additionalCurrency) {
     const formattedPrice = formatMoney(intl, price);
     return { formattedPrice, priceTitle: formattedPrice };
   } else if (price) {
@@ -383,6 +383,7 @@ export class ListingPageComponent extends Component {
     const { key: priceType, value: { amount, currency } } = getLowestPrice(currentListing);
 
     const { formattedPrice, priceTitle } = priceData(amount && currency ? new Money(amount, currency) : null, intl);
+    console.log({ formattedPrice, priceTitle })
 
 
     const handleBookingSubmit = values => {
