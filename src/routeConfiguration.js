64fb25cfcc6704f51d3ca49087082e703c8c72ff
Vesa Luusua <vesa.luusua@gmail.com>
2022-09-05 15:26:13 +0300

    Add PreviewResolverPage for Console preview links. E.g. '/preview?asset-path=content/pages/about.json'

--- src/routeConfiguration.js
+++ src/routeConfiguration.js
@@ -2,6 +2,7 @@ import React from 'react';
 import loadable from '@loadable/component';
 import getPageDataLoadingAPI from './containers/pageDataLoadingAPI';
 import { NotFoundPage } from './containers';
+import PreviewResolverPage from './containers/PreviewResolverPage/PreviewResolverPage';
 
 // routeConfiguration needs to initialize containers first
 // Otherwise, components will import form container eventually and
@@ -349,6 +350,14 @@ const routeConfiguration = () => {
       authPage: 'LoginPage',
       component: EmailVerificationPage,
       loadData: pageDataLoadingAPI.EmailVerificationPage.loadData,
+    },
+    // Do not change this path!
+    //
+    // The API expects that the application implements /preview endpoint
+    {
+      path: '/preview',
+      name: 'PreviewResolverPage',
+      component: PreviewResolverPage ,
     },
   ];
 };
