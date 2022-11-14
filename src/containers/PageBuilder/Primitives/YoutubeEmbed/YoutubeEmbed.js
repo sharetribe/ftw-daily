a85b2835df9359fc90199dcaad01458b3eee8b8d
Vesa Luusua <vesa.luusua@gmail.com>
2022-11-14 15:47:04 +0200

    Lazy load Youtube iframe to improve page performance if iframe is below the fold.

--- src/containers/PageBuilder/Primitives/YoutubeEmbed/YoutubeEmbed.js
+++ src/containers/PageBuilder/Primitives/YoutubeEmbed/YoutubeEmbed.js
@@ -2,13 +2,19 @@ import React from 'react';
 import { string } from 'prop-types';
 import classNames from 'classnames';
 
+import { lazyLoadWithDimensions } from '../../../../util/contextHelpers';
+
 import { AspectRatioWrapper } from '../../../../components/index.js';
 
 import css from './YoutubeEmbed.module.css';
 
 const RADIX = 10;
+const BLACK_BG = '#000000';
 
-export const YoutubeEmbed = React.forwardRef((props, ref) => {
+const IFrame = props => <iframe {...props} />;
+const LazyIFrame = lazyLoadWithDimensions(IFrame);
+
+export const YoutubeEmbed = props => {
   const { className, rootClassName, youtubeVideoId, aspectRatio } = props;
   const hasSlash = aspectRatio.indexOf('/') > 0;
   const [aspectWidth, aspectHeight] = hasSlash ? aspectRatio.split('/') : [16, 9];
@@ -18,9 +24,10 @@ export const YoutubeEmbed = React.forwardRef((props, ref) => {
 
   return (
     <AspectRatioWrapper className={classes} width={width} height={height}>
-      <iframe
+      <LazyIFrame
         src={`https://www.youtube-nocookie.com/embed/${youtubeVideoId}`}
         className={css.iframe}
+        style={{ background: BLACK_BG }}
         frameBorder="0"
         allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
         allowFullScreen
@@ -28,7 +35,7 @@ export const YoutubeEmbed = React.forwardRef((props, ref) => {
       />
     </AspectRatioWrapper>
   );
-});
+};
 
 YoutubeEmbed.displayName = 'YoutubeEmbed';
 
