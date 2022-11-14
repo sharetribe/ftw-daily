a448c4c65c99601b7c7cc5abe91abd6c69c73081
Vesa Luusua <vesa.luusua@gmail.com>
2022-11-14 17:57:00 +0200

    Edit LandingPage/Fallback (Janne's commit splitted) changes to LandingPage FallBack

--- src/containers/LandingPage/FallbackPage.js
+++ src/containers/LandingPage/FallbackPage.js
@@ -1,85 +1,44 @@
 import React from 'react';
-
 import PageBuilder from '../PageBuilder/PageBuilder';
+import css from './FallbackPage.module.css';
 
 // Create fallback content (array of sections) in page asset format:
 export const fallbackSections = {
   sections: [
     {
-      sectionType: 'features',
-      sectionId: 'hero',
-      background: { type: 'customBackground', color: '#ffff00' },
-      // backgroundImage: {
-      //   type: 'image',
-      //   alt: 'Background image',
-      //   image: {
-      //     id: 'image',
-      //     type: 'imageAsset',
-      //     attributes: {
-      //       variants: {
-      //         square1x: {
-      //           url: `https://picsum.photos/400/400`,
-      //           width: 400,
-      //           height: 400,
-      //         },
-      //         square2x: {
-      //           url: `https://picsum.photos/800/800`,
-      //           width: 800,
-      //           height: 800,
-      //         },
-      //       },
-      //     },
-      //   },
-      // },
-      blocks: [
-        {
-          blockType: 'defaultBlock',
-          blockId: 'hero-content',
-          media: {
-            type: 'image',
-            alt: 'First image',
-            image: {
-              id: 'image',
-              type: 'imageAsset',
-              attributes: {
-                variants: {
-                  square1x: {
-                    url: `https://picsum.photos/400/400`,
-                    width: 400,
-                    height: 400,
-                  },
-                  square2x: {
-                    url: `https://picsum.photos/800/800`,
-                    width: 800,
-                    height: 800,
-                  },
-                },
-              },
-            },
-          },
-          title: { type: 'heading1', content: 'My marketplace' },
-          text: {
-            type: 'markdown',
-            content:
-              '### My unique marketplace for booking listings\n### You can also list your services here!',
-          },
-          callToAction: {
-            type: 'internalButtonLink',
-            href: '/s',
-            label: 'Browse marketplace',
-          },
-        },
-      ],
+      sectionType: 'customMaintenance',
+      sectionId: 'maintenance-mode',
     },
   ],
 };
 
-// This is the fallback page, in case there's no Privacy Policy asset defined in Console.
+const SectionMaintenanceMode = props => {
+  const { sectionId } = props;
+
+  return (
+    <section id={sectionId} className={css.root}>
+      <div className={css.content}>
+        <h1>Maintenance mode</h1>
+        <p>
+          The marketplace is not fully operational at the moment. Try refreshing the page and if
+          that does not solve the issue, contact the marketplace admins.
+        </p>
+      </div>
+    </section>
+  );
+};
+
+// This is the fallback page, in case there's no Landing Page asset defined in Console.
 const FallbackPage = props => {
   const { title, description, schema, contentType } = props;
   return (
     <PageBuilder
       pageAssetsData={fallbackSections}
+      options={{
+        sectionComponents: {
+          customMaintenance: { component: SectionMaintenanceMode },
+        },
+      }}
       title={title}
       description={description}
       schema={schema}
