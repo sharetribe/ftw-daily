86e68d181bef4cb1b72b3a74d7ae47cc38bf4e03
Vesa Luusua <vesa.luusua@gmail.com>
2023-01-04 15:30:54 +0200

    terms-of-service asset doesn't have 'page' postfix

--- src/containers/TermsOfServicePage/TermsOfServicePage.duck.js
+++ src/containers/TermsOfServicePage/TermsOfServicePage.duck.js
@@ -2,6 +2,6 @@ import { fetchPageAssets } from '../../ducks/hostedAssets.duck';
 export const ASSET_NAME = 'terms-of-service';
 
 export const loadData = (params, search) => dispatch => {
-  const pageAsset = { termsOfServicePage: `content/pages/${ASSET_NAME}.json` };
+  const pageAsset = { termsOfService: `content/pages/${ASSET_NAME}.json` };
   return dispatch(fetchPageAssets(pageAsset, true));
 };
