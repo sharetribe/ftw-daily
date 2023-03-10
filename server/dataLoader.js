219b5b43e37030b5e3b105941954bbde610841f1
sentral09 <jude@hotpatch.com>
2023-03-10 13:01:49 +0000

    Revert "Data Loader update attempt 1"
    This reverts commit e795797bf19adce77f10f862e93fc963c91f6674.

--- server/dataLoader.js
+++ server/dataLoader.js
@@ -8,7 +8,6 @@ exports.loadData = function(requestUrl, sdk, matchPathname, configureStore, rout
   const store = configureStore({}, sdk);
 
   const dataLoadingCalls = matchedRoutes.reduce((calls, match) => {
-    matchedRoutes.reduce((calls, match) => {
     const { route, params } = match;
     if (typeof route.loadData === 'function' && !route.auth) {
       calls.push(store.dispatch(route.loadData(params, query)));
@@ -16,18 +15,9 @@ exports.loadData = function(requestUrl, sdk, matchPathname, configureStore, rout
     return calls;
   }, []);
 
-  // First fetch app-wide assets
-  // Then make loadData calls
-  // And return object containing preloaded state and translations
-  // This order supports other asset (in the future) that should be fetched before data calls.
-  return store
-    .dispatch(fetchAppAssets(config.appCdnAssets))
-    .then(fetchedAppAssets => {
-      translations = fetchedAppAssets?.translations?.data || {};
-      return Promise.all(dataLoadingCalls());
-   })
+  return Promise.all(dataLoadingCalls)
     .then(() => {
-      return { preloadedState: store.getState(), translations };
+      return store.getState();
     })
     .catch(e => {
       log.error(e, 'server-side-data-load-failed');
